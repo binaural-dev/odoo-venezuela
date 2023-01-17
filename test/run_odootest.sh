8ac972e101d5649dff560d1fae45bc4796f6dd60
Jose Vizcaya <joseangel@binauraldev.com>
2023-01-17 17:53:42 -0400

    WIP: Script para obtener tags.

--- test/run_odootest.sh
+++ test/run_odootest.sh
@@ -4,14 +4,18 @@ while getopts 'm:' flag;
 do
     case "${flag}" in
         m)
-            message = ${OPTARG}
+            message=${OPTARG}
             run_test_with_param
     esac
 done
 
 run_test_with_param(){
     echo ${message}
-    if [ "$message" == *"{"* ] || [ "$message" == *"}"*]; then
-        
+    pattern='{(\w+)([,\w]*)}$'
+    if [[ $message =~ $pattern ]]; then
+        echo $(echo $message | grep -oP '{(\w+)([,\w]*)}$')
+    else
+        echo "Oops!"
+        exit 1
     fi
 }
