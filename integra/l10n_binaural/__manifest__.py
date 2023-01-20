be64d23c4a8b83da59a8a9326b62438ac46977c0
Jose Acosta <jose.acosta@binaural.com.ve>
2023-01-20 15:59:04 -0400

    feat: creacion del modulo para plan de cuentas

---
+++ integra/l10n_binaural/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Binaural plan de cuentas",
+    "version": "1.0.0",
+    "category": "Accounting/Localizations/Account Charts",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+        # "l10n_multilang",
+    ],
+    "data": [
+        # # Chart of Accounts
+        # "data/account_chart_template_data.xml",
+        # "data/account_account_tag_data.xml",
+        # "data/account.account.template.csv",
+        # "data/account.group.template.csv",
+        # # Taxes
+        # "data/account_tax_group_data.xml",
+        # "data/account_tax_report_data.xml",
+        # "data/account_tax_template_data.xml",
+        # "data/account_fiscal_position_template_data.xml",
+        # "data/account_account_template_post_data.xml",
+        # "data/account_chart_post_data.xml",
+        # "data/account_chart_template_try_loading.xml",
+        # # Views and others
+        # "views/xxxmodel_views.xml",
+    ],
+    "demo": [
+        # "demo/demo_company.xml",
+    ],
+}
