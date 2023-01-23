441ed01d1c8421bf63bb9f9f3aacf65fe4655125
Jose Acosta <jose.acosta@binaural.com.ve>
2023-01-23 18:49:05 -0400

    proveedores y clientes por defecto

--- integra/l10n_binaural/__manifest__.py
+++ integra/l10n_binaural/__manifest__.py
@@ -3,25 +3,14 @@
     "version": "1.0.0",
     "category": "Accounting/Localizations/Account Charts",
     "license": "LGPL-3",
-    "depends": ["account"],
+    "depends": ["account", "stock", "sale"],
     "data": [
         "data/res_currency_data.xml",
         "data/l10n_binaural_chart_data.xml",
         "data/account.account.template.csv",
         "data/account_tax_group_data.xml",
         "data/account_tax_template_data.xml",
-        # # Taxes
-        # "data/account_tax_group_data.xml",
-        # "data/account_tax_report_data.xml",
-        # "data/account_tax_template_data.xml",
-        # "data/account_fiscal_position_template_data.xml",
-        # "data/account_account_template_post_data.xml",
-        # "data/account_chart_post_data.xml",
-        # "data/account_chart_template_try_loading.xml",
-        # # Views and others
-        # "views/xxxmodel_views.xml",
-    ],
-    "demo": [
-        # "demo/demo_company.xml",
+        "data/product_template_data.xml",
+        "data/res_partner_data.xml",
     ],
 }
