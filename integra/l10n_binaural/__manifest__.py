c7012ec0e365b92017859c3c505c6ad60d80b4b3
Jose Acosta <jose.acosta@binaural.com.ve>
2023-01-24 10:05:24 -0400

    fix: config journals

--- integra/l10n_binaural/__manifest__.py
+++ integra/l10n_binaural/__manifest__.py
@@ -3,11 +3,12 @@
     "version": "1.0.0",
     "category": "Accounting/Localizations/Account Charts",
     "license": "LGPL-3",
-    "depends": ["account", "stock", "sale"],
+    "depends": ["account_accountant", "stock", "sale"],
     "data": [
         "data/res_currency_data.xml",
         "data/l10n_binaural_chart_data.xml",
         "data/account.account.template.csv",
+        "data/account_journal_data.xml",
         "data/account_tax_group_data.xml",
         "data/account_tax_template_data.xml",
         "data/product_template_data.xml",
