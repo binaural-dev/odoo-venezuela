bbedaa39eb094469623aed32bfcdc45b885e9755
Miguel Gozaine <96434156+miguel-binaural@users.noreply.github.com>
2023-02-09 14:46:46 -0400

    feat: Binaural Account Asset (#35)
    Se creó el modulo de gestión de activos fijos, para agregar al modelo de
    activos fijos los campos de tasa y tasa inversa.
    Cuando se selecciona un apunte en el campo original_move_line_ids las
    tasas del asiento al cual pertenece viajan al activo fijo.
    Cuando se calcula la depreciación o se confirma el activo fijo, los
    asientos creados van a tener las tasas del activo.
    
    Co-authored-by: Miguel Gozaine <hordemzerado@gmail.com>

---
+++ integra/binaural_account_asset/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Gestión de activos fijos (Binaural)",
+    "summary": """
+        Módulo con modificaciones para la gestión de activos fijos en Venezuela.
+    """,
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://binauraldev.com/",
+    "category": "Technical",
+    "version": "16.0.0.0.1",
+    # any module necessary for this one to work correctly
+    "depends": ["base", "account_asset", "binaural_accountant"],
+    # always loaded
+    "data": [
+        "views/account_asset.xml",
+    ],
+}
