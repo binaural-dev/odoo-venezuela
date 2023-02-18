789bed1a370ac79cfd8fb305c48bcdb4f1bdd8c2
andyengh <anderson@binauraldev.com>
2023-02-18 00:17:58 -0400

    fix: No recalcular los datos
    *Ahora los datos de los subtotales vienen de las lineas de las facturas

--- integra/binaural_accountant/models/account_tax.py
+++ integra/binaural_accountant/models/account_tax.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from odoo import api, models, _
 
 
@@ -7,9 +8,11 @@ class AccountTax(models.Model):
     @api.model
     def _prepare_tax_totals(self, base_lines, currency, tax_lines=None):
         """
-        This function calculates the total taxes in the alternate currency,
-        calling itself 2 times, once in its original currency and once with
-        the amounts multiplied by the rate of the alternate currency.
+        This function adds the alternate currency tax amounts to tax_totals.
+        In it, the parent function is executed 2 times, once for the original
+        currency and once for the alternate currency.
+
+        The data that is brought is not recalculated, that is, it comes from the lines of the entry
         ------
         Parameters: (Parameters inherited)
             base_lines: list of dict
@@ -34,18 +37,25 @@ class AccountTax(models.Model):
         if not foreign_currency:
             foreign_currency = self.env.company.currency_foreign_id or False
 
-        for base_line in base_lines:
-            rate = base_line["record"].move_id.foreign_inverse_rate
+        taxes = []
 
-            base_line["price_unit"] = base_line["price_unit"] * rate
-            base_line["price_subtotal"] = base_line["price_subtotal"] * rate
+        for base_line in base_lines:
+            base_line["price_unit"] = base_line["record"].foreign_price
+            base_line["price_subtotal"] = base_line["record"].foreign_subtotal
             base_line["currency"] = foreign_currency
+
+            if base_line["taxes"]:
+                taxes.append({"tax": base_line["taxes"][0], "base": base_line["price_subtotal"]})
 
         if tax_lines:
             for tax_line in tax_lines:
-                rate = tax_line["record"].move_id.foreign_inverse_rate
                 tax_line["currency"] = foreign_currency
-                tax_line["tax_amount"] = tax_line["tax_amount"] * rate
+                tax_line["tax_amount"] = 0.0
+                for tax in taxes:
+                    if tax_line["tax_repartition_line"].invoice_tax_id.id == tax["tax"].id:
+                        tax_line["tax_amount"] += tax["base"] * (
+                            tax_line["tax_repartition_line"].invoice_tax_id.amount / 100
+                        )
 
         foreign_taxes = super()._prepare_tax_totals(base_lines, foreign_currency, tax_lines)
 
