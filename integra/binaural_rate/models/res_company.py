8a5d0f823d19543f6709e8b12345f25927b227a3
Jose Acosta <jose.acosta@binaural.com.ve>
2023-02-23 17:23:08 -0400

    feat: se agrega peticion de las tasas al res.company
    se utiliza la funcion escrita en tools para llamar a la tasa del dia en la pagina del banco central

--- integra/binaural_rate/models/res_company.py
+++ integra/binaural_rate/models/res_company.py
@@ -1,4 +1,8 @@
 from odoo import api, fields, models, _
+from ...tools import binaural_bcv_query
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class ResCompany(models.Model):
@@ -7,3 +11,9 @@ class ResCompany(models.Model):
     currency_foreign_id = fields.Many2one(
         "res.currency", string="Currency Foreign", help="Currency Foreign for the company"
     )
+
+    currency_provider = fields.Selection(selection_add=[("bcv", "Venezuelan Central Bank")])
+
+    def _parse_bcv_data(self, availible_currencies):
+        _logger.info("Parsing BCV data")
+        return binaural_bcv_query.get_usd_rate_of_the_day_bcv()
