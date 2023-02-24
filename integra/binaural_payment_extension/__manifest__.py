aed9ffd5d74b23d0af882ddd493288e6daa785f7
bryanbinaural <bryan@binaural.com.ve>
2023-02-24 17:07:38 -0400

    Feature maestros de retenciones

---
+++ integra/binaural_payment_extension/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Binaural Extensiones de pago",
+
+    'summary': """
+       Modulo de extensiones de pago """,
+
+    'license': 'LGPL-3',
+    
+    'author': "Binauraldev",
+    'website': "https://binauraldev.com/",
+    'category': 'Accountant/Accountant',
+    'version': '16.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'binaural_rate', 'account_accountant' ],
+
+    # always loaded
+    'data': [],
+
+    'images': ['static/description/icon.png'],
+
+    'application':True,
+}
