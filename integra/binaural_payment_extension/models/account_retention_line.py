a4254a228a7dd42af9cc28ffc2cff447c1637dba
bryanbinaural <bryan@binaural.com.ve>
2023-03-15 18:23:49 -0400

    Creando retenciones de ISLR desde formulario

--- integra/binaural_payment_extension/models/account_retention_line.py
+++ integra/binaural_payment_extension/models/account_retention_line.py
@@ -45,8 +45,8 @@ class AccountRetentionLine(models.Model):
     invoice_total = fields.Float(string="Total invoiced", digits=(16, 2))
     iva_amount = fields.Float(string="IVA", digits=(16, 2))
 
-    retention_amount = fields.Float(digits=(16, 2))
-    foreign_retention_amount = fields.Float(digits=(16, 2))
+    retention_amount = fields.Float(digits="Tasa", compute="_compute_retention_amount", store=True)
+    foreign_retention_amount = fields.Float(digits="Tasa", compute="_compute_retention_amount", store=True)
 
     payment_concept_id = fields.Many2one(
         "payment.concept", "Payment concept", ondelete="cascade", index=True
@@ -126,15 +126,24 @@ class AccountRetentionLine(models.Model):
                     ]
                     record.foreign_invoice_total = record.move_id.tax_totals["foreign_amount_total"]
 
-                    # compute the retention amount
-                    record.retention_amount = (
-                        (record.invoice_amount * record.related_percentage_tax_base / 100)
-                        * record.related_percentage_fees
-                        / 100
-                    )
+                    
 
-                    record.foreign_retention_amount = (
-                        (record.foreign_invoice_amount * record.related_percentage_tax_base / 100)
-                        * record.related_percentage_fees
-                        / 100
-                    )
+    @api.onchange("invoice_amount", "related_percentage_tax_base", "related_percentage_fees")
+    @api.depends("invoice_amount", "related_percentage_tax_base", "related_percentage_fees")
+    def _compute_retention_amount(self):
+        """
+        
+        """
+        # ("retention_id", "=", False), ("retention_id.type_retention", "=", "islr")
+        for record in self:
+            record.retention_amount = (
+                (record.invoice_amount * record.related_percentage_tax_base / 100)
+                * record.related_percentage_fees
+                / 100
+            )
+
+            record.foreign_retention_amount = (
+                (record.foreign_invoice_amount * record.related_percentage_tax_base / 100)
+                * record.related_percentage_fees
+                / 100
+            )
