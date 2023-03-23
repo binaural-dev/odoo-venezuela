ac97b08af38522b663d63a8b868bfaa9592252ce
Jose Vizcaya <joseangel@binauraldev.com>
2023-03-23 16:23:32 -0400

    WIP: Simplificada la estructura del modulo.

--- integra/binaural_filter_partner_mixin/__manifest__.py
+++ integra/binaural_filter_partner_mixin/__manifest__.py
@@ -1,35 +1,16 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "binaural_filter_partner_mixin",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+    "name": "Binaural Filtro de Contacto",
+    "summary": """
+        Modulo tècnico el cual contiene un mixin para filtrar
+        los contactos de los clientes y proveedores.
     """,
-
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://www.binauraldev.com",
+    "category": "Technical",
+    "version": "16.0.0.0.1",
+    "depends": ["base"],
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
     ],
 }
