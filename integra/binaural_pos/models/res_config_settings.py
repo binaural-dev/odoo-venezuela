5f8b03983d69060ebb922810f6b696937aa04d80
Anderson Armeya <anderson@binaural.com.ve>
2023-03-25 17:35:25 -0400

    feat: IGTF en POS 16 YEAAAHHHHH!!!!
    *Se Agrego funcion para mandar el monto de igtf a la cuenta de IGTF
    colocada en la configuracion
    
    *Se Elimino un logger sobrante en accountant

--- integra/binaural_pos/models/res_config_settings.py
+++ integra/binaural_pos/models/res_config_settings.py
@@ -4,4 +4,4 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     module_binaural_pos_igtf = fields.Boolean(related='company_id.module_binaural_pos_igtf', readonly=False)
-    module_binaural_igtf = fields.Boolean(related='company_id.module_binaural_igtf', readonly=False)
+    module_binaural_base_igtf = fields.Boolean(related='company_id.module_binaural_base_igtf', readonly=False)
--- integra/binaural_pos/static/src/js/PaymentScreen.js
+++ integra/binaural_pos/static/src/js/PaymentScreen.js
@@ -3,16 +3,17 @@ odoo.define("binaural_pos.PaymentScreen", function(require) {
   const PaymentScreen = require("point_of_sale.PaymentScreen")
   const Registries = require("point_of_sale.Registries")
   const NumberBuffer = require('point_of_sale.NumberBuffer');
-  const { _t } = require('web.core');
 
   const BinauralPaymentScreen = (PaymentScreen) =>
     class BinauralPaymentScreen extends PaymentScreen {
       _updateSelectedPaymentline() {
+        console.log("AQUIS")
         if (!this.selectedPaymentLine) return; // do nothing if no selected payment line
 
         if (!this.selectedPaymentLine.payment_method.is_foreign_currency) {
           return super._updateSelectedPaymentline()
         }
+        console.log("AQUIIIIIIIIIIIIIIIIIIIII")
 
         this.selectedPaymentLine.set_foreign_amount(NumberBuffer.getFloat())
         this.selectedPaymentLine.set_amount(
