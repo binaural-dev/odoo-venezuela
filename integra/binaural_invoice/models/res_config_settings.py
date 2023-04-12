40e4c114f02823d1a2f6366a10e8338ea93d93c7
Jose Vizcaya <joseangel@binauraldev.com>
2023-04-12 15:09:19 -0400

    WIP: Configuracion para series de factura
    Tomando lo que previamente estaba en la version 14 de Integra, se
    implementó la configuración que permite activar la facturación de ventas
    por series.

--- integra/binaural_invoice/models/res_config_settings.py
+++ integra/binaural_invoice/models/res_config_settings.py
@@ -4,7 +4,5 @@ from odoo import api, fields, models, _
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    max_product_invoice = fields.Integer(
-        related="company_id.max_product_invoice",
-        readonly=False,
-    )
+    max_product_invoice = fields.Integer(related="company_id.max_product_invoice", readonly=False)
+    series_invoicing = fields.Boolean(related="company_id.series_invoicing", readonly=False)
