aaf8776daed61504c43b4db77d64581e8dd6ea25
Jose Vizcaya <joseangel@binauraldev.com>
2023-04-17 18:12:38 -0400

    WIP: Creacion de modulo de inventario.

---
+++ integra/binaural_stock/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Binaural Inventario",
+    "summary": """
+        Personalizaciones relacionadas al inventario.
+    """,
+    "author": "Binauraldev",
+    "website": "https://www.binauraldev.com",
+    "category": "Stock/Analytic/Accounting",
+    "version": "16.0.0.0.1",
+    "depends": ["analytic"],
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/stock_picking_views.xml",
+    ],
+}
