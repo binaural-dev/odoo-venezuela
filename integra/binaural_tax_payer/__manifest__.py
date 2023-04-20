a4bdd7e7e2721f5b74d2ef3b3cba08b51531c5c0
bryanbinaural <bryan@binaural.com.ve>
2023-04-20 16:11:12 -0400

    Fix: Traducciones

--- integra/binaural_tax_payer/__manifest__.py
+++ integra/binaural_tax_payer/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "1.0",
+    "version": "1.1",
     "depends": ["base", "binaural_rate", "binaural_tax"],
     "data": [
         "views/res_partner.xml",
