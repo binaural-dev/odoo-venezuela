aef52128cd47b18f2b6ea025f1a8f53c7a339385
Miguel Gozaine <miguel@binauraldev.com>
2023-04-24 11:20:33 -0400

    fix: Od Journal Sequence
    Problema:
    El módulo od_journal_sequence pone el campo name del model account.move
    como readonly, por lo cual no se permite poner el nombre en las facturas
    de proveedor.
    
    Solución:
    Se hereda la vista del módulo od_journal_sequence en la que se pone el
    campo como readonly y se cambia.

--- integra/binaural_invoice/__manifest__.py
+++ integra/binaural_invoice/__manifest__.py
@@ -8,26 +8,27 @@
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
     # any module necessary for this one to work correctly
-    "depends": ["binaural_accountant", "binaural_contact", "binaural_fiscal", "binaural_filter_partner"],
+    "depends": [
+        "binaural_accountant",
+        "binaural_contact",
+        "binaural_fiscal",
+        "binaural_filter_partner",
+        "od_journal_sequence",
+    ],
     # always loaded
     "data": [
         "security/binaural_invoice_groups.xml",
         "security/ir.model.access.csv",
-
         "data/account_data.xml",
         "data/invoice_free_form_paperformat.xml",
         "data/invoice_sale_note_paperformat.xml",
-
         "report/report_invoice_free_form.xml",
         "report/report_invoice_sale_note.xml",
-
         "views/account_move.xml",
         "views/account_journal_views.xml",
         "views/res_config_settings.xml",
-
         "wizard/accounting_reports_views.xml",
     ],
-
-    'images': ['static/description/icon.png'],
-    'application': True
+    "images": ["static/description/icon.png"],
+    "application": True,
 }
