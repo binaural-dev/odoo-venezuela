0ab9203345400cbb561591849d064ff7c567c58e
Jose Vizcaya <joseangel@binauraldev.com>
2023-05-03 10:22:17 -0400

    FEAT: Categorías de productos multi-compañía.
    Se creó el módulo de Binaural Inventario para colocar las
    personalizaciones o localizaciones correspondientes a ello.
    
    Se migró la personalización que vuelve las categorías de productos a
    multi-compañìa, agregando la regla de registro para que los usuarios
    pertenecientes a determinadas empresas, solamente puedan ver aquellas
    categorías que solo formen parte de esta.

---
+++ integra/binaural_stock/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Binaural Inventario",
+    "summary": """
+        Modulo de localización relacionado
+        al inventario.
+    """,
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://www.binauraldev.com",
+    "category": "Stock",
+    "version": "16.0.0.0.1",
+    "depends": ["stock"],
+    "data": [
+        # 'security/ir.model.access.csv',
+        "security/security_binaural_stock.xml",
+        "views/product_category_views.xml",
+    ],
+}
