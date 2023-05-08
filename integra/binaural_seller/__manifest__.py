143ffc2b7aae31b171758a5ea56c6f325f35f137
Carlos-Lin-Binaural <carlos@binauraldev.com>
2023-05-08 18:21:14 -0400

    Feat module Seller
    -Se migro el modulo de binaural_seller de integra14 al integra16
    -Se heredo res_partner para ingresar el campo seller_id
    	*Se sobre escribio el metodo commercial_fields() para integrarle seller_id
    -Se heredo account_move para ingresar el campo seller_id
    -Se agregaron las vistas heredadas de la migracion
    -Se crearon las traducciones

---
+++ integra/binaural_seller/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Binaural Vendedores",
+    "summary": """
+         Modulo para vendedores """,
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://binauraldev.com/",
+    "version": "16.2",
+    "category": "Hr/Invoicing/Account",
+    "depends": [
+        "contacts",
+        "account_accountant",
+        "hr"
+    ],
+    "data": [
+        "views/account_move_inherit_view.xml",
+        "views/res_partner_inherit_view_form.xml",
+    ],
+    "images": ["static/description/icon.png"],
+}
