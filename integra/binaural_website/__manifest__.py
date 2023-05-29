11da181385caf3b3f467986885a0b18bcd67a316
Jose <jose.acosta@binaural.com.ve>
2023-05-29 17:05:25 -0400

    feat: se agrega el la vista de ventas para el sitio web
    Se agrega la vista de ventas dentro del modulo de sitio web que ya existe
    en integra 14 y para que tenga el campo nuevo city_id en lugar del campo
    city nativo de odoo.
    
    Se soluciona tambien el que el campo city_id llega vacio al momento de
    buscar seleccionar un campo desde la vista ya que los selection poseian
    un display none.
    
    Se vuelven a cargar las traducciones del modulo.
    
    Se soluciona el problema al tratar de ingresar al la vista del sitio web
    para editar tus dtos de contacto.

--- integra/binaural_website/__manifest__.py
+++ integra/binaural_website/__manifest__.py
@@ -9,17 +9,19 @@
     'author': "Binauraldev",
     'website': "https://binauraldev.com/",
     'category': 'Website',
-    'version': '0.1.1',
+    'version': '0.2.0',
     # any module necessary for this one to work correctly
-    'depends': ['base','website','binaural_location'],
+    'depends': ['base','website','binaural_location','website_sale'],
     
     "data": [
         "static/src/views/profile_view.xml",
+        "static/src/views/website_sale_view.xml",
     ],
     
     "assets": {
         'web.assets_frontend': [
             'binaural_website/static/src/js/profile.js',
+            'binaural_website/static/src/js/website_sale.js'
         ]
     },
 
