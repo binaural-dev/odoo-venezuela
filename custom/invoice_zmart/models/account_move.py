e5550876aef7bfd5bcb54b94b67ef1aec46ad88d
yeison <yeison@binauraldev.com>
2023-06-13 11:35:08 -0400

    Metodos de prueba borrado

--- custom/invoice_zmart/models/account_move.py
+++ custom/invoice_zmart/models/account_move.py
@@ -5,22 +5,3 @@ class AccountInvoice(models.Model):
 
     printed = fields.Boolean(
         default=False)
-    
-    # def print_and_mark_as_printed(self):
-    #     report_name = 'binaural_invoice.template_invoice_sale_note_binaural_invoice'
-    #     self.ensure_one()
-    #     self.env['ir.actions.report']._render_qweb_pdf(report_name, [self.id])
-    #     self.write({'printed': True})
-    #     return True
-
-    def print_and_mark_as_printed(self):
-        report_name = 'binaural_invoice.template_invoice_sale_note_binaural_invoice'
-        self.ensure_one()
-        self.env['ir.actions.report']._render_qweb_pdf(report_name, [self.id])
-        self.write({'printed': True})
-        return True
-
-    def action_invoice_sent(self):
-        self.ensure_one()
-        self.print_and_mark_as_printed()
-        return True
