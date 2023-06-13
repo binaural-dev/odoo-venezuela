18f83d53a5fa39d2443b381a0369894512fa4cf0
yeison <yeison@binauraldev.com>
2023-06-13 13:07:47 -0400

    Comentarios borrados

--- custom/invoice_zmart/models/account_move.py
+++ custom/invoice_zmart/models/account_move.py
@@ -4,13 +4,6 @@ class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
     printed = fields.Boolean(default=False)
-    
-    # def print_and_mark_as_printed(self):
-    #     report_name = 'binaural_invoice.template_invoice_sale_note_binaural_invoice'
-    #     self.ensure_one()
-    #     self.env['ir.actions.report']._render_qweb_pdf(report_name, [self.id])
-    #     self.write({'printed': True})
-    #     return True
 
     def button_invoice(self):
         return self.env.ref('account.account_invoices').report_action(self)
