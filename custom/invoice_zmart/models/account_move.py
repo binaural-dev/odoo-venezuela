696706a063ad18d19420c8a40f55b3279e01c72c
yeison <yeison@binauraldev.com>
2023-06-13 13:02:23 -0400

    Botones de impresion -La accion de elegir una opcion en el boton de imprimir fue agrgada a botones en sus respectivos modulos

--- custom/invoice_zmart/models/account_move.py
+++ custom/invoice_zmart/models/account_move.py
@@ -3,5 +3,22 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
-    printed = fields.Boolean(
-        default=False)
+    printed = fields.Boolean(default=False)
+    
+    # def print_and_mark_as_printed(self):
+    #     report_name = 'binaural_invoice.template_invoice_sale_note_binaural_invoice'
+    #     self.ensure_one()
+    #     self.env['ir.actions.report']._render_qweb_pdf(report_name, [self.id])
+    #     self.write({'printed': True})
+    #     return True
+
+    def button_invoice(self):
+        return self.env.ref('account.account_invoices').report_action(self)
+    
+    def button_free_form(self):
+        return self.env.ref('binaural_invoice.action_invoice_free_form_binaural_invoice').report_action(self)
+    
+    def button_invoice_sale_note(self):
+        return self.env.ref('binaural_invoice.action_invoice_sale_note_binaural_invoice').report_action(self)
+    
+    
--- custom/purchase_zmart/models/purchase_order.py
+++ custom/purchase_zmart/models/purchase_order.py
@@ -7,3 +7,9 @@ class PurchaseOrderZmart(models.Model):
     name_company = fields.Many2one('purchase.company', string="Company")
     vl_number = fields.Char(string="VL Number")
     date_in_store = fields.Date()
+    
+    def button_report_purchase_order(self):
+        return self.env.ref('purchase.action_report_purchase_order').report_action(self)
+    
+    def button_report_purchase_quotation(self):
+        return self.env.ref('purchase.report_purchase_quotation').report_action(self)
