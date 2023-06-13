7270221f0c9470576861705192542a4595835022
yeison <yeison@binauraldev.com>
2023-06-13 09:25:15 -0400

    Campo de impresion -Se agrego campo de impreso en la vista form del account move

--- custom/invoice_zmart/models/account_move.py
+++ custom/invoice_zmart/models/account_move.py
@@ -3,4 +3,24 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
 
-    printed = fields.Boolean(default=False)
+    printed = fields.Boolean(
+        default=False)
+    
+    # def print_and_mark_as_printed(self):
+    #     report_name = 'binaural_invoice.template_invoice_sale_note_binaural_invoice'
+    #     self.ensure_one()
+    #     self.env['ir.actions.report']._render_qweb_pdf(report_name, [self.id])
+    #     self.write({'printed': True})
+    #     return True
+
+    def print_and_mark_as_printed(self):
+        report_name = 'binaural_invoice.template_invoice_sale_note_binaural_invoice'
+        self.ensure_one()
+        self.env['ir.actions.report']._render_qweb_pdf(report_name, [self.id])
+        self.write({'printed': True})
+        return True
+
+    def action_invoice_sent(self):
+        self.ensure_one()
+        self.print_and_mark_as_printed()
+        return True
