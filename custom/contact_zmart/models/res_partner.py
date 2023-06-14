ba506a426092c8f6cb36d173aa131c17d6c89a59
yeison <yeison@binauraldev.com>
2023-06-14 17:16:13 -0400

    Campos en el modulo de contactos -Se agragron nuevos campos al modulo de contactos y los campos que adjuntan un archivo pueden adjuntar mas de uno

--- custom/contact_zmart/models/res_partner.py
+++ custom/contact_zmart/models/res_partner.py
@@ -4,5 +4,33 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     sales_area = fields.Many2one('res.partner.sale')
-    rif = fields.Binary()
-    commercial_register = fields.Binary()
+    commercial_register = fields.Many2many(
+        'ir.attachment',
+        'commercial_register_rel',
+        'commercial_register_id',
+        'attachment_id'
+    )
+    rif = fields.Many2many(
+        'ir.attachment',
+        'rif_rel',
+        'rif_id',
+        'attachment_id'
+    )
+    legal_representante = fields.Many2many(
+        'ir.attachment',
+        'legal_representante_rel',
+        'legal_representante_id',
+        'attachment_id'
+    )
+    commercial_reference = fields.Many2many(
+        'ir.attachment',
+        'commercial_reference_rel',
+        'commercial_reference_id',
+        'attachment_id'
+    )
+    bank_reference = fields.Many2many(
+        'ir.attachment',
+        'bank_reference_rel',
+        'bank_reference_id',
+        'attachment_id'
+    )
