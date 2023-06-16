3ece1d9334ee3dc309ea0a993fea95748b9b7fde
yeison <yeison@binauraldev.com>
2023-06-16 09:09:19 -0400

    Campos Corregidos -Se agregaron la inforamacion en los campos solicitada por el cliente y se tradujo cada campo

--- custom/sale_zmart/models/sale_order.py
+++ custom/sale_zmart/models/sale_order.py
@@ -1,11 +1,17 @@
+from datetime import datetime,timedelta
 from odoo import api, fields, models
 
 class SaleOrderZmart(models.Model):
     _inherit = "sale.order"
     
-    shipping_type = fields.Many2one(
-        'sale.shipping.type',
-        string="shipping type"
+    shipping_type = fields.Selection(
+        [
+            ("withdrawal", "withdrawal"),
+            ("shipment", "shipment"),
+        ],
+        default="withdrawal",
+        store=True,
+        required=True
     )
     name_company = fields.Many2one(
         'sale.company', 
@@ -33,7 +39,35 @@ class SaleOrderZmart(models.Model):
         required=True
     )
     product_id = fields.Many2one('product.template', string='Product')
+    unlock_date = fields.Date(string='Unlock Date')
 
+    def action_confirm(self):
+        for order in self:
+            # Set the unlock date to 7 days from the confirmation date
+            unlock_date = datetime.now() + timedelta(days=2)
+            order.unlock_date = unlock_date.date()
+
+            # Lock the inventory for the products in the order
+            for line in order.order_line:
+                line.product_id.virtual_available -= line.product_uom_qty
+                line.product_id.outgoing_qty += line.product_uom_qty
+
+            # Call the original confirm function to create the order
+            res = super().action_confirm()
+
+            # Schedule the inventory to be unlocked on the unlock date
+            order.env['stock.change.product.qty'].sudo().create({
+                'product_id': line.product_id.id,
+                'location_id': line.order_id.warehouse_id.lot_stock_id.id,
+                'new_quantity': line.product_uom_qty,
+                'product_uom_id': line.product_uom.id,
+                'company_id': order.company_id.id,
+                'description': 'Unlock inventory',
+                'date': order.unlock_date,
+                'action': 'down',
+            })
+
+        return res
     def button_sale_order(self):
         return self.env.ref('sale.action_report_saleorder').report_action(self)
 
