a91417e0c5c689331ad70a9d4025e3b4f8c2444a
yeison <yeison@binauraldev.com>
2023-06-18 09:02:47 -0400

    Ajuste de vistas e identacion

--- custom/sale_zmart/models/stock_picking.py
+++ custom/sale_zmart/models/stock_picking.py
@@ -1,10 +1,28 @@
 from odoo import models, fields
-import logging
-_logger = logging.getLogger(__name__)
-class StockPickingBinauralInventario(models.Model):
+
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    shipping_type = fields.Selection(related='sale_id.shipping_type', string="Shipping type")
-    name_company = fields.Many2one(related='sale_id.name_company', string="Company")
-    packing_factor = fields.Char(store="True")
-    sequence_code = fields.Char(related='picking_type_id.sequence_code')
+    shipping_type = fields.Selection(
+        related = 'sale_id.shipping_type', 
+        string = "Shipping type"
+    )
+    shipping_name_company = fields.Many2one(
+        related = 'sale_id.shipping_name_company'
+    )
+    shipping_method = fields.Selection(
+        [
+            ("prepaid", "Prepaid"),
+            ("free", "Free"),
+            ("collect_at_destination", "Collect at Destination"),
+        ],
+        related = "sale_id.shipping_method",
+        default = "free",
+        store = True
+    )
+    packing_factor = fields.Char(
+        store = "True"
+    )
+    sequence_code = fields.Char(
+        related = 'picking_type_id.sequence_code'
+    )
