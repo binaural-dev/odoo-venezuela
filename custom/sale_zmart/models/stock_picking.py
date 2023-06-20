d6e23c2aa6cd639d786988349a3125095424dbf0
yeison <yeison@binauraldev.com>
2023-06-20 10:57:27 -0400

    Ajuste de tamano de etiqueta y campos agregados

--- custom/sale_zmart/models/stock_picking.py
+++ custom/sale_zmart/models/stock_picking.py
@@ -1,4 +1,7 @@
-from odoo import models, fields
+import base64
+import qrcode
+import io
+from odoo import models, fields,api
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -44,5 +47,31 @@ class StockPicking(models.Model):
         'res.users', 
         default = lambda self: self.env.user
     )
+    package_qty = fields.Integer(
+        "Package Quantity", 
+        help = "Quantity of packages used in the picking"
+    )
+    qr_code = fields.Binary(
+        string = 'Código QR', 
+        compute = '_compute_qr_code'
+    )
+    
+    @api.depends('name')
+    def _compute_qr_code(self):
+        for record in self:
+            # Generar el código QR
+            qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+            qr.add_data(record.name)
+            qr.make(fit=True)
+            img = qr.make_image(fill_color="black", back_color="white")
+
+            # Convertir la imagen a base64
+            buffer = io.BytesIO()
+            img.save(buffer, format="PNG")
+            qr_image = base64.b64encode(buffer.getvalue())
+
+            # Actualizar el campo binario con el código QR
+            record.qr_code = qr_image
     def print_label(self):
         return self.env.ref('sale_zmart.action_print_label').report_action(self)
+    
