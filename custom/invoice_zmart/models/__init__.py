5fe299bf96ebb47c30a491192d4ec7c6dfa1d255
yeison <yeison@binauraldev.com>
2023-06-20 15:09:12 -0400

    Ajuste de nota de entrega en $ y bs y creacion de grupo para tener boton disponible

--- custom/invoice_zmart/models/__init__.py
+++ custom/invoice_zmart/models/__init__.py
@@ -2,3 +2,4 @@ from . import account_move
 from . import ir_action_report
 from . import account_payment_register
 from . import account_payment
+from . import res_group
--- custom/invoice_zmart/models/account_move.py
+++ custom/invoice_zmart/models/account_move.py
@@ -11,4 +11,7 @@ class AccountInvoice(models.Model):
     def button_invoice_sale_note(self):
         return self.env.ref('binaural_invoice.action_invoice_sale_note_binaural_invoice').report_action(self)
     
+    def button_invoice_sale_note_bs(self):
+        return self.env.ref('invoice_zmart.action_invoice_sale_note_bs').report_action(self)
+    
     
