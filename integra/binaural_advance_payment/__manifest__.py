d6aecbf5bb426226a0de99ab7dc0150d89fecca1
Bryan <104509398+bryanbinaural@users.noreply.github.com>
2023-06-30 16:28:59 -0400

    Update __manifest__.py
    Actualizacion de Anticipos

--- integra/binaural_advance_payment/__manifest__.py
+++ integra/binaural_advance_payment/__manifest__.py
@@ -8,7 +8,7 @@
     "category": "Accounting/Igtf",
     "version": "16.0.0.0.1",
     # any module necessary for this one to work correctly
-    "depends": ["base", "binaural_accountant", "binaural_tax", "binaural_rate", "binaural_fiscal", "binaural_payment_reports"],
+    "depends": ["base", "binaural_accountant", "binaural_tax", "binaural_rate", "binaural_fiscal"],
     # always loaded
     "data": [
         "views/res_config_settings.xml",
