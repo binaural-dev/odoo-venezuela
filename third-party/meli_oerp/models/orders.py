ee3ee0011825dafa4b7ba6d6f99e3fd47fb9150a
Manuel Guerrero <73857822+manuelgc1201@users.noreply.github.com>
2023-07-02 22:22:58 -0400

    fix: campo eliminado en update de ml
    Se vuelve a agregar el campo cancel_blocked que moldeo interactive borro en su ultima actualizacion

--- third-party/meli_oerp/models/orders.py
+++ third-party/meli_oerp/models/orders.py
@@ -2612,6 +2612,8 @@ class sale_order_cancel_wiz_meli(models.TransientModel):
     _name = "sale.order.cancel.wiz.meli"
     _description = "Cancel Order"
 
+	cancel_blocked = fields.Boolean(string="Desbloquear y Cancelar", default=True)
+	
     def cancel_order(self, context=None):
         context = context or self.env.context
         orders_ids = ('active_ids' in context and context['active_ids']) or []
