efd8e9b37e43fe6709f0da8d415d37665c6c0f44
Miguel Gozaine <miguel@binauraldev.com>
2023-07-05 16:57:59 -0400

    fix: Cambio en POS #3504
    Ahora no se permite validar el pago de una orden de punto de venta si
    hay cambio pendiente.

--- integra/binaural_pos/static/src/js/PaymentScreen.js
+++ integra/binaural_pos/static/src/js/PaymentScreen.js
@@ -1,39 +1,49 @@
-odoo.define("binaural_pos.PaymentScreen", function(require) {
-
-  const PaymentScreen = require("point_of_sale.PaymentScreen")
-  const Registries = require("point_of_sale.Registries")
-  const NumberBuffer = require('point_of_sale.NumberBuffer');
+odoo.define("binaural_pos.PaymentScreen", function (require) {
+  const PaymentScreen = require("point_of_sale.PaymentScreen");
+  const Registries = require("point_of_sale.Registries");
+  const NumberBuffer = require("point_of_sale.NumberBuffer");
 
   const BinauralPaymentScreen = (PaymentScreen) =>
     class BinauralPaymentScreen extends PaymentScreen {
       _updateSelectedPaymentline() {
-        if (!this.selectedPaymentLine) return; // do nothing if no selected payment line
+        // do nothing if there is not a selected payment line
+        if (!this.selectedPaymentLine) return;
 
         if (!this.selectedPaymentLine.payment_method.is_foreign_currency) {
-          let res = super._updateSelectedPaymentline()
-          if(!!this.selectedPaymentLine){
-            this.selectedPaymentLine
-              .set_foreign_amount(NumberBuffer.getFloat() * this.env.pos.config.foreign_rate)
+          let res = super._updateSelectedPaymentline();
+          if (!!this.selectedPaymentLine) {
+            this.selectedPaymentLine.set_foreign_amount(
+              NumberBuffer.getFloat() * this.env.pos.config.foreign_rate
+            );
           }
-          return res
+          return res;
         }
 
         if (NumberBuffer.get() === null) {
-          this.deletePaymentLine({ detail: { cid: this.selectedPaymentLine.cid } });
+          this.deletePaymentLine({
+            detail: { cid: this.selectedPaymentLine.cid },
+          });
         } else {
-          this.selectedPaymentLine.set_foreign_amount(NumberBuffer.getFloat())
+          this.selectedPaymentLine.set_foreign_amount(NumberBuffer.getFloat());
           this.selectedPaymentLine.set_amount(
-            NumberBuffer.getFloat() * this.env.pos.foreign_currency["inverse_rate"]
+            NumberBuffer.getFloat() *
+              this.env.pos.foreign_currency["inverse_rate"]
           );
         }
       }
       toggleIsToInvoice() {
         // click_invoice
-        this.currentOrder.toggle_receipt_invoice(!this.currentOrder.is_to_receipt());
+        this.currentOrder.toggle_receipt_invoice(
+          !this.currentOrder.is_to_receipt()
+        );
         this.render(true);
       }
-    }
+      get isChangeZero() {
+				console.log(this.currentOrder.get_change());
+        return this.currentOrder.get_change() === 0;
+      }
+    };
 
-  Registries.Component.extend(PaymentScreen, BinauralPaymentScreen)
-  return PaymentScreen
-})
+  Registries.Component.extend(PaymentScreen, BinauralPaymentScreen);
+  return PaymentScreen;
+});
