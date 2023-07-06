440bc6f21ae180041b82ee85192cc7e55b829f94
Miguel Gozaine <miguel@binauraldev.com>
2023-07-06 19:37:27 -0400

    fix: #3546 Validación de retenciones con monto mayor al adeudado

--- integra/binaural_payment_extension/models/account_retention_line.py
+++ integra/binaural_payment_extension/models/account_retention_line.py
@@ -373,7 +373,13 @@ class AccountRetentionLine(models.Model):
             ):
                 raise ValidationError(_("You can not create a retention with 0 amount."))
 
-            if record.retention_amount > record.move_id.amount_residual:
+            is_vef_the_base_currency = self.env.company.currency_id == self.env.ref("base.VEF")
+            is_client_retention = record.retention_id.type == "out_invoice"
+            if (
+                is_vef_the_base_currency
+                and is_client_retention
+                and record.retention_amount > record.move_id.amount_residual
+            ):
                 raise ValidationError(
                     _(
                         "The total amount of the retention is greater than the residual amount of"
