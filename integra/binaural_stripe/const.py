6e53c89c38dc16fa3720e255a73307f27cef72a6
Manuel Guerrero <manuel.guerrero@binauraldev.com>
2023-07-09 19:05:53 -0400

    wip: modulo para permitir usar stripe en vzla

---
+++ integra/binaural_stripe/const.py
@@ -0,0 +1,52 @@
+# The countries supported by Stripe. See https://stripe.com/global page.
+from odoo.addons.payment_stripe.const import SUPPORTED_COUNTRIES
+# SUPPORTED_COUNTRIES = {
+#     'AE',
+#     'AT',
+#     'AU',
+#     'BE',
+#     'BG',
+#     'BR',
+#     'CA',
+#     'CH',
+#     'CY',
+#     'CZ',
+#     'DE',
+#     'DK',
+#     'EE',
+#     'ES',
+#     'FI',
+#     'FR',
+#     'GB',
+#     'GI',  # Beta
+#     'GR',
+#     'HK',
+#     'HR',  # Beta
+#     'HU',
+#     'ID',  # Beta
+#     'IE',
+#     'IT',
+#     'JP',
+#     'LI',  # Beta
+#     'LT',
+#     'LU',
+#     'LV',
+#     'MT',
+#     'MX',
+#     'MY',
+#     'NL',
+#     'NO',
+#     'NZ',
+#     'PH',  # Beta
+#     'PL',
+#     'PT',
+#     'RO',
+#     'SE',
+#     'SG',
+#     'SI',
+#     'SK',
+#     'TH',  # Beta
+#     'US',
+#     'VE'
+# }
+SUPPORTED_COUNTRIES.add('VE')
