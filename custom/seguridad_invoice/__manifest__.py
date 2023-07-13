6ff867ab19272da9a63f0b1f97c37b74564e28c5
yeison <yeison@binauraldev.com>
2023-07-13 16:53:14 -0400

    -Adaptar Formatos de Factura y Pedido(#3586) -Creacion de modulo para agrgar modulo con perzonalizacion de factura -https://binaural.odoo.com/web#id=3586&cids=2&model=project.task&view_type=form

---
+++ custom/seguridad_invoice/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Seguridad Invoice",
+    "summary": """
+       Modulo para personalizar formato de factura en seguridad""",
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://binauraldev.com/",
+    "category": "Account/Account",
+    "version": "16.0",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        'data/paperformat.xml',
+        'reports/account_report.xml',
+       
+    ],
+    "images": ["static/description/icon.png"],
+    "application": True,
+}
