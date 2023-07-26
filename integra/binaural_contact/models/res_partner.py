bd4e2d523d41ca7e289415a8ac873726d4e765a0
Anderson Armeya <anderson@binaural.com.ve>
2023-07-26 10:59:13 -0400

    FIX #3589: Busqueda de CNE
    Se agrego para la busqueda en cuanto cambien el prefix_vat (tipo de
    documento)

--- integra/binaural_contact/models/res_partner.py
+++ integra/binaural_contact/models/res_partner.py
@@ -56,7 +56,7 @@ class ResPartner(models.Model):
                 if not re.match(pattern, record.vat):
                     raise MissingError(_("The vat field only accepts numbers"))
 
-    @api.onchange("vat")
+    @api.onchange("vat","prefix_vat")
     def _onchange_(self):
         """This function assign the name of the person by the vat number and the prefix of the vat number
         calling the function get_default_name_by_vat from binaural_cne_query
