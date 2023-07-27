6d4279a830cdfb2e03cef50fa1df5628ca88af81
Miguel Gozaine <96434156+miguel-binaural@users.noreply.github.com>
2023-07-27 14:18:31 -0400

    Update res_config_settings.py

--- integra/binaural_seller/models/res_config_settings.py
+++ integra/binaural_seller/models/res_config_settings.py
@@ -5,13 +5,6 @@ from odoo.exceptions import UserError
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    company_id = fields.Many2one(
-        "res.company",
-        string="Company",
-        required=True,
-        default=lambda self: self.env.company,
-    )
-
     initial_seller = fields.Many2one(
         string="Default seller when creating contact",
         related="company_id.initial_seller",
