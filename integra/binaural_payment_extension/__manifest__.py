5f3c9e0ed18e7de77bdbcdd967f27700f72a5a39
Miguel Gozaine <miguel@binauraldev.com>
2023-07-29 13:57:50 -0400

    Update manifest #3590

--- integra/binaural_payment_extension/__manifest__.py
+++ integra/binaural_payment_extension/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "16.11",
+    "version": "16.13",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
