f8578d05dd2b01b926b4a7d75458e069c51af943
Anderson Armeya <anderson@binaural.com.ve>
2023-08-02 13:39:40 -0400

    FIX #4069: Monto de producto con impuesto
    Se cambio la configuracion en produccion y ahora muestra el monto del
    producto BI + IVA

--- custom/libreria_reports/__manifest__.py
+++ custom/libreria_reports/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "16.0.0.0.3",
+    "version": "16.0.0.0.4",
     "depends": ["product","binaural_rate"],
     "application": True,
     "data": [
