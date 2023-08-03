879f528a912f1723dfda797e037f329903d1da96
yeisonasuaje <yeison@binaural.com.ve>
2023-08-03 12:45:44 -0400

    -Permitir seleccionar si la nota de entrega será impresa en $ o BS.(#2338) -Se creo grupo para impresiones de nota de entrega y factura en dolares, se creo boton para imprimir facturra o nota de entrega en dolares -https://binaural.odoo.com/web#id=2338&cids=2&menu_id=255&action=327&active_id=18&model=project.task&view_type=form

--- custom/invoice_zmart/models/account_move.py
+++ custom/invoice_zmart/models/account_move.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
@@ -7,28 +7,14 @@ class AccountInvoice(models.Model):
     
     def button_free_form(self):
         self.write({'printed': True})
-        return self.env.ref('binaural_invoice.action_invoice_free_form_binaural_invoice').report_action(self)
+        return self.env.ref('invoice_zmart.action_invoice_free_form_bs').report_action(self)
     
     def button_free_form_usd(self):
         self.write({'printed': True})
-        return self.env.ref('binaural_invoice.action_invoice_free_form_binaural_invoice').report_action(self)
+        return self.env.ref('invoice_zmart.action_invoice_free_form_usd').report_action(self)
     
     def button_invoice_sale_note(self):
-        return self.env.ref('binaural_invoice.action_invoice_sale_note_binaural_invoice').report_action(self)
+        return self.env.ref('invoice_zmart.action_invoice_sale_note_usd').report_action(self)
     
     def button_invoice_sale_note_bs(self):
         return self.env.ref('invoice_zmart.action_invoice_sale_note_bs').report_action(self)
-    
-    def button_invoice_sale_note_rma(self):
-        return self.env.ref('invoice_zmart.action_invoice_sale_note_rma').report_action(self)
-    
-    
-    class AccountInvoiceLine(models.Model):
-        _inherit = 'account.move.line'
-
-        location_id = fields.Many2one('stock.location', 'Location', store=False, default='_default_location_id')
-
-        def _default_location_id(self):
-            company = self.env.company
-            default_location_id = company.default_location_id
-            return default_location_id
