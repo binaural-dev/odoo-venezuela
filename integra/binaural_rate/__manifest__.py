ff3e14d833af5a1a317fd58851d7f1550ffe8fd6
Jose <jose.acosta@binaural.com.ve>
2023-08-04 12:08:29 -0400

    FIX: #1321 (ticket) configuracion por defecto para actualizar la tasa

--- integra/binaural_rate/__manifest__.py
+++ integra/binaural_rate/__manifest__.py
@@ -7,11 +7,12 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.1",
+    "version": "16.0.0.0.2",
     # any module necessary for this one to work correctly
     "depends": ["base", "currency_rate_live"],
     # always loaded
     "data": [
+        "data/update_currency_data.xml",
         "views/res_config_settings.xml",
     ],
 }
