a7aef3a70a04b78abd73ab976b641ede455fa827
bryanbinaural <bryan@binaural.com.ve>
2023-08-07 16:47:31 -0400

    FIX: Corregir IGTF en Pagos de NC de Clientes Ticket#1323

--- integra/binaural_igtf/wizard/account_payment_register.py
+++ integra/binaural_igtf/wizard/account_payment_register.py
@@ -1,4 +1,6 @@
 from odoo import api, models, fields, _
+import logging
+_logger = logging.getLogger(__name__)
 
 class AccountPaymentRegisterIgtf(models.TransientModel):
     _inherit = "account.payment.register"
