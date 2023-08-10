d2ba9f3e0ef9373bea7d7f6d39d378e426ff4adb
yeisonasuaje <yeison@binaural.com.ve>
2023-08-10 16:46:21 -0400

    -Modificar Etiqueta(#4113) -Modificaciones de campos en el formato de la etiqueta -https://binaural.odoo.com/web#id=4113&cids=2&model=project.task&view_type=form

--- custom/sale_zmart/__manifest__.py
+++ custom/sale_zmart/__manifest__.py
@@ -20,6 +20,7 @@
         "report/stock_report_view.xml",
         "views/sale_order.xml",
         "views/stock_picking.xml",
+        "views/res_company.xml"
     ],
     "images": ["static/description/icon.png"],
     "application": True,
