290ee44ec37a541b04e8109b6e5f8425bf49ba53
Miguel Gozaine <hordemzerado@gmail.com>
2023-08-13 19:05:57 -0400

    fix: Readonly false en campo de configuración

--- integra/binaural_website_sale/models/res_config_settings.py
+++ integra/binaural_website_sale/models/res_config_settings.py
@@ -5,5 +5,6 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     do_not_show_products_without_availability_on_site = fields.Boolean(
-        related="website_id.do_not_show_products_without_availability_on_site"
+        related="website_id.do_not_show_products_without_availability_on_site",
+        readonly=False,
     )
