a54ee2cf869d64293b4768c2e47f7c198866cd3f
Anderson Armeya <anderson@binaural.com.ve>
2023-08-14 15:50:08 -0400

    FIX #4158: Ajuste de IGTF
    En el caso de
    Factura: 100$
    
    Y se paga de la siguiente manera:
    
    102$
    1$
    --------
    BI IGTF: 100$
    IGTF: 3$
    ----
    TOTAL: 103$
    
    El igtf no se colocaba a ningun metodo de pago porque este debe estar
    pagado completamente en uno solo.
    
    Actualmente el igtf se resta al pago de 102$ por lo que el pago en PDV
    queda por 102$ mas en el asiento queda
    
    CUENTA        Debe | Haber
    CXC                |   99$
    Igtf               |    3$
    CXC (POS)      102$|

--- integra/binaural_pos_igtf/__manifest__.py
+++ integra/binaural_pos_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "16.0.3",
+    "version": "16.0.4",
     "depends": ["base", "binaural_pos", "binaural_base_igtf"],
     "data": ["views/pos_payment_method.xml"],
     "images": ["static/description/icon.png"],
