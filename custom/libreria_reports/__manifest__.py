9166ad73966f8f2c6b160ef0d783321ed8cb8c7a
Jose <jose.acosta@binaural.com.ve>
2023-08-29 16:28:56 -0400

    FIX: #1585 (ticket) etiqueta de producto
    Se movio hacia la derecha la informacion de
    la etiqueta
    
    **Modulos Afectados**
    
    libreria_reports

--- custom/libreria_reports/__manifest__.py
+++ custom/libreria_reports/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "16.0.0.0.6",
+    "version": "16.0.0.0.7",
     "depends": ["product","binaural_rate"],
     "application": True,
     "data": [
