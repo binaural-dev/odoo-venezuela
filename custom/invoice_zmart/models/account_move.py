cf440dcbca0fafd397381db1eb23ce642f4f08fb
yeisonasuaje <yeison@binaural.com.ve>
2023-08-30 15:42:45 -0400

    -Correciones al formato nota de entrega(#4322) -Se heredo la funcion action_post para que al confirmar una factura con un diario no fiscal rescriba los impuestos en la lineas de producto -https://binaural.odoo.com/web#id=4322&cids=2&model=project.task&view_type=form

--- custom/invoice_zmart/models/account_move.py
+++ custom/invoice_zmart/models/account_move.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api,_
 from odoo.exceptions import ValidationError
 
+
 class AccountInvoice(models.Model):
     _inherit = "account.move"
 
@@ -23,7 +24,6 @@ class AccountInvoice(models.Model):
             return self.env.ref("invoice_zmart.action_invoice_free_form_bs").report_action(self)
         raise ValidationError(_( 'Cannot print an invoice with a non-fiscal journal'))
 
-
     def button_free_form_usd(self):
         if self.journal_id.fiscal:
             self.write({"printed": True})
@@ -39,3 +39,12 @@ class AccountInvoice(models.Model):
         if not self.journal_id.fiscal:
             return self.env.ref("invoice_zmart.action_invoice_sale_note_bs").report_action(self)
         raise ValidationError(_( 'Cannot print an sale note with a fiscal journal'))
+    
+    def action_post(self):
+        res = super().action_post()
+        if not self.journal_id.fiscal:
+            self.state =  'draft'
+            for line in self.invoice_line_ids:
+                line.tax_ids = False
+            self.state =  'posted'
+        return res
