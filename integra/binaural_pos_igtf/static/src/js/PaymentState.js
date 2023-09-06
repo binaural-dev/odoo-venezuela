cd2921b511d057ef0bff2d32a2f34c6c5fe89494
Anderson Armeya <anderson@binaural.com.ve>
2023-09-06 16:45:40 -0400

    FIX TICKET#1648: binaural_pos_igtf
    Se cambio la estructura de generar el apunte de igtf
    
    Ahora el igtf se genera unicamente en el pago en $, caso de que el pago
    sea por partes este se ira el igtf correspondiente a cada pago
    
    Se soluciono al momento de intercambiar entre Factura y recibo actualice
    el igtf e inclusive cuando se recarga la pagina
    
    Se agrego el monto del igtf a cobrar en el metodo de pago en $ mostrando
    asi ej
    
    Dolares 100$
    Igtf = 3$
    
    Se coloco en el ajuste de credito y debito alterno el monto cobrado
    exactamente desde el POS, y asi este no haga el recalculo

--- integra/binaural_pos_igtf/static/src/js/PaymentState.js
+++ integra/binaural_pos_igtf/static/src/js/PaymentState.js
@@ -10,17 +10,29 @@ odoo.define("binaural_pos_igtf.PaymentState", function(require) {
       constructor() {
         super(...arguments)
         this.include_igtf = this.include_igtf || false
+        this.igtf_amount = this.igtf_amount || 0
+        this.foreign_igtf_amount = this.foreign_igtf_amount || 0
       }
       set_include_igtf(value) {
         this.include_igtf = value
       }
+      set_igtf_amount(amount){
+        this.igtf_amount = amount
+      }
+      set_foreign_igtf_amount(amount){
+        this.foreign_igtf_amount = amount
+      }
       init_from_JSON(json) {
         super.init_from_JSON(...arguments);
         this.include_igtf = json.include_igtf || false;
+        this.igtf_amount = json.igtf_amount || 0;
+        this.foreign_igtf_amount = json.foreign_igtf_amount || 0;
       }
       export_as_JSON() {
         let res = super.export_as_JSON();
         res["include_igtf"] = this.include_igtf;
+        res["igtf_amount"] = this.igtf_amount;
+        res["foreign_igtf_amount"] = this.foreign_igtf_amount;
         return res
       }
     };
