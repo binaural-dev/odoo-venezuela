ea1b736133cd7730f544643544df74521421d2b8
Anderson Armeya <anderson@binaural.com.ve>
2023-09-20 08:49:28 -0400

    FEAT #5187: binaural_stock_barcode
    Carritos multicompañia

--- integra/binaural_stock_barcode/models/stock_picking_cart.py
+++ integra/binaural_stock_barcode/models/stock_picking_cart.py
@@ -1,19 +1,28 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 import random
 
 
 class StockPickingCart(models.Model):
     _name = "stock.picking.cart"
+    _check_company_auto = True
+    _description = "Stock Picking Cart"
 
     def _default_barcode(self):
         return self.generate_random_barcode()
 
     name = fields.Char(string="Description", required=True)
     barcode = fields.Char(string="Barcode", required=True, default=_default_barcode)
+    company_id = fields.Many2one(default=lambda self: self.env.company.id)
 
     pick_id = fields.Many2one("stock.picking", string="PICK")
     pack_id = fields.Many2one("stock.picking", string="PACK")
     out_id = fields.Many2one("stock.picking", string="OUT")
+
+    @api.onchange("barcode")
+    def _onchange_barcode(self):
+        if self.barcode and self.env["stock.picking.cart"].search([("barcode", "=", self.barcode)]):
+            raise ValidationError("Barcode already exists")
 
     def generate_random_barcode(self):
         pattern = self.env.ref("binaural_stock_barcode.picking_cart_rule").pattern
