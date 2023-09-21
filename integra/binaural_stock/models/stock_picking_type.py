50f59020386f1e9eaf9cff1378b3123952082df3
Anderson Armeya <anderson@binaural.com.ve>
2023-09-21 15:23:58 -0400

    FEAT #5183: binaural_stock, binaural_stock_barcode
    Ajustando en Wizard de Configuracion mostrar el tipo de operacion
    (PICK,PACK,OUT)

--- integra/binaural_stock/models/stock_picking_type.py
+++ integra/binaural_stock/models/stock_picking_type.py
@@ -1,8 +1,25 @@
-from odoo import _, models
+from odoo import _, fields, models
 
 
 class StockPickingType(models.Model):
     _inherit = "stock.picking.type"
+
+    type_steps = fields.Selection(
+        [
+            ("in", "IN"),
+            ("out", "OUT"),
+            ("int", "INT"),
+            ("pick", "PICK"),
+            ("pack", "PACK"),
+        ],
+        string="Type Steps",
+        compute="_compute_type_steps",
+        store=True,
+    )
+
+    def _compute_type_steps(self):
+        for record in self:
+            record.type_steps = record._get_type_steps()
 
     def _get_type_steps(self):
         if self.warehouse_id.in_type_id.id == self.id:
