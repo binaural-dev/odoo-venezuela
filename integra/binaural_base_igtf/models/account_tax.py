63981b53455d499dba31bf9baea8ff721400b4dc
Anderson Armeya <anderson@binaural.com.ve>
2023-09-29 15:03:08 -0400

    FEAT #6413: binaural_base_igtf
    * Se agrego configuracion para mostrar el igtf sugerido, en caso de que
      este tenga un pago con igtf no muestra
    
    * Creacion de README

--- integra/binaural_base_igtf/models/account_tax.py
+++ integra/binaural_base_igtf/models/account_tax.py
@@ -1,3 +1,4 @@
+from operator import is_
 from odoo import api, models, _
 from odoo.tools.misc import formatLang
 from odoo.tools.float_utils import float_round, float_is_zero
@@ -36,17 +37,34 @@ class AccountTax(models.Model):
 
         apply_igtf = False
         invoice = self.env["account.move"]
+        type_model = ""
+        base_igtf = 0
+        is_igtf_suggested = False
 
         for base_line in base_lines:
+            type_model = base_line["record"]._name
             if base_line["record"]._name == "account.move.line":
                 invoice = base_line["record"].move_id
 
         foreign_currency = self.env.company.currency_foreign_id
         rate = invoice.foreign_inverse_rate
-        float_igtf_percentage = self.env.company.igtf_percentage if not invoice.is_two_percentage else 2
+        float_igtf_percentage = (
+            self.env.company.igtf_percentage if not invoice.is_two_percentage else 2
+        )
         igtf_percentage = (float_igtf_percentage or 0) / 100
 
-        igtf_base_amount = float_round(invoice.bi_igtf or 0, precision_rounding=currency.rounding)
+        if type_model == "account.move.line" and self.env.company.show_igtf_suggested_account_move:
+            is_igtf_suggested = True
+            base_igtf = res.get("amount_total", 0)
+        if type_model == "sale.order.line" and self.env.company.show_igtf_suggested_sale_order:
+            is_igtf_suggested = True
+            base_igtf = res.get("amount_total", 0)
+
+        if invoice.bi_igtf:
+            is_igtf_suggested = False
+            base_igtf = invoice.bi_igtf
+
+        igtf_base_amount = float_round(base_igtf or 0, precision_rounding=currency.rounding)
 
         if float_is_zero(igtf_base_amount, precision_rounding=currency.rounding) == False:
             apply_igtf = True
@@ -97,5 +115,6 @@ class AccountTax(models.Model):
         res["formatted_foreign_amount_total_igtf"] = formatLang(
             self.env, res["foreign_amount_total_igtf"], currency_obj=foreign_currency
         )
+        res["igtf"]["is_igtf_suggested"] = is_igtf_suggested
 
         return res
