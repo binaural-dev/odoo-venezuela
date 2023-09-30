e80a78d75f6c0b4d3486732ddd3d856ad09b21da
Anderson Armeya <anderson@binaural.com.ve>
2023-09-30 12:30:28 -0400

    FEAT #6431: binaural_stock
    Agregando Campo Numero de cajas visualizado en la lista de
    transferencias y en las trasferencias de tipo OUT o Expediciones.
    
    Importante: Se espera en el desarrollo de inventario avanzado tener los
    tipos de picking para adaptarlo a esa funcion

---
+++ integra/binaural_stock/models/stock_picking.py
@@ -0,0 +1,14 @@
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    number_of_boxes = fields.Integer(default=0)
+    is_out = fields.Boolean(compute="_compute_is_out")
+
+    def _compute_is_out(self):
+        for record in self:
+            record.is_out = (
+                record.picking_type_id.warehouse_id.out_type_id.id == record.picking_type_id.id
+            )
