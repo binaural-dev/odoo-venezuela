1ee6e5de5d4d69f55b25aef92a30fc6370974216
yeisonasuaje <yeison@binaural.com.ve>
2023-10-01 00:03:24 -0400

    -Migrar Formato Nota de Entrega RMA del módulo de ventas al módulo RMA(#6418) -Ajustre aumentop de tamano de letra en forma libre bs y $ -Ajuste de linea en nota de netrega $ y bs -Se creo boton para imprimr reporte rma desde el picking -Validacion de boton para imprimir reporte rma -https://binaural.odoo.com/web#model=project.task&id=6418&active_id=6418&cids=2

--- custom/invoice_zmart/__manifest__.py
+++ custom/invoice_zmart/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Binauraldev",
     'website': "https://binauraldev.com/",
     'category': 'Invoice',
-    'version': '16.0.0.7',
+    'version': '16.0.0.8',
     'depends': ['binaural_invoice'],
     'data': [
         'data/paperformat.xml',
--- custom/report_rma_zmart/models/__init__.py
+++ custom/report_rma_zmart/models/__init__.py
@@ -1,1 +1,3 @@
 from . import rma
+from . import rma_delivery
+from . import stock_picking
