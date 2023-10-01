7b1cfd4417a42f8a0d36e3d68b567f9fe2ebc435
yeisonasuaje <yeison@binaural.com.ve>
2023-10-01 15:22:15 -0400

    -Migrar Formato Nota de Entrega RMA del módulo de ventas al módulo RMA -Ajuste de dimesiones de la etiqueta -https://binaural.odoo.com/web#id=6418&cids=2&model=project.task&view_type=form

--- custom/sale_zmart/__manifest__.py
+++ custom/sale_zmart/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sale/Sale",
-    "version": "16.0.0.5",
+    "version": "16.0.0.6",
     "depends": [
         "sale","invoice_zmart"
     ],
