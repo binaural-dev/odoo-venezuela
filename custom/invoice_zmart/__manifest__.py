e4d8a2657fcbe96d5822434575c3f4f94e91b813
yeisonasuaje <yeison@binaural.com.ve>
2023-10-04 10:31:54 -0400

    -Ajustar Notas de Entrega(#6469) -Se bajo colocacion de montos -https://binaural.odoo.com/web#model=project.task&id=6469&active_id=6469&cids=2

--- custom/invoice_zmart/__manifest__.py
+++ custom/invoice_zmart/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Binauraldev",
     'website': "https://binauraldev.com/",
     'category': 'Invoice',
-    'version': '16.0.0.17',
+    'version': '16.0.0.18',
     'depends': ['binaural_invoice', 'binaural_base_igtf'],
     'data': [
         'data/paperformat.xml',
