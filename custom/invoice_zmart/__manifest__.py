61ff3235b4dc908bee919bb08d7cedcec7ecdd32
yeisonasuaje <yeison@binaural.com.ve>
2023-10-09 14:57:57 -0400

    -Ajuste de IGTF en Forma libre en Bs(#6473) -Se quito el atributo que muestra las letras en minuscula -se actualizo manifest -https://binaural.odoo.com/web#id=6473&cids=2&model=project.task&view_type=form

--- custom/invoice_zmart/__manifest__.py
+++ custom/invoice_zmart/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Binauraldev",
     'website': "https://binauraldev.com/",
     'category': 'Invoice',
-    'version': '16.0.0.21',
+    'version': '16.0.0.23',
     'depends': ['binaural_invoice', 'binaural_base_igtf'],
     'data': [
         'data/paperformat.xml',
