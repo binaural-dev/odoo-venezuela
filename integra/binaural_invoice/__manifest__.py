3e543321d622da415e8f9d68556cdded7c6ea7d8
yeisonasuaje <yeison@binaural.com.ve>
2023-10-11 14:19:04 -0400

    Atributo no update quitado para probar se deja de fallar la data en staging actuaizacion de manifest

--- integra/binaural_invoice/__manifest__.py
+++ integra/binaural_invoice/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Binaural Facturación",
     "summary": """
        Modulo para contabilidad Venezolana """,
-    "version": "16.0.0.0.13",
+    "version": "16.0.0.0.14",
     "license": "LGPL-3",
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
