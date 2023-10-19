7dbe9921a26d32d92785119348d61c8f98451d67
Anderson Armeya <anderson@binaural.com.ve>
2023-10-19 16:11:30 -0400

    FIX #7245: sale_zmart
    Al momento de actualizar integra se agrego un campo que ya estaba en
    zmart por lo que ahora hereda de Integra

--- custom/sale_zmart/__manifest__.py
+++ custom/sale_zmart/__manifest__.py
@@ -6,22 +6,20 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sale/Sale",
-    "version": "16.0.0.6",
-    "depends": [
-        "sale","invoice_zmart"
-    ],
+    "version": "16.0.0.7",
+    "depends": ["sale", "invoice_zmart", "binaural_stock"],
     "data": [
-        'data/res_group.xml',
+        "data/res_group.xml",
         "data/paperformat.xml",
         "data/mail_templates.xml",
         "data/ir_cron.xml",
         "report/delivery_note_rma.xml",
         "report/report_sale.xml",
         "report/stock_report_view.xml",
-        'report/sale_order_note_usd.xml',
+        "report/sale_order_note_usd.xml",
         "views/sale_order.xml",
         "views/stock_picking.xml",
-        "views/res_company.xml"
+        "views/res_company.xml",
     ],
     "images": ["static/description/icon.png"],
     "application": True,
