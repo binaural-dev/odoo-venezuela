09242a9730078945725b8d944b0f7a0ca29e65ef
Anderson Armeya <anderson@binaural.com.ve>
2023-10-28 10:45:47 -0400

    Manifest

--- integra/binaural_sale/__manifest__.py
+++ integra/binaural_sale/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sales/Sales",
-    "version": "16.7.3",
+    "version": "16.7.4",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
