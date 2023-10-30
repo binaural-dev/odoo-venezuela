0e0d0a76109339ac9c662053c6c8a769f4fb6737
Anderson Armeya <anderson@binaural.com.ve>
2023-10-30 10:10:30 -0400

    FIX #7646: sale_zmart
    En los usuarios que no tenian acceso de Ventas no podian obtener el
    numero ni el tipo de la factura

--- custom/sale_zmart/__manifest__.py
+++ custom/sale_zmart/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sale/Sale",
-    "version": "16.0.0.10",
+    "version": "16.0.0.11",
     "depends": [
         "binaural_sale",
         "invoice_zmart",
