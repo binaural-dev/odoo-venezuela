789ed3222a212e0a57fae1a8706e54341feb037a
ChristopherBinaural <christopher@binaural.com.ve>
2023-10-30 15:21:24 -0400

    WIP: odoo_7890_tag_dispatch_guide
    *increased version.

--- custom/sale_zmart/__manifest__.py
+++ custom/sale_zmart/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sale/Sale",
-    "version": "16.0.0.11",
+    "version": "16.0.0.12",
     "depends": [
         "binaural_sale",
         "invoice_zmart",
