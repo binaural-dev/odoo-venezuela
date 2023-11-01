88fbbbde90b5e65520a6291ba059536e93933296
ChristopherBinaural <christopher@binaural.com.ve>
2023-11-01 16:37:22 -0400

    WIP: odoo_7649_add_number_account_move_tag_report
    * Version up.
    * get invoice names and concat witch ,

--- custom/sale_zmart/__manifest__.py
+++ custom/sale_zmart/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sale/Sale",
-    "version": "16.0.0.16",
+    "version": "16.0.0.17",
     "depends": [
         "binaural_sale",
         "invoice_zmart",
