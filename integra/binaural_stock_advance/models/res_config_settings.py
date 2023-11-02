2bf3750f106be08e39d4a95149ea9dedfb4af795
bryanbinaural <bryan@binaural.com.ve>
2023-11-02 09:12:22 -0400

    Actualizar ultimo Costo (CIF+Aranceles) #7631

--- integra/binaural_stock_advance/models/res_config_settings.py
+++ integra/binaural_stock_advance/models/res_config_settings.py
@@ -6,3 +6,8 @@ class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     check_advance_stock = fields.Boolean(related='company_id.check_advance_stock', readonly=False)
+    use_fee_percentage = fields.Boolean(related='company_id.use_fee_percentage', readonly=False)
+    apply_fob = fields.Boolean(related='company_id.apply_fob', readonly=False)
+    apply_cif = fields.Boolean(related='company_id.apply_cif', readonly=False)
+    service_products_ids = fields.Many2many(related='company_id.service_products_ids', readonly=False)
+    
