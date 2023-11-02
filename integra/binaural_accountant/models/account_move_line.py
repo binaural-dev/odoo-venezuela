e2f78272434c210793d832b62a8b0c4660f430ae
Miguel Gozaine <miguel@binauraldev.com>
2023-11-02 14:42:25 -0400

    fix: Método inveres_amount_currency repetido

--- integra/binaural_accountant/models/account_move_line.py
+++ integra/binaural_accountant/models/account_move_line.py
@@ -71,7 +71,7 @@ class AccountMoveLine(models.Model):
                 and not self.env.is_protected(self._fields["balance"], line)
             ):
                 line.balance = line.company_id.currency_id.round(
-                    line.amount_currency / line.currency_rate
+                    line.amount_currency / line.foreign_rate
                 )
             elif (
                 line.currency_id != line.company_id.currency_id
@@ -123,23 +123,6 @@ class AccountMoveLine(models.Model):
                 line.amount_currency = line.currency_id.round(line.balance * line.foreign_rate)
             if line.currency_id == line.company_id.currency_id:
                 line.amount_currency = line.balance
-
-    @api.onchange("amount_currency", "currency_id")
-    def _inverse_amount_currency(self):
-        for line in self:
-            if (
-                line.currency_id == line.company_id.currency_id
-                and line.balance != line.amount_currency
-            ):
-                line.balance = line.amount_currency
-            elif (
-                line.currency_id != line.company_id.currency_id
-                and not line.move_id.is_invoice(True)
-                and not self.env.is_protected(self._fields["balance"], line)
-            ):
-                line.balance = line.company_id.currency_id.round(
-                    line.amount_currency / line.foreign_rate
-                )
 
     def _prepare_analytic_distribution_line(
         self, distribution, account_id, distribution_on_each_plan
