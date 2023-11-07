fa30614b775814105f5d497c7c318bcf2dedb1cf
ChristopherBinaural <christopher@binaural.com.ve>
2023-11-07 13:46:55 -0400

    WIP: fix_staging_odoo_8964_set_required_field_to_create_contacts
    * version up.
    
    * no_quick_create to partner_id on account.move, stock.picking and sale.order.

--- custom/sale_zmart/__manifest__.py
+++ custom/sale_zmart/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sale/Sale",
-    "version": "16.0.0.20",
+    "version": "16.0.0.21",
     "depends": [
         "binaural_sale",
         "invoice_zmart",
