a7f3c5afe00411ebb7b78c36d5116ecdd3335d5f
Miguel Gozaine <hordemzerado@gmail.com>
2023-11-08 14:19:41 -0400

    FEAT #9610: binaural_costs_matrix
    Se agregó un módulo que agrega campos de costos y márgenes a la vista de
    normas de precio, pensado para ser utilizado como una matriz de costos.
    
    Tarea: https://binaural.odoo.com/_odoo/upgrade/binaural#id=9610&menu_id=255&cids=2&action=341&model=project.task&view_type=form

---
+++ integra/binaural_costs_matrix/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Matriz de Costos",
+    "summary": """Modulo para la gestion de la matriz de costos""",
+    "author": "Binauraldev",
+    "website": "https://www.binauraldev.com",
+    "category": "Sales",
+    "version": "0.1",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "product",
+        "binaural_last_cost",
+        "binaural_sale",
+        "binaural_brand",
+    ],
+    # always loaded
+    "data": [
+        "views/product_pricelist_item_views.xml",
+    ],
+}
