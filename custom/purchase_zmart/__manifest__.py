3945d0eb90073c22d28b463d968810ae7b9ecafd
ChristopherBinaural <christopher@binaural.com.ve>
2023-11-13 16:32:02 -0400

    WIP: rls_odoo_7921_show_date_eta_port

--- custom/purchase_zmart/__manifest__.py
+++ custom/purchase_zmart/__manifest__.py
@@ -9,6 +9,8 @@
     "version": "16.0.0.1",
     "depends": [
         "binaural_purchase",
+        "stock",
+        "web",
     ],
     "data": [
         "security/ir.model.access.csv",
@@ -18,4 +20,20 @@
     ],
     "images": ["static/description/icon.png"],
     "application": True,
+    "assets": {
+        'web.assets_qweb': [
+            # 'stock/static/src/**/*.js',
+            # 'stock/static/src/**/*.xml',
+            # 'stock/static/src/scss/stock_forecasted.scss',
+            # 'stock/static/src/scss/forecast_widget.scss',
+            # 'stock/static/src/scss/stock_empty_screen.scss',
+            # 'stock/static/src/views/**/*',
+            "purchase_zmart/static/src/stock_forecasted/forecasted_details.xml",
+            # (
+            #     "replace",
+            #     "stock/static/src/stock_forecasted/forecasted_details.xml",
+            #     "purchase_zmart/static/src/stock_forecasted/forecasted_details.xml",
+            # ),
+        ],
+    },
 }
