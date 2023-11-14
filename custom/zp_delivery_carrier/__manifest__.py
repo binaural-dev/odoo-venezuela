7590eafceb2783467c56cf1bda1569c02dc4e60d
ChristopherBinaural <christopher@binaural.com.ve>
2023-11-14 17:17:23 -0400

    TAG #7898: zp_delivery_carrier
    Problema:
    
    Odoo#7898 Ajustes en Métodos de Envío.
    
    El costo del método de envió seleccionado no se esta calculando correctamente.
    
    Solución:
    
    Usando el peso incluyendo el 10% para obtener el peso que excede la condicion de la regla de metodo de envio.
    
    Version aumentada a 16.0.0.0.4
    
    Tarea:
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- custom/zp_delivery_carrier/__manifest__.py
+++ custom/zp_delivery_carrier/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Delivery / Stock",
-    "version": "16.0.0.0.3",
+    "version": "16.0.0.0.4",
     "depends": ["binaural_rate", "delivery"],
     "data": [
         # 'security/ir.model.access.csv',
