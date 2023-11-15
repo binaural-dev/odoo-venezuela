715b7ff42330de9872ec45423cd3e178abb9fa33
ChristopherBinaural <christopher@binaural.com.ve>
2023-11-15 18:43:05 -0400

    TAG #7921: Mostrar la Fecha ETA Puerto.
    Problema:
    
    Odoo#7921 Mostrar la Fecha ETA Puerto
    
    El campo de llega a puerto ETA se muestra en ingles.
    
    Solución:
    
    Actualice el archivo de traducciones.
    
    Version aumentada a 16.0.0.4
    
    Tarea:
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- custom/purchase_zmart/__manifest__.py
+++ custom/purchase_zmart/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Purchase/Purchase",
-    "version": "16.0.0.3",
+    "version": "16.0.0.4",
     "depends": [
         "binaural_purchase",
         "stock",
