20c33a282410a96112b734ff5f7303c6780e22e8
ChristopherBinaural <christopher@binaural.com.ve>
2023-11-15 13:35:27 -0400

    TAG #7898: zp_delivery_carrier
    Problema:
    
    Odoo#7898 Ajustes en Métodos de Envío.
    
    Los gramos no los estaba considerando un kilo.
    
    Solución:
    
    Por cada gramo extra se redondea a un kilo.
    
    Version aumentada a 16.0.0.0.5
    
    Tarea:
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- custom/zp_delivery_carrier/__manifest__.py
+++ custom/zp_delivery_carrier/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Delivery / Stock",
-    "version": "16.0.0.0.4",
+    "version": "16.0.0.0.5",
     "depends": ["binaural_rate", "delivery"],
     "data": [
         # 'security/ir.model.access.csv',
