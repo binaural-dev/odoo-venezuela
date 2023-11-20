8eb8c33982785bbdd7972cfe9bfe717e060b4f12
Carlos-Lin-Binaural <carlos@binauraldev.com>
2023-11-20 10:52:56 -0400

    FIX-Ticket#2404-sh_product_catalog_generator
    Problema: Al exportar el catalogo en Excel, este salia un error en Codigo
    
    Solucion: Se actualizo el Modulo de Terceros
    
    Ticket: https://binaural.odoo.com/web?debug=1#id=2404&menu_id=293&cids=2&action=387&model=helpdesk.ticket&view_type=form

--- third-party/sh_product_catalog_generator/models/default_before_pages.py
+++ third-party/sh_product_catalog_generator/models/default_before_pages.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) Softhealer Technologies.
 
-from odoo import models, fields
+from odoo import models, fields,api,_
+from odoo.exceptions import UserError, ValidationError
 
 class BeforePages(models.Model):
     _name = 'default.before.pages'
@@ -9,3 +10,13 @@ class BeforePages(models.Model):
 
     name = fields.Char("Title",required=True)
     before_datas = fields.Binary("Page Details (PDF only)", required=True)
+    before_datas_filename = fields.Char()
+
+    @api.constrains('before_datas')
+    def _constrains_reconcile(self):
+        for rec in self:
+            if type(rec.before_datas_filename) != bool:
+                if not str(rec.before_datas_filename).endswith('.pdf'):
+                    raise UserError(_('Please note that only PDF files are accepted. Kindly select a PDF file to proceed.'))
+                return True
+            raise UserError(_('Please note that only PDF files are accepted. Kindly select a PDF file to proceed.'))
