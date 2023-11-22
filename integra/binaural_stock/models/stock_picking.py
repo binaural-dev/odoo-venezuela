47648d85977ce1d179de66a3b7d7a8f249540aaf
Carlos-Lin-Binaural <carlos@binauraldev.com>
2023-11-22 15:19:13 -0400

    FIX-#11290-Binaural_Stock

--- integra/binaural_stock/models/stock_picking.py
+++ integra/binaural_stock/models/stock_picking.py
@@ -5,6 +5,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
+
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
@@ -19,16 +20,23 @@ class StockPicking(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        self.validate_block_transfers_expedition(vals_list)
+        for val in vals_list:
+            self.validate_block_transfers_expedition(val)
         return super().create(vals_list)
-    
+
     def button_validate(self):
         self.validate_block_transfers_expedition()
         return super().button_validate()
 
     def validate_block_transfers_expedition(self, vals=None):
-        block_transfer_expedition = self.env.user.has_group("binaural_stock.group_block_type_inventory_transfers_expeditions")
+        block_transfer_expedition = self.env.user.has_group(
+            "binaural_stock.group_block_type_inventory_transfers_expeditions"
+        )
         if block_transfer_expedition:
-            picking_type = self.env["stock.picking.type"].search([("id", "=", vals[0]["picking_type_id"])]) if vals else self.picking_type_id
+            picking_type = (
+                self.env["stock.picking.type"].search([("id", "=", vals.get("picking_type_id", False))])
+                if vals
+                else self.picking_type_id
+            )
             if picking_type.code == "outgoing":
                 raise UserError(_("You do not have permission to make shipment-type transfers"))
