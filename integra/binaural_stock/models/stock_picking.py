827f9e947d7a3ee6d9ee7e72838c0e4ef478b186
Carlos-Lin-Binaural <carlos@binauraldev.com>
2023-11-22 14:35:35 -0400

    FEAT-#11290-Binaural_Stock
    Problema: los usuarios no tenian limitacion con hacer transferencias de
    inventario tipo expedicion
    
    Solucion: Se agrego un grupo que limitara el crear transferencias de inventario
    tipo expedicion
    
    Tarea: https://binaural.odoo.com/web#id=11290&menu_id=255&cids=2&action=341&model=project.task&view_type=form

--- integra/binaural_stock/models/stock_picking.py
+++ integra/binaural_stock/models/stock_picking.py
@@ -1,5 +1,9 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
@@ -12,3 +16,13 @@ class StockPicking(models.Model):
             record.is_out = (
                 record.picking_type_id.warehouse_id.out_type_id.id == record.picking_type_id.id
             )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        block_transfer_expedition = self.env.user.has_group("binaural_stock.group_block_type_inventory_transfers_expeditions")
+        if block_transfer_expedition:
+            picking_type = self.env["stock.picking.type"].search([("id", "=", vals_list[0]["picking_type_id"])])
+            if picking_type.code == "outgoing":
+                raise UserError(_("You do not have permission to make shipment-type transfers"))
+        return res
