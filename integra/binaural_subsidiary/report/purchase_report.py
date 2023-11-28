0aafd1e74bc9fef37591371ef90ef561aabf20cb
Carlos-Lin-Binaural <carlos@binauraldev.com>
2023-11-28 11:34:57 -0400

    FEAT-#11316-Binaural_subsidiary
    Problema: En las vistas analiticas de compras y ventas no tenian el campo
    de subcursal
    
    Solucion: Se agregaron los campos de subcursales en el reporte analitico
    
    Tarea: https://binaural.odoo.com/web#id=11316&menu_id=255&cids=2&action=341&model=project.task&view_type=form

---
+++ integra/binaural_subsidiary/report/purchase_report.py
@@ -0,0 +1,25 @@
+from odoo import fields, models
+
+class PurchaseReport(models.Model):
+    _inherit = "purchase.report"
+
+    subsidiary = fields.Char()
+
+    def _select(self):
+        res = super()._select()
+        res += """,
+        aac.name AS subsidiary
+        """
+        return res
+
+    def _from(self):
+        res = super()._from()
+        res += "LEFT JOIN account_analytic_account aac ON po.account_analytic_id = aac.id"
+        return res
+
+    def _group_by(self):
+        res = super()._group_by()
+        res += """,
+        aac.name
+        """
+        return res
