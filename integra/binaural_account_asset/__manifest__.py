13ca8a2052d2b9136f924787fc847b29286d99b3
bryanbinaural <bryan@binaural.com.ve>
2023-12-07 15:55:03 -0400

    FIX: binaural_account_asset
    Problema: Singleton.
    
    Solucion: Se recorre en for
    
    Tarea: No hay

--- integra/binaural_account_asset/__manifest__.py
+++ integra/binaural_account_asset/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.2",
+    "version": "1.0",
     # any module necessary for this one to work correctly
     "depends": ["base", "account_asset", "binaural_accountant"],
     # always loaded
