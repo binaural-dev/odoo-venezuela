a2c8658b01aab5c0afcfa2b92cb663493c896cf2
Miguel Gozaine <miguel@binauraldev.com>
2023-12-19 16:51:21 -0400

    fix: Validación de CIU en retenciones con sucursales

--- integra/binaural_subsidiary_payment_extension/models/__init__.py
+++ integra/binaural_subsidiary_payment_extension/models/__init__.py
@@ -2,5 +2,6 @@ from . import account_move_line
 from . import account_retention
 from . import account_retention_line
 from . import analytic_account
+from . import product_template
 from . import res_company
 from . import res_config_settings
