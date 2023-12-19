e13d058ea8dd371cf8abebe4671948b87353b334
Miguel Gozaine <miguel@binauraldev.com>
2023-12-19 16:50:27 -0400

    feat: #12913 Agregar Cuenta de inventario en almacen

--- integra/binaural_subsidiary_stock/models/stock_warehouse.py
+++ integra/binaural_subsidiary_stock/models/stock_warehouse.py
@@ -7,3 +7,11 @@ class StockWarehouse(models.Model):
     subsidiary_id = fields.Many2one(
         "account.analytic.account", string="Subsidiary", domain=[("is_subsidiary", "=", True)]
     )
+    inventory_account_id = fields.Many2one(
+        "account.account",
+        string="Inventory Account",
+        help=(
+            "This account will be used to make the valuation move when there is a transfer between"
+            " subsidiaries of the same company."
+        ),
+    )
