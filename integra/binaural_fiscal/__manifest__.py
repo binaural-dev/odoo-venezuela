33002c98c0039a08d6ca642f0d179cd3bdd763bc
Carlos-Lin-Binaural <carlos@binauraldev.com>
2023-12-21 16:38:00 -0400

    FEAT-#11322-Binaural_Fiscal
    - Se creo el grupo "Ocultar Campo Fiscal (Diario Contable)"
    
    - Se actualizo traduccion y readme
    
    Tarea: https://binaural.odoo.com/web#id=11322&cids=2&model=project.task&view_type=form

--- integra/binaural_fiscal/__manifest__.py
+++ integra/binaural_fiscal/__manifest__.py
@@ -7,11 +7,12 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.2",
+    "version": "16.0.0.0.3",
     # any module necessary for this one to work correctly
     "depends": ["base", "account_accountant"],
     # always loaded
     "data": [
+        "security/binaural_fiscal_groups.xml",
         "views/account_journal.xml",
         "views/account_move.xml",
     ],
