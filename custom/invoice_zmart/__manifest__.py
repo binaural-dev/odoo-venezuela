40ed81bebf2570cf2461d296eb1eb933b472211f
christopher <christopher@binauraldev.com>
2023-12-26 13:55:30 -0400

    TAG #10714: invoice_zmart
    Problema:
    
    Odoo#10714 Generar una Nota de Entrega por Pedido de Venta.
    
    Al generar la Nota de Entrega en dolares se mueve el campo Vendedor y cuando la dirección es larga se sobre escriben la lista de los productos de la segunda pagina.
    
    Solucion:
    
    * Actualice el formato de papel.
    
    Tarea:
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- custom/invoice_zmart/__manifest__.py
+++ custom/invoice_zmart/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Binauraldev",
     'website': "https://binauraldev.com/",
     'category': 'Invoice',
-    'version': '16.0.1.9',
+    'version': '16.0.1.9.0',
     'depends': ['binaural_invoice', 'binaural_base_igtf'],
     'data': [
         'data/paperformat.xml',
