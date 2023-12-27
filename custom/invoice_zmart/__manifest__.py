1a7f6795d2f57656b744c74416d789cb8a91f950
yeisonasuaje <yeison@binaural.com.ve>
2023-12-27 17:07:16 -0400

    -Ticket: # 2619 Ajustar logo en formato de retencion ISLR -Problema: la imagen en el rporte de retenciones se ve muy alargada lateralmente -Solucion: Se heredo el xpath de la imagen y se ajusto su estilo de tamano -https://binaural.odoo.com/web#id=2619&cids=2&model=helpdesk.ticket&view_type=form

--- custom/invoice_zmart/__manifest__.py
+++ custom/invoice_zmart/__manifest__.py
@@ -8,8 +8,8 @@
     'author': "Binauraldev",
     'website': "https://binauraldev.com/",
     'category': 'Invoice',
-    'version': '16.0.1.5',
-    'depends': ['binaural_invoice', 'binaural_base_igtf'],
+    "version": "16.0.1.10.0",
+    'depends': ['binaural_invoice', 'binaural_base_igtf','binaural_payment_extension'],
     'data': [
         'data/paperformat.xml',
         'data/res_group.xml',
@@ -19,6 +19,7 @@
         'report/digital_invoice.xml',
         'report/free_form_bs.xml',
         'report/free_form_usd.xml',
+        'report/retention_voucher_templates.xml',
         'views/account_move.xml',
         'views/account_payment.xml',
         'wizard/wizard_confirm_account_move_view.xml',
