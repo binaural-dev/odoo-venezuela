bbaf26c6f738dd5182851339f5e77f3504cd61ab
Miguel Gozaine <96434156+miguel-binaural@users.noreply.github.com>
2024-01-05 17:17:10 -0400

    feat: Cierre fiscal por sucursales #11282 (#47)

---
+++ integra/binaural_subsidiary_fiscalyear_closing/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Binaural Cierre Fiscal Con Sucursales",
+    "summary": """Crear asientos de cierre fiscal por sucursal""",
+    "author": "Binaural",
+    "website": "https://www.binauraldev.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "category": "Technical",
+    "version": "16.0.1.0.0",
+    # any module necessary for this one to work correctly
+    "depends": ["binaural_account_fiscalyear_closing"],
+    # always loaded
+    "auto_install": True,
+}
