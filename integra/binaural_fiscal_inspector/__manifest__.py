16b75700cb1a3be608b5d5948737fda46d44ab7e
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-01-05 14:35:04 -0400

    FEAT-#13887-Binaural_Fiscal_Inspector
    - Se migro el grupo de inspeccion fiscal a Binaural_Fiscal_Inspector
    
    - Se creo un usuario llamado "Inspector Fiscal" que aplica el grupo de
    inspeccion Fiscal

--- integra/binaural_fiscal_inspector/__manifest__.py
+++ integra/binaural_fiscal_inspector/__manifest__.py
@@ -1,18 +1,21 @@
 {
-    "name": "Binaural Fiscal",
+    "name": "Binaural Inspeccion Fiscal",
     "summary": """
-        Módulo para obtener campo fiscal en el diario
+        Módulo para crear el usuario de inspeccion fiscal
     """,
     "license": "LGPL-3",
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.3",
+    "version": "16.0.0.0.0",
     # any module necessary for this one to work correctly
     "depends": ["base", "account_accountant", "account"],
     # always loaded
     "data": [
-        "views/account_journal.xml",
-        "views/account_move.xml",
+        "security/binaural_fiscal_groups.xml",
+        "security/ir_rule.xml",
+        "security/ir.model.access.csv",
+        "data/res_users.xml",
+        "views/menu_item.xml",
     ],
 }
