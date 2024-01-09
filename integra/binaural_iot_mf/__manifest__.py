a95cd58d199591ad30ce1852eef63096ff6c4125
Anderson Armeya <anderson@binauraldev.com>
2024-01-09 14:45:21 -0400

    FIX #13896: binaural_iot_mf
    Problema: Al tener el parametro website redirije a /es_VE/*

--- integra/binaural_iot_mf/__manifest__.py
+++ integra/binaural_iot_mf/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Binaural IoT - The Factory HKA",
-    "version": "16.0.3.1.0",
+    "version": "16.0.3.2.0",
     "category": "Accounting",
     "summary": "The Factory HKA (SDK) en IoT",
     "license": "LGPL-3",
