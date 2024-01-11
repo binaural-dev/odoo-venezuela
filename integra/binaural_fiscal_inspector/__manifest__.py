103344c1a33a51f606e8e2dfad9bbed0f51b6bb7
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-01-11 15:19:14 -0400

    FIX-#13906-Binaural_Fiscal_Inspector
    - Se corrigio un label de "Estado de resultado"

--- integra/binaural_fiscal_inspector/__manifest__.py
+++ integra/binaural_fiscal_inspector/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.7",
+    "version": "16.0.0.0.8",
     # any module necessary for this one to work correctly
     "depends": ["base", "account_accountant", "account","account_reports","binaural_account_reports"],
     # always loaded
