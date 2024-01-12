eb19516c3dca55d07640ab1956d91b927d88814f
bryanbinaural <bryan@binaural.com.ve>
2024-01-12 12:00:38 -0400

    Fix: se cambia validacion

--- integra/binaural_club_socios/__manifest__.py
+++ integra/binaural_club_socios/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "16.0.0.0.2",
+    "version": "16.0.0.0.3",
     "depends": [
         "base",
         "base_automation",
