a770ce71aa72872d57d26177289bd46ba9fc54aa
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-01-17 11:00:13 -0400

    FIX-#13956-Binaural_website_sale
    Se Ajusto la funcionalidad para mostrar los productos multicompania que
    sea dependiente del dominio del sitio web
    
    Tarea: https://binaural.odoo.com/web#id=13956&cids=2&model=project.task&view_type=form

--- integra/binaural_website_sale/__manifest__.py
+++ integra/binaural_website_sale/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Visualizar solo los productos con cantidad disponible en la tienda",
     "website": "https://binauraldev.com/",
     "license": "LGPL-3",
-    "version": "16.0.0.0.6",
+    "version": "16.0.0.0.7",
     "depends": ["binaural_stock", "website_sale"],
     "data": [
         "views/res_config_settings_views.xml",
