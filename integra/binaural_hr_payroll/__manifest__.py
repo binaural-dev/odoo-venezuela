ffa8df1ace5fcf9f80962d307c36134cc1ed7729
Miguel Gozaine <96434156+miguel-binaural@users.noreply.github.com>
2024-01-24 16:52:54 -0400

    Update __manifest__.py

--- integra/binaural_hr_payroll/__manifest__.py
+++ integra/binaural_hr_payroll/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Payroll Localization",
-    "version": "16.0.1.0.3",
+    "version": "16.0.1.0.4",
     # any module necessary for this one to work correctly
     "depends": ["hr", "hr_payroll", "binaural_rate"],
     # always loaded
