f9c9b2195415508296bed58cb365c1419cdcfdef
Anderson Armeya <anderson@binauraldev.com>
2024-01-24 17:17:29 -0400

    FEAT #14599: binaural_pos_sale
    Problema: El sistema te permite tener un presupuesto en una tarifa y
    guardarse en pos con otra, por lo que se le muestra una alerta en pos
    indicando que la tarifa es diferente a las disponibles de la caja
    
    Tarea:
    
    https://binaural.odoo.com/web#id=14599&cids=2&menu_id=255&action=341&model=project.task&view_type=form

--- integra/binaural_pos_sale/static/src/js/SaleOrderManagementScreen.js
+++ integra/binaural_pos_sale/static/src/js/SaleOrderManagementScreen.js
@@ -17,35 +17,52 @@ odoo.define('binaural_pos_sale.SaleOrderManagementScreen', function(require) {
         });
 
         const sale_lines = await this._getSOLines(sale_order[0].order_line);
+
+        if (!this.env.pos.config.available_pricelist_ids.includes(sale_order[0].pricelist_id[0])) {
+          const { confirmed } = await Gui.showPopup(
+            'ConfirmPopup',
+            {
+              title: this.env._t(`Are you sure to process the order with ${sale_order[0].pricelist_id[0]} since your checkout currently does not allow that rate?`),
+            }
+          );
+          if (!confirmed) {
+            throw new Error()
+          }
+        }
+
         sale_order[0].order_line = sale_lines;
 
         return sale_order[0];
       }
       async _onClickSaleOrder(event) {
-        const origin = this._getSaleOrderOrigin(this.env.pos.get_order())
-        if (!origin) {
-          return await super._onClickSaleOrder(...arguments)
-        }
-
-        const clickedOrder = event.detail;
-        let orders = []
-
-        for (const line of this.env.pos.get_order().get_orderlines()) {
-          if (line.sale_order_origin_id) {
-            orders.push(line.sale_order_origin_id)
+        try {
+          const origin = this._getSaleOrderOrigin(this.env.pos.get_order())
+          if (!origin) {
+            return await super._onClickSaleOrder(...arguments)
           }
+
+          const clickedOrder = event.detail;
+          let orders = []
+
+          for (const line of this.env.pos.get_order().get_orderlines()) {
+            if (line.sale_order_origin_id) {
+              orders.push(line.sale_order_origin_id)
+            }
+          }
+
+          orders = orders.map(el => el.id)
+
+          if (!orders.includes(clickedOrder.id)) {
+            return await super._onClickSaleOrder(...arguments)
+          }
+
+          return this.showPopup('ErrorPopup', {
+            title: _t('You already contain this sales order'),
+            body: _t(`One of the lines of this order, you already have it including the sales orde`)
+          });
+        } catch (error) {
+          this.close()
         }
-
-        orders = orders.map(el => el.id)
-
-        if (!orders.includes(clickedOrder.id)) {
-          return await super._onClickSaleOrder(...arguments)
-        }
-
-        return this.showPopup('ErrorPopup', {
-          title: _t('You already contain this sales order'),
-          body: _t(`One of the lines of this order, you already have it including the sales orde`)
-        });
       }
     }
 
