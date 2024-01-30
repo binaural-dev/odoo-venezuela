af6d4ae81c9f0976d0124664472421a892fd5670
christopher <christopher@binauraldev.com>
2024-01-29 21:42:36 -0400

    TAG #5030: binaural_pos, binaural_pos_discount.
    Problema:
    
    * Agregar a la ficha del empleado campo de seleccion de nombre "Tipo de empleado pos" con los valores cajeros, supervisor, mesonero.
    
    * Agregar en ajustes opcion para activar o desactivar implementacion de descuentos con clave de supervisor.
    
    ** Solo se mostrara cuando al menos de las opciones de ajustes siguientes este activada "Descuento Global" y "Descuento por linea".
    
    * Desde la vista principal de pos, al hacer click en cualquiera de los boton "%Desc" y "% Descuentos" desplegar ventana emergente de clave supervisor.
    
    * Crear ventana emergente para agregar clave supervisor.
    
    ** Al aceptar, validar con contraseñas almacenadas en el pos al momento en que cargo la session.
    
    ** Validar y mostrar mensajes de error al ser invalida la contraseña.
    
    ** Obtener los pin de las fichas de empleado de tipo supervisor.
    
    Solucion:
    
    * Extendiendo de SelectCashierMixin para obtener sus propiedades y crear un nuevo popup.
    
    * Mostrando el popup SupervisorPopup.
    
    Tarea:
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- integra/binaural_pos/static/src/js/SupervisorPopup.js
+++ integra/binaural_pos/static/src/js/SupervisorPopup.js
@@ -1,39 +1,74 @@
 odoo.define('binaural_pos.SupervisorPopup', function(require) {
   'use strict';
 
-  console.log('loooooog');
   const { useRef } = owl;
-  const AbstractAwaitablePopup = require('point_of_sale.AbstractAwaitablePopup');
   const Registries = require('point_of_sale.Registries');
+  const SelectCashierMixin = require('binaural_pos.SelectCashierMixin');
+  const PosComponent = require('point_of_sale.PosComponent');
+  const { useBarcodeReader } = require('point_of_sale.custom_hooks');
 
-  // formerly SelectionPopupWidget
-  class SupervisorPopup extends AbstractAwaitablePopup {
-    constructor() {
-      super(...arguments);
+  // const AbstractAwaitablePopup = require('point_of_sale.AbstractAwaitablePopup');
+
+  class SupervisorPopup extends SelectCashierMixin(PosComponent) {
+    setup() {
+      super.setup();
       this.password = "";
       this.inputRef = useRef('input');
+      useBarcodeReader({cashier: this.barcodeCashierAction}, true);
     }
+
+    async askPin(employee) {
+
+      if (this.password == "") return;
+
+      if (employee.pin === Sha1.hash(this.password)) {
+        return employee;
+      } 
+
+      await this.showPopup('ErrorPopup', {
+          title: this.env._t('Incorrect Password'),
+      });
+      
+    }
+
+    async selectCashier() {
+    }
+
+    async barcodeCashierAction(code) {
+      this.inputRef.el.value = code;
+    }
+
     mounted() {
       this.inputRef.el.focus();
     }
+
     captureChange(event) {
       this.password = event.target.value;
+      console.log('check if change on scan code fo just use this.password', this.password); 
     }
+
+    cancel() {
+      this.env.posbus.trigger('close-popup', {
+        popupId: this.props.id,
+        response: { confirmed: false, payload: false },
+      });
+    }
+  
+    confirm() {
+      console.log('confirm',this.env.pos.employees);
+      this.env.posbus.trigger('close-popup', {
+        popupId: this.props.id,
+        response: { confirmed: true, payload: true },
+      });
+    }
+
     checkconfirm(event){
       if (event.key == "Enter") {
         this.confirm()
       }
     }
-    confirm() {
-      if (true) {
-        super.confirm()
-        return
-      }
-      this.showPopup('ErrorPopup', {
-        title: 'Wrong Password',
-        body: 'The password is incorrect',
-      });
-    }
+
+
   }
 
   SupervisorPopup.template = 'SupervisorPopup';
