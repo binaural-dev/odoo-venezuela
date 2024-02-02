5f0339c7b9029816563c653d1c0940d5667fa4db
Miguel Gozaine <miguel@binauraldev.com>
2024-02-02 10:00:02 -0400

    fix: Método de conciliar en apuntes contables
    En el modulo binaural_mobile se sobreescribió el método de conciliación
    del modelo de apunte contable, pero no se devolvían los partials (que es
    lo que hace el método original).
    
    Al momento de hacer la conciliación bancaria, se espera que el método de
    conciliación de los apuntes devuelva los partials, por lo cual falla.
    
    Ahora el método que sobreescribe al original devuelve los partials.

--- integra/binaural_mobile/__manifest__.py
+++ integra/binaural_mobile/__manifest__.py
@@ -9,7 +9,7 @@
     "license": "LGPL-3",
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
-    "version": "16.0.0.1.2",
+    "version": "16.0.0.1.3",
     "category": "Human Resources/Stock/Sales/Invoicing",
     "depends": [
         "web",
