0801f8e2afdea6a1fffa155685d1065373eaef06
Anderson Armeya <anderson@binauraldev.com>
2024-02-08 17:28:07 -0400

    FEAT: binaural_commissions
    Se muestra si es un producto excluido
    
    Tarea:

--- integra/binaural_commissions/__manifest__.py
+++ integra/binaural_commissions/__manifest__.py
@@ -8,7 +8,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Expense/Payroll",
-    "version": "16.0.1.0.7",
+    "version": "16.0.1.0.8",
     # any module necessary for this one to work correctly
     "depends": [
         "account",
