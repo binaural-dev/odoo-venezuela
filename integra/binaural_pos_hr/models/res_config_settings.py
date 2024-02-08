830ae4da77d493ae46e6911aa722b090066a11a0
christopher <christopher@binauraldev.com>
2024-02-08 16:05:05 -0400

    TAG #16303: binaural_pos_hr
    Problema:
    
    Escenario: Actualmente la configuración esta en binaural (por compañia), se requiere que este en Terminal Punto de Venta para que se pueda activar por caja (imagen2)
    
    Solucion:
    
    * Elimine el campo en la compañia y lo deje en pos.settings. Con el related haciendo referencia a pos.setting y no a company_id.
    
    * Movi los campos de configuracion de clave de supervisario en la seccion de ajustes por caja.
    
    Tarea:
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- integra/binaural_pos_hr/models/res_config_settings.py
+++ integra/binaural_pos_hr/models/res_config_settings.py
@@ -5,5 +5,5 @@ from odoo.exceptions import ValidationError
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    pos_discount_require_supervisor_key = fields.Boolean(related="company_id.pos_discount_require_supervisor_key", readonly=False)
-    pos_refund_require_supervisor_key = fields.Boolean(related="company_id.pos_refund_require_supervisor_key", readonly=False)
+    pos_discount_require_supervisor_key = fields.Boolean(related="pos_config_id.pos_discount_require_supervisor_key", readonly=False)
+    pos_refund_require_supervisor_key = fields.Boolean(related="pos_config_id.pos_refund_require_supervisor_key", readonly=False)
