f643c272f285cb5db3f586d067002b4b7c48fa53
Miguel Gozaine <miguel@binauraldev.com>
2024-02-21 15:40:16 -0400

    fix: #2963 El campo Tipo de contacto no viaja de pedidos a facturas

--- integra/binaural_sale/__manifest__.py
+++ integra/binaural_sale/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sales/Sales",
-    "version": "16.0.0.9.0",
+    "version": "16.0.0.9.1",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
