6b8aad943b42ec9fcf7df634c5e45820dedc082a
Miguel Gozaine <96434156+miguel-binaural@users.noreply.github.com>
2024-02-26 08:52:36 -0400

    feat: #16930 Funciones de vacaciones en el empleado (#17)

--- integra/binaural_hr_payroll/models/hr_employee.py
+++ integra/binaural_hr_payroll/models/hr_employee.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime
 from dateutil import relativedelta
 from math import floor
 
@@ -116,3 +117,119 @@ class HrEmployee(models.Model):
         profit_sharing_days = self.company_id.profit_sharing_days_qty
         moves = self.get_all_payroll_moves()
         return (profit_sharing_days / 360) * (moves[-1]["total_accrued"] / 30)
+
+    def _get_vacation_bonus_days_of_previous_moves(self):
+        self.ensure_one()
+        vacation_moves = self.env["hr.payroll.move"].search(
+            [
+                ("employee_id", "=", self.id),
+                # ("move_type", "=", "vacation"),
+            ]
+        )
+        result = sum(move.vacation_bonus_days for move in vacation_moves)
+        return result
+
+    def _has_paid_vacation(self, year=datetime.today().year):
+        first_day_of_year = date(year, 1, 1)
+        last_day_of_year = date(year, 12, 31)
+
+        vacation_moves = self.env["hr.payroll.move"].search(
+            [
+                ("employee_id", "=", self.id),
+                ("move_type", "=", "vacation"),
+                ("date", ">=", first_day_of_year),
+                ("date", "<=", last_day_of_year),
+            ]
+        )
+
+        return any(vacation_moves)
+
+    def get_not_paid_vacation_bonus_days(self):
+        """
+        Compute the number of bonus days of the previous years that had not been paid (without
+        taking into account the fraction of the current year).
+
+        Those are, the number of bonus days that correspond to the employee on all history minus
+        the bonus days on the payroll moves of type vacation that the they have.
+
+        Returns
+        -------
+        int
+            The number of bonus days not paid.
+        """
+        self.ensure_one()
+        seniority_years = self._get_seniority_in_years()
+        if seniority_years == 0:
+            return 0
+
+        additional_days = self.company_id.additional_vacation_days_after_first_year
+        annual_vacation_days = self.company_id.first_year_vacation_days
+        bonus_days_of_the_current_year = annual_vacation_days + (seniority_years * additional_days)
+
+        vacation_slips = self.env["hr.payroll.move"].search(
+            [
+                ("employee_id", "=", self.id),
+                # ("move_type", "=", "vacation"),
+            ]
+        )
+        bonus_days_taken = sum(slip.vacation_bonus_days for slip in vacation_slips)
+
+        total_employee_bonus_days = sum(range(annual_vacation_days, bonus_days_of_the_current_year))
+        return total_employee_bonus_days - bonus_days_taken
+
+    def get_fractional_vacation_days(self, is_bonus=False):
+        """
+        Compute the fractional days or bonus days of vacation for the current year.
+
+        If the vacations of the employee had already been paid this year and they departure month
+        (or the current one) is less than they entry month, the result will be 0.
+
+        If the days that are gonna be computed are bonus, the total of days to use as a fraction
+        are calculated taking into account the first years for the additional days, else we use the
+        additional days starting on year 2.
+
+        Parameters
+        ----------
+        is_bonus : bool
+            If the days that are gonna be computed are bonus vacation days.
+
+        Returns
+        -------
+        float
+            The fraction of days or bonus days of the current year.
+        """
+        self.ensure_one()
+        first_day_of_year = date.today().replace(month=1, day=1)
+        last_day_of_year = date.today().replace(month=12, day=1)
+        vacation_slips_of_current_year = self.env["hr.payroll.move"].search(
+            [
+                ("employee_id", "=", self.id),
+                ("date_to_vacation", ">=", first_day_of_year),
+                ("date_to_vacation", "<=", last_day_of_year),
+            ],
+            order="date desc",
+            limit=1,
+        )
+
+        if vacation_slips_of_current_year and self.entry_date.month >= (
+            self.departure_date.month if self.departure_date else date.today.month
+        ):
+            return 0
+
+        seniority_years = self._get_seniority_in_years()
+        vacation_days = self.company_id.first_year_vacation_day
+        additional_vacation_days_per_year = (
+            self.company_id.additional_vacation_days_after_first_year
+        )
+        additional_vacation_days_total = seniority_years * additional_vacation_days_per_year
+
+        # If the days are not the bonus one, the additional days must be added starting year 2 of
+        # the employee.
+        if not is_bonus:
+            additional_vacation_days_total -= additional_vacation_days_per_year
+        months_worked_this_year = (
+            self.departure_date.month if self.departure_date else date.today().month
+        ) - self.entry_date.month
+
+        days_per_month = (additional_vacation_days_total + vacation_days) / 12
+        return days_per_month * months_worked_this_year
