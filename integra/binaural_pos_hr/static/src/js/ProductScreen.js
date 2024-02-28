639b984a11e0e32a20101a6becaed23606ca0f0e
Miguel Gozaine <miguel@binauraldev.com>
2024-02-28 17:20:44 -0400

    fix: #3010 Editar cantidades en POS

--- integra/binaural_pos_hr/static/src/js/ProductScreen.js
+++ integra/binaural_pos_hr/static/src/js/ProductScreen.js
@@ -1,50 +1,46 @@
-odoo.define('binaural_pos_hr.ProductScreen', function(require) {
+odoo.define("binaural_pos_hr.ProductScreen", function (require) {
   "use strict";
 
-  const Registries = require('point_of_sale.Registries');
-  const ProductScreen = require('point_of_sale.ProductScreen');
-  const NumberBuffer = require('point_of_sale.NumberBuffer');
-  const { _t } = require('web.core');
-  const { Gui } = require("point_of_sale.Gui");
+  const Registries = require("point_of_sale.Registries");
+  const ProductScreen = require("point_of_sale.ProductScreen");
+  const { parse } = require("web.field_utils");
+  const { _t } = require("web.core");
 
   const BinauralHrProductScreen = (ProductScreen) =>
-    class BinauralHrProductScreen extends ProductScreen {
-      _setValue(val) {
+    class extends ProductScreen {
+      async _setValue(inputValue) {
         const order = this.env.pos.get_order();
         const selectedLine = order.get_selected_orderline();
-        if (this.env.pos.numpadMode === 'quantity' && val === "" && selectedLine.get_quantity() === 0) {
-          super._setValue("remove")
-        } else {
-          return super._setValue(...arguments)
-        }
-      }
-      async _updateSelectedOrderline(event) {
-        if (!this.env.pos.config.pos_remove_orderline_require_supervisor_key) {
-          await super._updateSelectedOrderline(...arguments)
-        }
-        const order = this.env.pos.get_order();
-        const selectedLine = order.get_selected_orderline();
-        if (!selectedLine) {
-          await super._updateSelectedOrderline(...arguments)
-        }
+        const currentQuantity = selectedLine.get_quantity();
+        const newQuantity = parse.float(inputValue);
 
-        let keys = ["Backspace","+", "-"]
-
-        if (this.env.pos.numpadMode === 'quantity' && keys.includes(event.detail.key)) {
-          const { confirmed } = await this.showPopup("SupervisorPopup", {
-            title: _t("Insert Supervisor's Password"),
-          });
+        // Supervisor check for removing an item or reducing quantity
+        if (
+          this.env.pos.numpadMode === "quantity" &&
+          ((inputValue === "" && currentQuantity === 0) ||
+            newQuantity < currentQuantity)
+        ) {
+          const confirmed = await this._requireSupervisorApproval();
           if (!confirmed) {
-            return
+            return super._setValue(currentQuantity);
+          }
+          if (inputValue === "" && currentQuantity === 0) {
+            return super._setValue("remove");
           }
         }
 
-        await super._updateSelectedOrderline(...arguments)
+        return super._setValue(...arguments);
+      }
+
+      async _requireSupervisorApproval() {
+        const { confirmed } = await this.showPopup("SupervisorPopup", {
+          title: _t("Insert Supervisor's Password"),
+        });
+        return confirmed;
       }
     };
 
   Registries.Component.extend(ProductScreen, BinauralHrProductScreen);
 
   return BinauralHrProductScreen;
-
 });
