1064fbca89dac8b5cfad86c61174eeab3b67493c
yeisonasuaje <yeison@binaural.com.ve>
2024-03-12 10:07:34 -0400

    -Revisar validación de no vender en cero desde POS y desde ventas Ticket -Funcion heredado con los campo de la original -https://binaural.odoo.com/web#id=3073&cids=2&menu_id=293&action=393&active_id=10&model=helpdesk.ticket&view_type=form

--- integra/binaural_pos_sale/models/sale_order.py
+++ integra/binaural_pos_sale/models/sale_order.py
@@ -3,14 +3,13 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.depends('pos_order_count')
+    @api.depends('state','pos_order_count')
     def _compute_invoice_status(self):
         res = super()._compute_invoice_status()
         for order in self:
             if order.pos_order_count > 0:
                 order.invoice_status = "invoiced"
         return res
-
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
@@ -22,5 +21,3 @@ class SaleOrderLine(models.Model):
         res.append("foreign_rate")
         res.append("foreign_inverse_rate")
         return res 
-    
-    
