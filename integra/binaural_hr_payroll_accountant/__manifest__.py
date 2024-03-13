48b4f656c9815edd5550aa0fa71976213c3ab91e
Miguel Gozaine <miguel@binauraldev.com>
2024-03-13 16:05:48 -0400

    feat: #19188 Nómina contabilidad
    Los totales alternos de cada línea de nómina perteneciente al cálculo
    del salario tienen que estar en el debe o haber alterno del apunte
    contable correspondiente a la regla.

---
+++ integra/binaural_hr_payroll_accountant/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Binaural Nómina Con Contabilidad",
+    "summary": """
+        Modificaciones de nómina con contabilidad.
+    """,
+    "author": "Binauraldev",
+    "website": "https://www.binauraldev.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    "version": "16.0.0.0.0",
+    # any module necessary for this one to work correctly
+    "depends": ["binaural_accountant", "binaural_hr_payroll"],
+    # always loaded
+    "auto_install": True,
+}
