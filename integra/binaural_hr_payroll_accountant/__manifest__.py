a9fbcaaa755f0194a062b7d2ffd0696daf5e241b
Miguel Gozaine <miguel@binauraldev.com>
2024-03-22 14:11:37 -0400

    update manifest

--- integra/binaural_hr_payroll_accountant/__manifest__.py
+++ integra/binaural_hr_payroll_accountant/__manifest__.py
@@ -8,7 +8,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    "version": "16.0.0.0.0",
+    "version": "16.0.0.0.1",
     # any module necessary for this one to work correctly
     "depends": ["binaural_accountant", "binaural_hr_payroll"],
     # always loaded
