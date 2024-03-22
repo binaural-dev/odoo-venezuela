00e2a4f53b16d4b661371e2125b7cc34565c1bdd
Miguel Gozaine <miguel@binauraldev.com>
2024-03-22 14:10:58 -0400

    fix: #19188 Valor en negativo en debe y haber alterno de asiento de nómina

--- integra/binaural_hr_payroll_accountant/models/hr_payslip_account.py
+++ integra/binaural_hr_payroll_accountant/models/hr_payslip_account.py
@@ -7,7 +7,7 @@ class HrPayslip(models.Model):
     def _prepare_line_values(self, line, account_id, date, debit, credit):
         values = super()._prepare_line_values(line, account_id, date, debit, credit)
 
-        foreign_amount = line.foreign_total
+        foreign_amount = abs(line.foreign_total)
         foreign_debit = foreign_amount if values["debit"] > 0.0 else 0.0
         foreign_credit = foreign_amount if values["credit"] > 0.0 else 0.0
 
