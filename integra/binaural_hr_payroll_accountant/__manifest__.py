990a164fbbb17e87ef882c6c5d676db0177fe381
Miguel Gozaine <96434156+miguel-binaural@users.noreply.github.com>
2024-04-02 10:04:01 -0400

    feat: #22150 Ajustes en asiento de recibo de nómina (#28)
    Se cambió la fecha del asiento que se genera a partir del recibo de
    nómina para que tenga por defecto la fecha que aparece en el final del
    período del recibo. También se cambió la referencia del asiento y la
    tasa.
    
    Tarea: https://binaural.odoo.com/web#id=22150&menu_id=255&cids=2&action=341&model=project.task&view_type=form

--- integra/binaural_hr_payroll_accountant/__manifest__.py
+++ integra/binaural_hr_payroll_accountant/__manifest__.py
@@ -8,7 +8,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    "version": "16.0.0.0.1",
+    "version": "16.0.0.0.2",
     # any module necessary for this one to work correctly
     "depends": ["binaural_accountant", "binaural_hr_payroll"],
     # always loaded
