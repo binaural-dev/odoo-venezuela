3b4a3b2caa4ac3b582dbfdfa3db2cb2ff749149d
Anderson Armeya <anderson@binauraldev.com>
2024-04-05 18:05:00 -0400

    IMP: binaural_commissions
    Al momento de recalcular la comision no recalcula el monto en la
    factura, pero si los dias, por lo que ya tengan facturas de proveedor se
    omiten

--- integra/binaural_commissions/wizard/set_commission_order_to_invoice.py
+++ integra/binaural_commissions/wizard/set_commission_order_to_invoice.py
@@ -15,34 +15,44 @@ class SetCommissionOrderToInvoice(models.Model):
     def action_confirm(self):
         order_message = []
         invoice_message = []
+        invoice_paid_message = []
+
+        invoices_paid = self.sale_order_ids.invoice_ids.filtered(
+            lambda x: x._can_recompute_commission()
+        )
+
+        available_orders = self.sale_order_ids.filtered(lambda x: invoices_paid in x.invoice_ids)
 
         if self.overwrite_commission:
-            order_message = self.sale_order_ids.order_line.filtered(
+            order_message = available_orders.order_line.filtered(
                 lambda x: x.commission_policy_line_image_ids
             ).order_id.mapped("name")
-            invoice_message = self.sale_order_ids.invoice_ids.invoice_line_ids.filtered(
+            invoice_message = available_orders.invoice_ids.invoice_line_ids.filtered(
                 lambda x: x.commission_image_id
             ).move_id.mapped("name")
+            invoice_paid_message = (self.sale_order_ids - available_orders).mapped("name")
 
-            self.sale_order_ids.order_line.commission_policy_line_image_ids = False
-            self.sale_order_ids.invoice_ids.invoice_line_ids.commission_image_id = False
+            available_orders.order_line.commission_policy_line_image_ids = False
+            available_orders.invoice_ids.invoice_line_ids.commission_image_id = False
 
         data_write = {
             "commission_invoice_date_field": self.company_id.commission_invoice_date_field,
             "compute_commission_when": self.company_id.compute_commission_when,
-            "priority_commission_policy_type": "/".join(self.env["commission.policy.type"].search([]).mapped("name"))
+            "priority_commission_policy_type": "/".join(
+                self.env["commission.policy.type"].search([]).mapped("name")
+            ),
         }
-        
-        if self.sale_order_ids.filtered(lambda x: x.state not in ["done","sale"]):
+
+        if self.sale_order_ids.filtered(lambda x: x.state not in ["done", "sale"]):
             raise ValidationError(_("Only orders in state 'Sale' or 'Done' can be processed"))
 
-        self.sale_order_ids.write(data_write)
-        self.sale_order_ids.assing_commission_policy_line_images_to_order_lines()
+        available_orders.write(data_write)
+        available_orders.assing_commission_policy_line_images_to_order_lines()
 
-        self.sale_order_ids.invoice_ids.write(data_write)
-        self.sale_order_ids.invoice_ids.calculate_commission()
+        available_orders.invoice_ids.write(data_write)
+        available_orders.invoice_ids.calculate_commission()
 
-        if len(order_message) == 0 and len(invoice_message) == 0:
+        if sum([len(order_message) + len(invoice_message) + len(invoice_paid_message)]) == 0:
             return True
 
         message = []
@@ -65,6 +75,12 @@ class SetCommissionOrderToInvoice(models.Model):
                     """,
                     ", ".join(invoice_message),
                 )
+            )
+
+        if len(invoice_paid_message) > 0:
+            message.append(
+                _(
+                    """The order(s) already had invoices paid, They have not been processed""",)
             )
 
         return {
