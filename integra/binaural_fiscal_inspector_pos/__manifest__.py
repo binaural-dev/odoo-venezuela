3d06a8bcf635d3860e7e33704c3cb63c7dfd8ea5
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-04-15 11:29:10 -0400

    FEAT-#23148-binaural_fiscal_inspector_pos
    - Se creo un modulo para los permisos que sean de pos

---
+++ integra/binaural_fiscal_inspector_pos/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Binaural Inspeccion Fiscal Pos",
+    "summary": """
+        Módulo para permisos de inspeccion fiscal a Pos
+    """,
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://binauraldev.com/",
+    "category": "Technical",
+    "version": "16.0.0.0.0",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "binaural_fiscal_inspector",
+        "binaural_pos",
+    ],
+    # always loaded
+    "data": [
+    ],
+    'auto_install': True,
+}
