67a2b798a070a2e52359e62585f4065faa6e35ab
Anderson Armeya <anderson@binauraldev.com>
2024-04-29 17:23:21 -0400

    FEAT #5035: binaural_sale_stock,binaural_seller_stock,binaural_stock_distribution,binaural_stock_distribution_seller,binaural_stock_barcode
    Se crea Guia de Reparto con y sin Vendedores
    
    Se crean los modulos:
    
    * Ventas Inventario
    * Vendedores Inventario
    * Guia de Reparto
    * Guia de Reparto (Vendedores)
    
    https://binaural.odoo.com/web#id=5035&cids=2&menu_id=255&action=341&model=project.task&view_type=form

---
+++ integra/binaural_sale_stock/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Binaural Ventas Inventario",
+    "summary": """Personalizaciones a entre Ventas e Inventario""",
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://www.binauraldev.com",
+    "category": "Stock",
+    "version": "16.0.16.0.11",
+    "depends": ["sale_stock"],
+    "data": [],
+    "application": True,
+}
--- integra/binaural_seller_stock/readme/DESCRIPTION.rst
+++ integra/binaural_seller_stock/readme/DESCRIPTION.rst
@@ -2,3 +2,7 @@
 
 Este modulo agrega el Vendedor a la etiqueta de bultos
 
+Agrega en el Modelo (Stock Picking - Transferencias)
+
+* Vendedor (Referencia del vendedor del Pedido de Ventas)
+
