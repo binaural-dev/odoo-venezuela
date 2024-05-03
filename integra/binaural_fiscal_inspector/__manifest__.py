509cc7f35dcf64cde0ac41c4b6e78eca69c73f9f
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-05-03 16:46:08 -0400

    FIX-#23148-Binaural_Fiscal_inspector
    - se agrego para que los botones de anular, borrador y publicar se ocultaran
    al ser un usuario del grupo Fiscal (no editable)
    
    - Se agrego restriccion para no sobreescribir retenciones al grupo Fiscal (no editable)
    
    Ticket= https://binaural.odoo.com/web#id=3385&cids=2&model=helpdesk.ticket&view_type=form

--- integra/binaural_fiscal_inspector/__manifest__.py
+++ integra/binaural_fiscal_inspector/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.2.1",
+    "version": "16.0.0.2.2",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
@@ -27,6 +27,7 @@
         "views/menu_item.xml",
         "views/account_move.xml",
         "views/account_payment.xml",
+        "views/account_retention.xml",
     ],
     "post_init_hook": "create_res_users_fiscal",
     "uninstall_hook": "inspector_uninstall_hook",
