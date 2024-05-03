0a592a99f0f9fdfcdac7c99993bea4208fe47b0d
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-05-03 17:29:44 -0400

    FIX-#23148-Binaural_Fiscal_inspector
    - Se pusieron los permisos necesarios para poder colocar en borrador el
    grupo fiscal Editable
    
    Tarea: https://binaural.odoo.com/web#id=23148&cids=2&model=project.task&view_type=form

--- integra/binaural_fiscal_inspector_pos/__manifest__.py
+++ integra/binaural_fiscal_inspector_pos/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.0",
+    "version": "16.0.0.0.1",
     # any module necessary for this one to work correctly
     "depends": [
         "binaural_fiscal_inspector",
