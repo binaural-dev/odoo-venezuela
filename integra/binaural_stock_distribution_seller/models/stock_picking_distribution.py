8bb1c18c49bf6ec8ddf451b530a46977141754ba
Anderson Armeya <anderson@binauraldev.com>
2024-05-10 10:26:02 -0400

    FIX #26486: binaural_stock_distribution
    Problema: Al modificar las lineas de la guia de reparto no se guardaban
    las lineas
    
    Solucion: Se coloco el readonly y filtrado de stock.picking
    
    Tarea:
    
    https://binaural.odoo.com/web#id=26486&cids=2&menu_id=255&action=341&model=project.task&view_type=form

--- integra/binaural_stock_distribution_seller/models/stock_picking_distribution.py
+++ integra/binaural_stock_distribution_seller/models/stock_picking_distribution.py
@@ -5,6 +5,6 @@ class StockPickingDistribution(models.Model):
     _inherit = "stock.picking.distribution"
 
     seller_id = fields.Many2one("hr.employee", domain=[("is_seller", "=", True)])
-    picking_seller_ids = fields.One2many(related="picking_ids")
+    picking_seller_ids = fields.One2many(related="picking_ids", readonly=False)
 
     
