946194fadf5555db898c2a520f13acdc91ea2200
bin-daldana <david@binauraldev.com>
2024-06-05 10:57:25 -0400

    FEAT: se agregaron los groups para Datos de produccion de leche de ancestros y Valoracion morfologica

--- integra/binaural_farming/__manifest__.py
+++ integra/binaural_farming/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Binaural Ganaderia",
     "summary": "Modulo para información de Ganaderia",
-    "version": "16.0.0.0.0",
+    "version": "16.0.0.0.1",
     "category": "Stock",
     "license": "LGPL-3",
     "author": "BinauralDev",
