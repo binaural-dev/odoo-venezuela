9d613b0264e526509acbbfe13036162db4ba35db
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-06-05 17:32:42 -0400

    FIX-Ticket#3941-Binaural_club_socios
    Problema: Falta el estado de la accion en el Country Club editable desde la accion en la version 16
    
    Solucion: se agrego de que el estatus sea clickeable desde el menu de accion
    
    Ticket: https://binaural.odoo.com/web#id=3941&cids=2&menu_id=293&action=386&model=helpdesk.ticket&view_type=form

--- integra/binaural_club_socios/__manifest__.py
+++ integra/binaural_club_socios/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "16.0.0.0.6",
+    "version": "16.0.0.0.7",
     "depends": [
         "base",
         "base_automation",
