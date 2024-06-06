aec5dca750c1af8bc9e5a4b7f3d3f0904bf97f6b
bin-daldana <david@binauraldev.com>
2024-06-06 15:45:56 -0400

    FIX: correcciones requeridas. Cambio de terminos y logica de campo de active dentro del modelo de raza y criador

--- integra/binaural_farming/__manifest__.py
+++ integra/binaural_farming/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Binaural Ganaderia",
     "summary": "Modulo para información de Ganaderia",
-    "version": "16.0.0.0.5",
+    "version": "16.0.0.0.6",
     "category": "Stock",
     "license": "LGPL-3",
     "author": "BinauralDev",
