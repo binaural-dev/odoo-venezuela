13e17ed566c1d12953ecae60afba3ca4a62e2487
bin-daldana <david@binauraldev.com>
2024-06-06 15:53:27 -0400

    FIX: correccion de acronimo por siglas

--- integra/binaural_farming/__manifest__.py
+++ integra/binaural_farming/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Binaural Ganaderia",
     "summary": "Modulo para información de Ganaderia",
-    "version": "16.0.0.0.6",
+    "version": "16.0.0.0.7",
     "category": "Stock",
     "license": "LGPL-3",
     "author": "BinauralDev",
