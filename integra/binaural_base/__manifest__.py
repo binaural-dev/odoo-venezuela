06d1e42734807803baef63a7b58c3c39b64dc13c
Anderson Armeya <anderson@binauraldev.com>
2024-06-06 09:19:47 -0400

    ADD: binaural_base (#1242)
    * ADD: binaural_base
    
    Modulo Base de Integra para administracion de Proyectos de Binaural
    
    Se crea una rama para el manejo de Versiones
    
    Se crea un script para actualizar staging mediante Dummy

---
+++ integra/binaural_base/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Binaural Base",
+    "summary": """
+        Módulo base para manejo de Modulos de Binaural 
+    """,
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://binauraldev.com/",
+    "category": "Technical",
+    "version": "16.0.0.0.3",
+    # any module necessary for this one to work correctly
+    "depends": ["base"],
+    # always loaded
+    "auto_install": True,
+    "data": ["data/ir_config_parameter.xml"],
+}
