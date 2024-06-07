3dbcbe96b97860ab6dc9a176f2a67e2fa10dc61e
bryanbinaural <bryan@binaural.com.ve>
2024-06-07 11:22:40 -0400

    FIX Anticipos con IGTF: binaural_advance_igtf
    Problema: Habian cambios que estaban en el proyecto Grena que no se habian pasado a Integra, dentro de esas modificaciones estan las validaciones para los asientos de anticipos con igtf para proveedores y validaciones para no crear asientos en cero, ademas de validaciones extras para asegurar el funcionamiento normal del modulo.
    
    Solucion: Se migran las validaciones necesarias.
    
    Tarea:https://binaural.odoo.com/web#id=3932&cids=2&model=helpdesk.ticket&view_type=form
    Signed-off-by: bryanbinaural <bryan@binaural.com.ve>

--- integra/binaural_advance_payment_igtf/wizard/account_payment_register.py
+++ integra/binaural_advance_payment_igtf/wizard/account_payment_register.py
@@ -7,7 +7,6 @@ _logger = logging.getLogger(__name__)
 
 class AccountPaymentRegister(models.TransientModel):
     _inherit = "account.payment.register"
-
     def _create_payments(self):
         """
         This method is called when the wizard is submitted. It will create a move to reconcile with the payment difference
@@ -17,6 +16,8 @@ class AccountPaymentRegister(models.TransientModel):
         """
         payments = super()._create_payments()
 
+        is_provider = False
+
         for payment in payments:
             partner_account_id = (
                 payment.partner_id.property_account_receivable_id.id
@@ -25,14 +26,24 @@ class AccountPaymentRegister(models.TransientModel):
             )
 
             payment_invoice_amount = payment.reconciled_invoice_ids.line_ids.filtered(
-                lambda line: line.account_id.account_type == "asset_receivable" or line.account_id.account_type == "liability_payable"
+                lambda line: line.account_id.account_type == "asset_receivable"
             ).amount_currency
 
-            amount = payment.amount
+            if payment_invoice_amount == 0:
+
+                payment_invoice_amount = payment.reconciled_bill_ids.line_ids.filtered(
+                    lambda line: line.account_id.account_type == "liability_payable"
+                ).amount_currency
+
+            amount = round(payment.amount, payment.currency_id.decimal_places)
 
             if self.env.company.currency_id.id == self.env.ref("base.VEF").id:
                 amount = amount * payment.foreign_rate
 
+            if payment_invoice_amount < 0:
+                # If amount of the payment invoice is negative, means its provider.
+                is_provider = True
+                payment_invoice_amount = payment_invoice_amount * -1
             if (
                 amount > payment_invoice_amount
                 and payment.journal_id.fiscal
@@ -44,9 +55,14 @@ class AccountPaymentRegister(models.TransientModel):
                 )
                 move_to_reconcile_with_payment_difference.action_post()
 
-                self._reconcile_payment_and_move_lines(
-                    payment, move_to_reconcile_with_payment_difference
-                )
+                if is_provider:
+                    self._reconcile_payment_provider_and_move_lines(
+                        payment, move_to_reconcile_with_payment_difference
+                    )
+                else:
+                    self._reconcile_payment_and_move_lines(
+                        payment, move_to_reconcile_with_payment_difference
+                    )
 
         return payments
 
@@ -60,7 +76,6 @@ class AccountPaymentRegister(models.TransientModel):
         Returns:
             account.move: Move object
         """
-
         advance_account_id = (
             self.env.company.advance_customer_account_id.id
             if payment.partner_type == "customer"
@@ -72,19 +87,33 @@ class AccountPaymentRegister(models.TransientModel):
             if payment.partner_type == "customer"
             else payment.partner_id.property_account_payable_id.id
         )
+        
+        asset_amount = payment.line_ids.filtered(
+            lambda line: line.account_id.account_type == "asset_receivable"
+        ).amount_currency
 
         invoice_amount = payment.reconciled_invoice_ids.line_ids.filtered(
             lambda line: line.account_id.id == partner_account_id
         ).amount_currency
-        asset_amout = payment.line_ids.filtered(
-            lambda line: line.account_id.account_type == "asset_receivable"
-        ).amount_currency
         
 
-        if self.env.company.currency_id.id == self.env.ref("base.VEF").id:
-            asset_amout = asset_amout * payment.foreign_rate
+        if asset_amount == 0:
 
-        amount_currency = asset_amout + invoice_amount
+            asset_amount = payment.line_ids.filtered(
+                lambda line: line.account_id.account_type == "liability_payable"
+            ).amount_currency
+
+        if invoice_amount == 0:
+
+            invoice_amount = payment.reconciled_bill_ids.line_ids.filtered(
+                lambda line: line.account_id.id == partner_account_id
+            ).amount_currency
+        
+        
+        if self.env.company.currency_id.id == self.env.ref("base.VEF").id:
+            asset_amount = asset_amount * payment.foreign_rate
+
+        amount_currency = asset_amount + invoice_amount
 
         payment_line_ids = [
             Command.create(
@@ -100,9 +129,7 @@ class AccountPaymentRegister(models.TransientModel):
                 },
             ),
         ]
-        if not self.env.company.advance_payment_igtf_journal_id:
-            raise ValidationError(_("You do not have a Journal configured for IGTF Advance"))
-        
+
         move_to_reconcile_with_payment_difference = self.env["account.move"].create(
             {
                 "journal_id": self.env.company.advance_payment_igtf_journal_id.id,
@@ -134,4 +161,24 @@ class AccountPaymentRegister(models.TransientModel):
         if asset_receivable_lines and payment_line:
             payment_line_to_reconcile = self.env["account.move.line"].browse([payment_line.id])
             payment_line_to_reconcile |= asset_receivable_lines
+            payment_line_to_reconcile.reconcile()
+
+    
+    def _reconcile_payment_provider_and_move_lines(self, payment, move):
+        """
+        Reconcile payment and move lines from provider.
+
+        Args:
+            payment (account.payment): Payment object
+            move (account.move): Move object
+        """
+        liability_payable_lines = move.line_ids.filtered(
+            lambda x: x.account_id.account_type == "liability_payable" and not x.reconciled
+        )
+        payment_line = payment.line_ids.filtered(
+            lambda x: x.account_id.account_type == "liability_payable" and not x.reconciled
+        )
+        if liability_payable_lines and payment_line:
+            payment_line_to_reconcile = self.env["account.move.line"].browse([payment_line.id])
+            payment_line_to_reconcile |= liability_payable_lines
             payment_line_to_reconcile.reconcile()
