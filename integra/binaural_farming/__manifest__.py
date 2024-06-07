651261f9e405950af6a50241ffbcfcd2cafa07aa
bin-daldana <david@binauraldev.com>
2024-06-07 11:51:32 -0400

    FIX #27684: binaural_farming
    Problema: Correcciones: Cambios de terminos, adicion de campos al modelo de produccion de leche por dias,
    	se le anexaron tres campos mas para registrar la cantidad de dias por la muestra,
    	de tradujo la data precargada ya que se registro en ingles y era en espaniol.
    
    Solucion: Se realizaron los cambios solicitados en la tarea
    
    Tarea: Agregar Campos a Formulario de Lotes
    Signed-off-by: bin-daldana <david@binauraldev.com>
    
    project.task
    https://binaural.odoo.com/web#id=27684&cids=2&menu_id=255&action=341&model=project.task&view_type=form

--- integra/binaural_farming/__manifest__.py
+++ integra/binaural_farming/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Binaural Ganaderia",
     "summary": "Modulo para información de Ganaderia",
-    "version": "16.0.0.0.7",
+    "version": "16.0.0.0.8",
     "category": "Stock",
     "license": "LGPL-3",
     "author": "BinauralDev",
