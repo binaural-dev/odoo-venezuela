6d232fa2d046c34b9b43ee774ccb8cfb2e53587f
bin-daldana <david@binauraldev.com>
2024-06-12 16:40:28 -0400

    FIX: campo de display name no era el campo que debia mostrar en la view, porque ese cambpo no es complete name, ya que ese lleva la traduccion

--- integra/binaural_farming/__manifest__.py
+++ integra/binaural_farming/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Binaural Ganaderia",
     "summary": "Modulo para información de Ganaderia",
-    "version": "16.0.0.0.14",
+    "version": "16.0.0.0.15",
     "category": "Stock",
     "license": "LGPL-3",
     "author": "BinauralDev",
