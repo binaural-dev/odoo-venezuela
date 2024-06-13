03bbf2d33f60a969c1d1d5cc7e375b62e09f6f1e
bin-daldana <david@binauraldev.com>
2024-06-13 17:42:27 -0400

    FIX manifest

--- integra/binaural_farming/__manifest__.py
+++ integra/binaural_farming/__manifest__.py
@@ -1,11 +1,7 @@
 {
     "name": "Binaural Ganaderia",
     "summary": "Modulo para información de Ganaderia",
-<<<<<<< Updated upstream
-    "version": "16.0.0.0.16",
-=======
     "version": "16.0.0.0.19",
->>>>>>> Stashed changes
     "category": "Stock",
     "license": "LGPL-3",
     "author": "BinauralDev",
