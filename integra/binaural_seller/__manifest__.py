0d0862a1f0ea9d40445ab19506f5e490dd289440
OmarYepez29 <omar@binauraldev.com>
2024-06-14 12:40:34 -0400

    FEAT-27024-binaural_seller Se agrego campo vendedor en Filtros y Agrupar en los reportes (análisis de ventas - modulo de ventas) (análisis de facturas - modulo de contabilidad) Tarea:https://binaural.odoo.com/web#id=27024&cids=2&menu_id=255&action=341&model=project.task&view_type=form

--- integra/binaural_seller/__manifest__.py
+++ integra/binaural_seller/__manifest__.py
@@ -5,7 +5,7 @@
     "license": "LGPL-3",
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
-    "version": "16.0.1.0.2",
+    "version": "16.0.1.0.3",
     "category": "Hr/Invoicing/Account",
     "depends": [
         "account",
@@ -19,6 +19,8 @@
         "data/res_partner.xml",
         "data/res_users.xml",
         "data/hr_employee.xml",
+        "report/account_invoice_report.xml",
+        "report/sale_report.xml",
         "views/account_move.xml",
         "views/res_partner.xml",
         "views/res_config_settings.xml",
