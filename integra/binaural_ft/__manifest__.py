77a34fa5885c158f7b8609d73041f4191d7b2386
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-06-19 11:48:35 -0400

    FIX #28815 Binaural_FT
    Se quitaron cosas innecesarios y se crearon funciones base

--- integra/binaural_ft/__manifest__.py
+++ integra/binaural_ft/__manifest__.py
@@ -10,16 +10,11 @@
     "category": "Stock",
     "version": "16.0.1.0.0",
     "depends": [
-        "stock",
-        "binaural_stock",
         "point_of_sale",
         "binaural_pos_receipt",
-        "binaural_invoice",
-        "binaural_payment_extension",
     ],
     "data": [
         "report/invoice_mf_ticket.xml",
-        "views/stock_picking.xml",
     ],
     "assets": {
         "point_of_sale.assets": [
@@ -28,4 +23,5 @@
             # "binaural_ft/static/src/css/*.css",
         ],
     },
+    "binaural": True,
 }
--- integra/binaural_ft/models/account_move.py
+++ integra/binaural_ft/models/account_move.py
@@ -11,60 +11,6 @@ _logger = logging.getLogger(__name__)
 MAX_CHAR_LINE = 64
 CHAR_PER_LINE = 44
 
-default = {
-    "name": "",
-    "header": [""],
-    "serial": "",
-}
-
-COMPANY = {
-    "9": {
-        "header": [
-            "COMERCIALIZADOR LA PARADA D.M C.A",
-            "AV FRANCISCO DE MIRANDA CON CALLE",
-            "BOLIVAR CASA 10 NRO 8 SECTOR PETARE",
-            "CARACAS (PETARE) MIRANDA",
-        ],
-        "serial": "Z7C7018335",
-    },
-    "8": {
-        "header": [
-            "EL NUEVO MERCADO DE PETARE II C.A",
-            "AV FRANCISCO DE MIRANDA LOCAL CACIQUE",
-            "NRO 2 Y 3 SECTOR PERATE CARACAS",
-            "MIRANDA ZONA POSTAL 1070",
-        ],
-        "serial": "Z7C7014867",
-    },
-    "10": {
-        "header": [
-            "FRIGORIFICO Y VIVERES EL PETARENO, C.A",
-            "CALLE FEDERACION EDIF PETARE PISO P.B",
-            "LOCAL P.B SECTOR PETARE CARACAS",
-            "(PETARE) MIRANDA",
-        ],
-        "serial": "Z7C7009751",
-    },
-    "7": {
-        "header": [
-            "SUPERMERCADO Y FRIGORIFICO JUERIS, CA",
-            "CALLE PEREZ LEON FINAL REDOMA DE PETARE",
-            "CASA S/N SECTOR PETARE CARACAS(PETARE)",
-            "MIRANDA ZONA POSTAL 1070",
-        ],
-        "serial": "Z7C7014390",
-    },
-    "1": {
-        "header": [
-            "COMERCIALIZADORA REZZ GONCALVES, C.A.",
-            "AV. FRANCISCO DE MIRANDA FINAL AVENIDA",
-            "LOCAL NRO 10 URB. PETARE CARACAS",
-            "(PETARE) MIRANDA",
-        ],
-        "serial": "Z7C7028957",
-    },
-}
-
 
 def get_spaces(string1, string2, type_a=False):
     char = MAX_CHAR_LINE if not type_a else 48
@@ -76,32 +22,11 @@ def get_spaces(string1, string2, type_a=False):
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    warehouse_id = fields.Many2one("stock.warehouse", compute="_compute_warehouse")
-
-    @api.depends("line_ids", "pos_order_ids")
-    def _compute_warehouse(self):
-        for record in self:
-            if record.pos_order_ids:
-                record.warehouse_id = record.pos_order_ids.config_id.warehouse_id
-                continue
-
-            for line in record.invoice_line_ids:
-                found_warehouse = False
-                for sale_line in line.sale_line_ids:
-                    record.warehouse_id = sale_line.warehouse_id
-                    found_warehouse = True
-                    continue
-                if found_warehouse:
-                    break
-
-            if not record.warehouse_id:
-                record.warehouse_id = False
-
     def get_receipt_base(self):
         lines = []
         lines.append(TXT_ALIGN_CT + TXT_BOLD_ON + b"SENIAT")
         lines.append(encode_text("RIF " + self.company_id.vat))
-        for line in COMPANY.get(str(self.company_id.id), default)["header"]:
+        for line in self.set_info_in_receipt().get(str(self.company_id.id), False)["header"]:
             lines.append(TXT_BOLD_OFF + encode_text(line))
         lines.append(
             TXT_ALIGN_LT
@@ -123,7 +48,7 @@ class AccountMove(models.Model):
             lines.append(
                 encode_text(
                     "#CONTROL/SERIAL IF: "
-                    + COMPANY.get(str(self.company_id.id), default)["serial"]
+                    + self.set_info_in_receipt().get(str(self.company_id.id), False)["serial"]
                 )
             )
             lines.append(TXT_ALIGN_CT + encode_text(text_move_type))
@@ -209,7 +134,7 @@ class AccountMove(models.Model):
             + TXT_ITALICS
             + encode_text(
                 get_spaces(
-                    "MH", COMPANY.get(str(self.company_id.id), default)["serial"], True
+                    "MH", self.set_info_in_receipt().get(str(self.company_id.id), False)["serial"], True
                 )
             )
         )
@@ -220,6 +145,9 @@ class AccountMove(models.Model):
 
         data += b"\x1d\x56\x41\n"
         return data.decode("latin-1")
+    
+    def set_info_in_receipt(self):
+        return {}
     
     def remove_code_in_name(self):
         name_without_code = ''
