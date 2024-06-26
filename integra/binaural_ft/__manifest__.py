7dbe4c9d32477f094aa21996eb3676c9485b1dd3
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-06-26 08:17:53 -0400

    fix-#28863-Binaural_FT
    Se condicionaron los botones de pos

--- integra/binaural_ft/__manifest__.py
+++ integra/binaural_ft/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "16.0.1.0.1",
+    "version": "16.0.1.0.2",
     "depends": [
         "point_of_sale",
         "binaural_pos_receipt",
