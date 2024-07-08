c285cb4d9241819de9a498cf2dd130141359df49
Carlos-Lin-Binaural <carlos@binauraldev.com>
2024-07-08 10:16:51 -0400

    HOTFIX ticket#4269 Binaural_subsidiary
    Problema: Singleton
    
    Solucion: se agrego un for para poder recorrer los records
    
    Ticket: https://binaural.odoo.com/web#id=4269&menu_id=293&cids=2&action=386&model=helpdesk.ticket&view_type=form

--- integra/binaural_subsidiary/__manifest__.py
+++ integra/binaural_subsidiary/__manifest__.py
@@ -4,7 +4,7 @@
     "license": "LGPL-3",
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
-    "version": "16.0.5.2.3",
+    "version": "16.0.5.2.4",
     "category": "Accountant",
     "depends": [
         "base",
