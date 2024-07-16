6e7c43225e8df476faf32764d1b7a82597d6ed5c
christopher <christopher@binauraldev.com>
2024-07-16 16:06:09 -0400

    FEAT #30742: binaural_pos
    Problema:
    
    Bloquear por teclado la edición de cantidades de productos que tiene código/referencia y solo poder editar los productos sin código/referencia.
    
    Solución:
    
    * Mejore la validación para bloquear el ingreso de cantidades.
    
    * Corregí que cuando eliminas una linea o estableces la linea con cantidad en 0 se permita para ambos casos lineas con barcode o sin el.
    
    * Traducciones.
    
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=30742&menu_id=255&cids=2&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- integra/binaural_pos/static/src/js/ProductScreen.js
+++ integra/binaural_pos/static/src/js/ProductScreen.js
@@ -32,14 +32,26 @@ odoo.define('binaural_pos.ProductScreen', function(require) {
         })
         return res
       }
-      async _setValue(inputValue) {
+
+      is_barcode_strict_mode_invalid(barcode = -1) {
+        const activate_barcode_strict_mode = this.env.pos.config.activate_barcode_strict_mode;
+
+        if (!activate_barcode_strict_mode) return false;
+
         const order = this.env.pos.get_order();
         const selectedLine = order.get_selected_orderline();
-        const barcode = selectedLine.product.barcode;
 
-        console.log("_setValue selectedLine", { selectedLine, barcode });
+        barcode = barcode === -1 ? selectedLine.product.barcode : barcode;
 
-        if (barcode) return;
+        if (barcode) return true;
+
+      }
+
+      async _setValue(inputValue) {
+
+        const is_barcode_strict_mode_invalid = this.is_barcode_strict_mode_invalid();
+
+        if (is_barcode_strict_mode_invalid && !(inputValue == "" || inputValue == "remove")) return;
 
         return super._setValue(inputValue);
 
@@ -47,13 +59,15 @@ odoo.define('binaural_pos.ProductScreen', function(require) {
       async _clickProduct(event) {
 
         if (!this.currentOrder) {
-            this.env.pos.add_new_order();
+          this.env.pos.add_new_order();
         }
+
         const product = event.detail;
-        const barcode = product.barcode;
         const options = await this._getAddProductOptions(product);
 
-        if (barcode) return;
+        const is_barcode_strict_mode_invalid = this.is_barcode_strict_mode_invalid(product.barcode);
+
+        if (is_barcode_strict_mode_invalid) return;
 
         // this.env.pos.db.get_product_by_id(foundProductIds[0]);
         // Do not add product if options is undefined.
