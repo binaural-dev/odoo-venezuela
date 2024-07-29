da511a1d1afdafb5d01c85f9a7e04608f5c7aa72
christopher <christopher@binauraldev.com>
2024-07-29 15:11:56 -0400

    FEAT #32346: binaural_mobile
    Problema:
    
    Odoo#32346 Adaptar buscador en creación de pagos con el permiso que permite ver todos los clientes
    
    Solución:
    
    * Modifique el controlador "/payments/client" para que modifique el domain de busqueda de contactos cuando el usuario no posea el grupo group_sellers_show_all_client.
    
    Y Se retornen solo los contactos asociados a ese vendedor, de lo contrario retornar todos los contactos.
    
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=32346&cids=2&menu_id=257&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- integra/binaural_mobile/controllers/account_move_payments.py
+++ integra/binaural_mobile/controllers/account_move_payments.py
@@ -70,10 +70,12 @@ class AccountMovePayments(http.Controller):
         seller_portal_id = request.env.user.employee_id.id
 
         common_domain = [
-            ("seller_ids", "=", seller_portal_id),
             ("is_public", "=", True),
             ("type", "=", "contact"),
         ]
+
+        if not request.env.user.has_group("binaural_mobile.group_sellers_show_all_client"):
+            common_domain += [('seller_ids', '=', seller_portal_id)]
 
         domain_name = common_domain + [("name", "=ilike", "%" + (query or "") + "%")]
         domain_vat = common_domain + [("vat", "=ilike", "%" + (query or "") + "%")]
