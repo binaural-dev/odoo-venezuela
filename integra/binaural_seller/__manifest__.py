0e0ffbe174a2904336fe49a83ebd4e0bf9b15fd2
christopher <christopher@binauraldev.com>
2024-07-31 12:21:19 -0400

    FEAT #32464 #32448: binaural_mobile, binaural_subsidiary
    Problema:
    
    Quitar actualización de Sucursal base cuando se actualiza el módulo.
    
    Quitar actualización de vendedor base cuando se actualiza el módulo
    
    Solución:
    
    * A los xml de data del contacto, usuario interno y empleado vendedor que se crean agregue el artributo no_update 1 para evitar que se actualicen los valores al actualizar el modulo.
    
    * De igual manera agrague el atributo no_update 1 para los elementos xml que crean la sucursal por defecto y su plan de sucursal por defecto.
    
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=32464&menu_id=255&cids=2&action=341&model=project.task&view_type=form
    
    https://binaural.odoo.com/web#id=32448&cids=2&menu_id=255&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- integra/binaural_seller/__manifest__.py
+++ integra/binaural_seller/__manifest__.py
@@ -5,7 +5,7 @@
     "license": "LGPL-3",
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
-    "version": "16.0.1.0.3",
+    "version": "16.0.1.0.4",
     "category": "Hr/Invoicing/Account",
     "depends": [
         "account",
