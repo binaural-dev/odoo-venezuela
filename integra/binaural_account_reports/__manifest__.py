292630a6c050b0691d5bc06610d5665097386953
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:39:12 -0400

    [MIG] binaural_account_reports: Migration to 17.0

--- integra/binaural_account_reports/__manifest__.py
+++ integra/binaural_account_reports/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "16.0.5.5.4",
+    "version": "17.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": ["base", "account_reports", "binaural_accountant"],
     # always loaded
