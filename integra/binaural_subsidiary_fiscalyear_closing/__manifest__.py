33d927152cae9aee488c0efb292dfbc56e3660c7
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:40:28 -0400

    [MIG] binaural_subsidiary_fiscalyear_closing: Migration to 17.0

--- integra/binaural_subsidiary_fiscalyear_closing/__manifest__.py
+++ integra/binaural_subsidiary_fiscalyear_closing/__manifest__.py
@@ -8,7 +8,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     "category": "Technical",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": ["binaural_account_fiscalyear_closing","binaural_subsidiary"],
     # always loaded
