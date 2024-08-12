423fb7a929a712384ccd2bead28183d4767ba529
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:39:17 -0400

    [MIG] binaural_auth_sign_fix: Migration to 17.0

--- integra/binaural_auth_sign_fix/__manifest__.py
+++ integra/binaural_auth_sign_fix/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Correccion de auth signup con validacion de crear cuenta en sitioweb",
     "website": "https://binauraldev.com/",
     "license": "LGPL-3",
-    "version": "16.0.5.1.1",
+    "version": "17.0.1.0.0",
     "depends": ["binaural_website_sale", "auth_signup"],
     "data": [
         "views/auth_signup.xml",
