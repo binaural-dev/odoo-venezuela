15bd285a6c90808ca0d7ed19a877489d4a43e116
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:40:22 -0400

    [MIG] binaural_stock_distribution_seller: Migration to 17.0

--- integra/binaural_stock_distribution_seller/__manifest__.py
+++ integra/binaural_stock_distribution_seller/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "16.0.1.0.2",
+    "version": "17.0.1.0.0",
     "depends": ["binaural_seller_stock", "binaural_stock_distribution"],
     "data": [
         "views/stock_picking_distribution_views.xml",
