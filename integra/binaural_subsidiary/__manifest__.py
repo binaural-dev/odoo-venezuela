feab3f88e42a99e5be62d168757ea9f3fab8aec8
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:40:27 -0400

    [MIG] binaural_subsidiary: Migration to 17.0

--- integra/binaural_subsidiary/__manifest__.py
+++ integra/binaural_subsidiary/__manifest__.py
@@ -4,7 +4,7 @@
     "license": "LGPL-3",
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
-    "version": "16.0.6.0.1",
+    "version": "17.0.1.0.0",
     "category": "Accountant",
     "depends": [
         "base",
