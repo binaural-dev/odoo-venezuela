30e4298b02e22d2ad92f9dfd2fb797ed7bd7af7d
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:39:36 -0400

    [MIG] binaural_fiscal_inspector_delivery: Migration to 17.0

--- integra/binaural_fiscal_inspector_delivery/__manifest__.py
+++ integra/binaural_fiscal_inspector_delivery/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.1",
+    "version": "17.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": [
         "binaural_fiscal_inspector",
