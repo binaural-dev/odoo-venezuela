6f2739ec96f7d5c8b43581c6a3df4224aec273a5
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:39:53 -0400

    [MIG] binaural_payment: Migration to 17.0

--- integra/binaural_payment/__manifest__.py
+++ integra/binaural_payment/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Hidden",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": ["binaural_rate", "account_payment"],
     "auto_install": True,
