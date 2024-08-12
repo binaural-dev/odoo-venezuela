cd359715b29166aa30021c208a80d97c8f0b83ab
Anderson Armeya <anderson@binauraldev.com>
2024-08-12 16:39:34 -0400

    [MIG] binaural_fiscal: Migration to 17.0

--- integra/binaural_fiscal/__manifest__.py
+++ integra/binaural_fiscal/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.1.0.5",
+    "version": "17.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": ["base", "account_accountant", "account","sale", "sales_team",],
     # always loaded
