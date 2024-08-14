536ec696f4ccec3cbfd58d8d0fe6e9f1602d3cd0
christopher <christopher@binauraldev.com>
2024-08-14 10:57:37 -0400

    FEAT #32854: binaural_payment_extension, binaural_payroll_payment_extension
    Problema:
    
    Retención de ISLR en XML por sueldos y salarios
    
    Solución:
    
    * Cree un nuevo modulo llamado binaural_payroll_payment_extension.
    
    - El cual contiene la implmentacion de agregar lineas de deduccion islr de recibos de nomina al reporte de xml islr.
    
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=32854&cids=2&menu_id=257&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- integra/binaural_payment_extension/wizard/wizard_retention_islr.py
+++ integra/binaural_payment_extension/wizard/wizard_retention_islr.py
@@ -218,83 +218,17 @@ class RetentionIslrReport(models.TransientModel):
             table_rows.append(new_row)
 
 
-        return table_rows, row_idx 
-
-
-    # payslip
-    def _get_payslip_line_domain(self, current_company_id=False):
-        domain = [
-            ("slip_id.date_to", ">=", self.date_start), 
-            ("slip_id.date_to", "<=", self.date_end),
-            ("salary_rule_id.code", "=", "PM-ISLR"),
-            ("slip_id.state", "in", ["done", "paid"])
-        ]
-
-        if current_company_id:
-            domain += [("company_id", "=", current_company_id)]
-
-        return domain
-
-    def _get_payslip_line_ids(self, current_company_id=False):
-        domain = self._get_payslip_line_domain()
-
-        payslip_line_ids  = self.env["hr.payslip.line"].search(domain)
-
-        return payslip_line_ids
-
-    @api.model    
-    def _get_from_payslip_retention_islr_excel_row(self, row_idx, slip_line_id, is_vef_currency):
-
-        new_row = self._get_retention_islr_excel_model_row()
-
-        slip_id = slip_line_id.slip_id
-
-        new_row["ID Sec"] = row_idx
-
-        new_row["RIF Retenido"] = f"{slip_id.employee_id.prefix_vat}{slip_id.employee_id.vat}"
-
-        pi = str(slip_id.date_to)
-
-        fpi = datetime.strptime(pi, "%Y-%m-%d")
-
-        new_row["Número factura"] = 0
-
-        new_row["Control Número"] = "NA"
-
-        new_row["Fecha Operación"] = fpi.strftime("%d/%m/%Y")
-
-        new_row["Código Concepto"] = 1
-
-        new_row["Monto Operación"] = (
-            (round(slip_line_id.foreign_total, 2) if is_vef_currency else slip_line_id.total) * -1
-        )
-
-        new_row["Porcentaje de retención"] = slip_id.employee_id.porc_ari
-
-        return new_row
-
-    def _get_from_payslip_retention_islr_excel_rows(self, table_rows, row_idx, current_company=False):
-        is_vef_currency = self.env.ref("base.VEF").id == self.env.company.currency_foreign_id.id
-
-        payslip_line_ids = self._get_payslip_line_ids(current_company)
-
-        for payslip_line_id in payslip_line_ids:
-            row_idx += 1
-
-            new_row = self._get_from_payslip_retention_islr_excel_row(row_idx, payslip_line_id, is_vef_currency)
-
-            table_rows.append(new_row)
-
         return table_rows, row_idx
 
+    def _get_table_rows_sorted(self, table_rows):
+        table_rows = sorted(table_rows, key=lambda row: datetime.strptime(row['Fecha Operación'], "%d/%m/%Y"))
 
+        return table_rows
 
     def _retention_islr_excel(self, current_company=False):
         table_rows, table_rows_count = self._get_retention_islr_excel_rows([], 0, current_company)
 
-        table_rows, table_rows_count = self._get_from_payslip_retention_islr_excel_rows(table_rows, table_rows_count, current_company)
-
-        table_rows = sorted(table_rows, key=lambda row: row['Fecha Operación'])
+        table_rows = self._get_table_rows_sorted(table_rows)
 
         table = pd.DataFrame(table_rows)
 
