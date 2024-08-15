9e1a03209d7bd66dc01a219bf55c1f22fc267477
christopher <christopher@binauraldev.com>
2024-08-15 18:26:56 -0400

    FIX #4570: binaural_club_socios
    Problema:
    
    ERROR AL CREAR CONTACTO RELACIONADO
    
    Solución:
    
    * Agregue archivo de migracion para borrar si existen las funciones de sql utilizadas para el reporte de consulta de socios en mora.
    
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=4570&cids=2&menu_id=302&action=386&model=helpdesk.ticket&view_type=form
    
    Tarea de proyecto []
    Ticket de soporte [X]

--- integra/binaural_club_socios/__manifest__.py
+++ integra/binaural_club_socios/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Binauraldev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "16.0.1.0.12",
+    "version": "16.0.1.1.0",
     "depends": [
         "base",
         "base_automation",
