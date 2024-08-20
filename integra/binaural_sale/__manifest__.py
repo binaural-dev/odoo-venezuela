b01a23dc33146af7f8ff3b1390d49e13b4caae25
yeisonasuaje <yeison@binaural.com.ve>
2024-08-20 08:59:22 -0400

    fix#4575 binaural_sale -Error Bloquear confirmación de pedidos -Problema: al tener activado la configuracion de bloquear con facturas pagadas la cantidad arrojada es errada -Solucion: se agrego al dominio que solo tome en cuenta el tipo out_invoice ya que actualmente estaba tomando todo incluido los asientos -https://binaural.odoo.com/web#id=4575&cids=2&model=helpdesk.ticket&view_type=form

--- integra/binaural_sale/__manifest__.py
+++ integra/binaural_sale/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Sales/Sales",
-    "version": "16.0.1.0.16",
+    "version": "16.0.1.0.17",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
