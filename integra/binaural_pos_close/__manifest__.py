655cd3165eb630bb9fe24c5282c77f45abcb44c2
Anderson Armeya <anderson@binauraldev.com>
2024-08-20 15:32:08 -0400

    [MIG] binaural_pos_close: Migration to 17.0

--- integra/binaural_pos_close/__manifest__.py
+++ integra/binaural_pos_close/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "category": "Point of Sale",
     "website": "https://binauraldev.com/",
-    "version": "16.0.0.0.2",
+    "version": "17.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": ["base", "point_of_sale", "binaural_pos", "binaural_accountant"],
     # always loaded
