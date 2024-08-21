754f0acdec7b1fccca8094b5ed9982097fdc4bf7
Anderson Armeya <anderson@binauraldev.com>
2024-08-21 15:27:37 -0400

    [DEL] removiendo integra 17.0

--- integra/binaural_mobile/models/account_move_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, _
-from odoo.exceptions import UserError
-
-
-class AccountMoveLineInh(models.Model):
-    _inherit = "account.move.line"
--- integra/binaural_purchase/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import account_move
-from . import product_template
-from . import product_template_tariff
-from . import purchase_order
-from . import purchase_order_line
-from . import res_company
-from . import res_config_settings
