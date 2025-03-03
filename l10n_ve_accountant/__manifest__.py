19489db7d3cc43f12737714ba2b002fc957a5cb8
bin-daldana <david@binauraldev.com>
2025-03-03 12:58:33 -0400

    [FEAT][I18N] #46319: l10n_ve
    Resumen:
     - Cambio de traducciones:
      - Pagos por Cobranza: Solo para Clientes.
      - Facturas Rectificativas por Notas de Credito: Para Clientes y Proveedores.
     - Se remueve key del manifest.
    
    Tarea (Link): Cambio de etiquetas En redaccion
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "16.0.5.0.7",
+    "version": "16.0.5.0.8",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
@@ -40,5 +40,4 @@
     ],
     "images": ["static/description/icon.png"],
     "application": True,
-    "binaural": True,
 }
--- l10n_ve_rate/__manifest__.py
+++ l10n_ve_rate/__manifest__.py
@@ -7,12 +7,11 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "16.0.0.0.4",
+    "version": "16.0.0.0.5",
     # any module necessary for this one to work correctly
     "depends": ["base"],
     # always loaded
     "data": [
         "views/res_config_settings.xml",
     ],
-    "binaural": True,
 }
