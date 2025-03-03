663ffe7ba7f6eb1eaab297feda93ec28383e2350
Mauricio Istúriz <mauricio@binauraldev.com>
2025-03-03 11:56:53 -0400

    Revert "Revert "[ADD] required modules""

---
+++ l10n_ve_filter_partner/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Binaural Filtro de Contacto",
+    "summary": """
+        Modulo tècnico el cual contiene un mixin para filtrar
+        los contactos de los clientes y proveedores.
+    """,
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://www.binauraldev.com",
+    "category": "Technical",
+    "version": "16.0.0.0.1",
+    "depends": ["web_domain_field"],
+    "data": [
+        # 'security/ir.model.access.csv',
+    ],
+    "binaural": True,
+}
