76dc27fae2019f931848b4a5caceb49fab5536d5
Mauricio Istúriz <mauricio@binauraldev.com>
2025-03-03 11:52:39 -0400

    Revert "[ADD] required modules"

--- l10n_ve_currency_rate_live/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
-
-    can_update_habil_days = fields.Boolean(
-        related="company_id.can_update_habil_days", readonly=False
-    )
