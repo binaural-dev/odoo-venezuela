c7d0ae3cf33119c7d3631c0af5707729ebdeca20
Mauricio Istúriz <mauricio@binauraldev.com>
2025-03-06 23:30:30 -0400

    [ADD] l10n_ve_sale

---
+++ l10n_ve_sale/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "Venezuela - Sales",
+    "summary": """
+       Módulo para ventas """,
+    "license": "LGPL-3",
+    "author": "binaural-dev",
+    "website": "https://binauraldev.com/",
+    "category": "Sales/Sales",
+    "version": "16.0.1.1.3",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "l10n_ve_tax",
+        "sale_management",
+        "l10n_ve_rate",
+        "l10n_ve_contact",
+        "l10n_ve_invoice",
+    ],
+    # always loaded
+    "data": [
+        "security/res_groups.xml",
+        "data/ir_cron.xml",
+        "views/res_config_settings.xml",
+        "views/sale_order.xml",
+        "views/product_pricelist_item_views.xml",
+        "views/menuitems.xml",
+    ],
+    "images": ["static/description/icon.png"],
+    "application": True,
+}
