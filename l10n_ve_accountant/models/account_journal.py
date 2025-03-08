33fa337879d2e5e2ca61ee1703ff6ba293ffe24d
David Mendoza <davidmendoza@binaural.com.ve>
2025-03-08 11:32:31 -0400

    FEAT #46322: l10n_ve_accountant
    Problema: Restringir la creacion de diarios de tipo ventas y compras
    
    Solución: Cambio en el nombre de la funcion que valida el grupo del usuario.
    
    Tarea (Link): https://binaural.odoo.com/web#id=46322&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]

--- l10n_ve_accountant/models/account_journal.py
+++ l10n_ve_accountant/models/account_journal.py
@@ -7,16 +7,16 @@ class AccountJournal(models.Model):
     @api.model
     def create(self, vals):
         for record in self:
-            record._validate_user_group(vals)
+            record._validate_support_user_group(vals)
         return super().create(vals)
     
     def write(self, vals):
         for record in self:
             if "type" in vals:
-                record._validate_user_group(vals)
+                record._validate_support_user_group(vals)
         return super().write(vals)
     
-    def _validate_user_group(self,vals):
+    def _validate_support_user_group(self,vals):
         user = self.env.user
         is_support_user = user.has_group('l10n_ve_accountant.group_support_user')
 
