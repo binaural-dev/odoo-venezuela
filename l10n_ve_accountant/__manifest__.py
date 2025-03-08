23a3fc9df787502fd1c0e28f39a1013f8c8d4de9
David Mendoza <davidmendoza@binaural.com.ve>
2025-03-08 11:32:31 -0400

    FEAT #46322: l10n_ve_accountant
    Problema: Restringir la creacion de diarios de tipo ventas y compras
    
    Solución: Cambio en el nombre de la funcion que valida el grupo del usuario.
    
    Tarea (Link): https://binaural.odoo.com/web#id=46322&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "16.0.5.0.11",
+    "version": "16.0.5.0.12",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
