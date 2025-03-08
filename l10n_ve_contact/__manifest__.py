403c048f6201663fdf9b596ae9c2c8030bcfe440
OmarYepez29 <omar@binauraldev.com>
2025-03-08 14:20:36 -0400

    [IMP]Correjidas etiquetas y traducciones de la tarea que estan la descripción.
    Link:https://binaural.odoo.com/web#id=46569&cids=2&menu_id=255&action=327&active_id=306&model=project.task&view_type=form

--- l10n_ve_contact/__manifest__.py
+++ l10n_ve_contact/__manifest__.py
@@ -2,22 +2,15 @@
     "name": "Binaural contactos",
     "summary": """
        Modulo para información de contacto """,
-
-    'license': 'LGPL-3',
-    
-    'author': "Binauraldev",
-    'website': "https://binauraldev.com/",
-    'category': 'Contacts/Contacts',
-    'version': '16.0.1.0.4',
-
+    "license": "LGPL-3",
+    "author": "Binauraldev",
+    "website": "https://binauraldev.com/",
+    "category": "Contacts/Contacts",
+    "version": "16.0.1.0.5",
     # any module necessary for this one to work correctly
-    'depends': ['base','contacts','l10n_ve_rate'],
-
+    "depends": ["base", "contacts", "l10n_ve_rate"],
     # always loaded
-    'data': [
-        'views/res_partner.xml',
-        "views/res_config_settings.xml"
-    ],
+    "data": ["views/res_partner.xml", "views/res_config_settings.xml"],
     "images": ["static/description/icon.png"],
     "application": True,
 }
