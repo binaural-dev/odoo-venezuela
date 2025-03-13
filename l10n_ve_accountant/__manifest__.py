378bb80dd9af948779c747d984610d2a3131bfba
bin-daldana <david@binauraldev.com>
2025-03-12 22:07:03 -0400

    FIX: translate .po - module: l10n_ve_accountant

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "16.0.5.0.19",
+    "version": "16.0.5.0.20",
     "depends": [
         "base",
         "web",
