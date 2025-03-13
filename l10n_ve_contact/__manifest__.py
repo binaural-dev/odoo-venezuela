4282e6679c76414720c29c906c05dd0d0deeca8e
bin-daldana <david@binauraldev.com>
2025-03-12 22:28:43 -0400

    I18N: adicion de traducciones

--- l10n_ve_contact/__manifest__.py
+++ l10n_ve_contact/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Contacts/Contacts",
-    "version": "16.0.1.0.8",
+    "version": "16.0.1.0.9",
     "depends": ["base", "contacts", "l10n_ve_rate", "l10n_ve_location"],
     "data": ["views/res_partner.xml", "views/res_config_settings.xml"],
     "images": ["static/description/icon.png"],
