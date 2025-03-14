10130f1465b6822611b3f6a09f43923ce7acb1ae
bin-daldana <david@binauraldev.com>
2025-03-14 01:05:40 -0400

    [ADD] Añadido campo para diario interno y botón para crear factura interna en la configuración

--- l10n_ve_stock_account/__manifest__.py
+++ l10n_ve_stock_account/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://www.binauraldev.com",
     "category": "Stock Account",
-    "version": "16.0.1.0.30",
+    "version": "16.0.1.0.31",
     "depends": [
         "l10n_ve_stock",
         "l10n_ve_accountant",
