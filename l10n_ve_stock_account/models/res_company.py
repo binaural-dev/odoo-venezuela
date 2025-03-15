875a16648e1da005c4494aebf453af6e663d81e5
bin-daldana <david@binauraldev.com>
2025-03-15 14:44:31 -0400

    FEAT: Bring code from stock_move_invoice

--- l10n_ve_stock_account/models/res_company.py
+++ l10n_ve_stock_account/models/res_company.py
@@ -8,16 +8,22 @@ _logger = logging.getLogger(__name__)
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    # customer_journal_id = fields.Many2one(
-    #     "account.journal",
-    #     string="Customer Journal",
-    #     help="To add customer journal",
-    # )
-    # vendor_journal_id = fields.Many2one(
-    #     "account.journal",
-    #     string="Vendor Journal",
-    #     help="To add vendor journal",
-    # )
+    customer_journal_id = fields.Many2one(
+        "account.journal",
+        string="Customer Journal",
+        help="To add customer journal",
+    )
+    vendor_journal_id = fields.Many2one(
+        "account.journal",
+        string="Vendor Journal",
+        help="To add vendor journal",
+    )
+
+    internal_consigned_journal_id = fields.Many2one(
+        "account.journal",
+        string="Internal Journal",
+        help="To add internal journal",
+    )
 
     invoice_cron_type = fields.Selection(
         [("last_business_day", _("Last Business Day")), ("last_day", _("Last Day"))],
@@ -25,4 +31,5 @@ class ResCompany(models.Model):
         default="last_business_day",
         required=True,
     )
+
     invoice_cron_time = fields.Float(required=True, default=18.0)
