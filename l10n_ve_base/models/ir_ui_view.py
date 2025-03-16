21973cab77a5f52c03ba8f805aa89c7fa5a0280e
bin-daldana <david@binauraldev.com>
2025-03-16 13:30:20 -0400

    FEAT: new restrict modify views

---
+++ l10n_ve_base/models/ir_ui_view.py
@@ -0,0 +1,22 @@
+from odoo import models, api, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class IrUIView(models.Model):
+    _inherit = "ir.ui.view"
+
+    @api.model
+    def create(self, vals):
+        if not self.env.user._is_superuser():
+            raise UserError(_("Only the superuser can create system views."))
+        return super(IrUIView, self).create(vals)
+
+    def write(self, vals):
+        if not self.env.user._is_superuser():
+            raise UserError(_("Only the superuser can modify system views."))
+        return super(IrUIView, self).write(vals)
+
+    def unlink(self):
+        if not self.env.user._is_superuser():
+            raise UserError(_("Only the superuser can delete system views."))
+        return super(IrUIView, self).unlink()
