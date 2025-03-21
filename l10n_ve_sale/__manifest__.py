8e36513d2d41887c6e25f93b81dc6a7082eecd30
OmarYepez29 <omar@binauraldev.com>
2025-03-21 14:40:11 -0400

    [WIP]Trayendo cambios de mi rama a la rama de migración

--- l10n_ve_sale/__manifest__.py
+++ l10n_ve_sale/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Sales/Sales",
-    "version": "17.0.0.0.0",
+    "version": "17.0.1.1.7",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
