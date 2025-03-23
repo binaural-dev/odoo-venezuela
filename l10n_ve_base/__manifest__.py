586dce6b44ee06417c6498511874c8b3f84633cb
bin-daldana <david@binauraldev.com>
2025-03-22 22:49:52 -0400

    MIG

--- l10n_ve_base/__manifest__.py
+++ l10n_ve_base/__manifest__.py
@@ -10,11 +10,10 @@
     "version": "17.0.0.0.0",
     "depends": ["base", "web"],
     "auto_install": True,
-    "data": ["security/ir.model.access.csv", "data/ir_config_parameter.xml"],
+    "data": ["security/ir.model.access.csv"],
     "assets": {
         "web.assets_backend": [
             "l10n_ve_base/static/src/core/debug/debug_menu_items.js",
         ],
     },
 }
-
