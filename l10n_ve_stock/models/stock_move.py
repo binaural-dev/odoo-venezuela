ffc60ceae1ad0c5e8e44254e1d5348322819a1d4
bin-daldana <david@binauraldev.com>
2025-03-24 12:41:43 -0400

    Eliminar declaraciones XML innecesarias y mejorar la legibilidad del código en varios archivos.

--- l10n_ve_stock/models/stock_move.py
+++ l10n_ve_stock/models/stock_move.py
@@ -1,8 +1,4 @@
 from odoo import _, api, fields, models
-from odoo.exceptions import UserError
-
-import logging
-_logger = logging.getLogger(__name__)
 
 class StockMove(models.Model):
     _inherit = "stock.move"
@@ -11,42 +7,3 @@ class StockMove(models.Model):
     priority_location = fields.Integer(
         string="Priority", related="product_id.priority_location", store=True
     )
-
-    @api.model
-    def default_get(self, fields):
-        self.validate_block_add_line_expedition()
-        return super(StockMove, self).default_get(fields)
-    
-    def validate_block_add_line_expedition(self):  
-        
-        block_transfer_expedition = self.env.user.has_group("l10n_ve_stock.group_block_type_inventory_transfers_expeditions")
-
-        if "default_picking_id" in self.env.context:
-            if block_transfer_expedition:
-                
-                picking_type = (
-                    self.env["stock.picking.type"].search(
-                        [("id", "=", self.env.context.get("picking_type_id", False))]
-                    )
-
-                    if self.env.context.get("picking_type_id", False)
-                    else self.picking_type_id
-                )
-
-                if picking_type and picking_type.code == "outgoing":
-                    raise UserError(_("You do not have permission to add lines to this transfer."))
-
-    @api.onchange('quantity_done')
-    def _onchange_quantity_done(self):
-        for record in self:
-            self._validate_transfer_quantity(record)
-
-            
-
-    def _validate_transfer_quantity(self, record):
-            if self.env.company.validate_without_product_quantity:
-                return
-
-            if record.quantity_done > record.forecast_availability:
-                record.quantity_done = 0
-                raise UserError(_("You cannot make transfers larger than the demand."))
--- l10n_ve_stock/models/stock_move_line.py
+++ l10n_ve_stock/models/stock_move_line.py
@@ -8,7 +8,7 @@ class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
     _order = "priority_location asc"
 
-    product_tag_ids = fields.Many2many(related='product_id.product_tag_ids')
+    product_tag_ids = fields.Many2many(related="product_id.product_tag_ids")
 
     priority_location = fields.Integer(
         string="Priority", related="product_id.priority_location", store=True
@@ -18,21 +18,3 @@ class StockMoveLine(models.Model):
         res = super()._get_fields_stock_barcode()
         res.append("priority_location")
         return res
-    
-    @api.onchange('product_id')
-    def _onchange_product_id(self):
-        block_transfer_expedition = self.env.user.has_group("l10n_ve_stock.group_block_type_inventory_transfers_expeditions")
-
-        if block_transfer_expedition:
-            
-            id_str = str(self.picking_id.id)
-            
-            if "_" in id_str:
-                id_numero = id_str.split("_")[1]
-            else:
-                id_numero = id_str
-
-            stock_picking = self.env["stock.picking"].sudo().search([("id", "=", id_numero)])
-
-            if stock_picking.picking_type_id and stock_picking.picking_type_id.code == "outgoing":
-                raise UserError(_("You do not have permission to add lines to this transfer."))
