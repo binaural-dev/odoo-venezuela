2ccfad2a7a68834a92d4efeca7e43db7ee9cde31
OmarYepez29 <omar@binauraldev.com>
2025-03-25 10:16:43 -0400

    [FIXME]NESTORRRRRRRRRR

--- l10n_ve_iot_mf/models/res_config_settings.py
+++ l10n_ve_iot_mf/models/res_config_settings.py
@@ -3,14 +3,10 @@ from odoo import _, api, fields, models
 
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
-    
+
     invoice_print_type = fields.Selection(
-        selection=[
-            ('free', 'Forma Libre'),
-            ('fiscal', 'Máquina Fiscal')
-        ],
-        string='Tipo de Impresión de Facturas',
-        related='company_id.invoice_print_type',
+        string="Tipo de Impresión de Facturas",
+        related="company_id.invoice_print_type",
         readonly=False,
-        help="Determina el tipo de impresión predeterminado para facturas de cliente"
+        help="Determina el tipo de impresión predeterminado para facturas de cliente",
     )
