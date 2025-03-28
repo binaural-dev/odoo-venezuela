d06b59f05d2154301bda62157fffee90e125cec0
OmarYepez29 <omar@binauraldev.com>
2025-03-27 21:47:22 -0400

    [WIP]Vistas y alerta agregada.

--- l10n_ve_pos_mf/models/pos_config.py
+++ l10n_ve_pos_mf/models/pos_config.py
@@ -12,10 +12,14 @@ class PosConfigInherit(models.Model):
             "('company_id', '=', company_id)]"
         ),
     )
-    serial_machine = fields.Char(related="iface_fiscal_data_module.serial_machine")
+    serial_machine = fields.Char(
+        related="iface_fiscal_data_module.serial_machine")
     flag_21 = fields.Selection(related="iface_fiscal_data_module.flag_21")
-    traditional_line = fields.Boolean(related="iface_fiscal_data_module.traditional_line")
-    has_cashbox = fields.Boolean(related="iface_fiscal_data_module.has_cashbox")
+    traditional_line = fields.Boolean(
+        related="iface_fiscal_data_module.traditional_line"
+    )
+    has_cashbox = fields.Boolean(
+        related="iface_fiscal_data_module.has_cashbox")
     access_button_mf = fields.Boolean()
     message_in_head = fields.Boolean()
 
@@ -24,3 +28,10 @@ class PosConfigInherit(models.Model):
         for config in self:
             if config.is_posbox:
                 config.iot_device_ids += config.iface_fiscal_data_module
+
+    def open_ui(self):
+        if not self.is_posbox or not self.iface_fiscal_data_module:
+            raise UserError(
+                _("Necesitas activar el IOT en la caja y asignarle una máquina fiscal.")
+            )
+        return super().open_ui()
