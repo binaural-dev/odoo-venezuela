2c38d82995aaaa9d2fc6472a4a2a165616916166
OmarYepez29 <omar@binauraldev.com>
2025-04-03 15:52:30 -0400

    [ADD] l10n_ve_pos: Agregando POS para normativa venezolana   * Conversión dinámica en tiempo real con tasas BCV/paralelas   * Configuración por método de pago:     - Moneda base (BsD)     - Monedas alternativas (USD/EUR) con actualización automática     - Límites de diferencial cambiario según regulación   * Validación de tasas en:     - Apertura/cierre de sesión POS     - Procesamiento de pagos     - Reconciliación contable
    - **Núcleo Mejorado:**
      * Nuevos modelos:
        - pos.currency.rate (histórico cambiario)
        - pos.fiscal.refund (auditoría devoluciones)
      * Extensión de pos.payment:
        - Monto base en divisa
        - Diferencial cambiario
        - Tasa de referencia

---
+++ l10n_ve_pos/__manifest__.py
@@ -0,0 +1,42 @@
+{
+    "name": "Venezuela - POS (Punto de Venta)",
+    "summary": """
+        Módulo de POS (Punto de Venta) en Venezuela
+    """,
+    "license": "LGPL-3",
+    "author": "binaural-dev",
+    "category": "Point of Sale",
+    "website": "https://binauraldev.com/",
+    "version": "16.0.1.0.11",
+    "depends": [
+        "base",
+        "point_of_sale",
+        "pos_sale",
+        "l10n_ve_location",
+        "l10n_ve_rate",
+        "l10n_ve_contact",
+        "l10n_ve_stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/res_group.xml",
+        "views/pos_payment_method.xml",
+        "views/pos_order.xml",
+        "views/res_config_settings.xml",
+        "views/pos_config_views.xml",
+        "views/pos_payment_views.xml",
+        "views/report_saledetails.xml",
+        "security/res_group.xml",
+        "wizard/payment_report.xml",
+    ],
+    "images": ["static/description/icon.png"],
+    "application": True,
+    "assets": {
+        "point_of_sale.assets": [
+            "l10n_ve_pos/static/src/js/*.js",
+            "l10n_ve_pos/static/src/xml/*.xml",
+            "l10n_ve_pos/static/src/css/*.css",
+        ],
+    },
+    "l10n_ve": True,
+}
