fbb3fa61f35e4cafca62ee0d6afe9c051402ed14
rsgg04 <ruben@binauraldev.com>
2025-04-04 09:30:35 -0400

    se modifico el js para mejorar la sincronia, que quito la validacion para que siempre guarde las ordenes en contabilidad asi falle la maquina fiscal, se valido para que el boton de imprimir en maquina fiscal o imprimir forma libre se muestre dependiendo de la configuracion de impresion

--- l10n_ve_iot_mf/__manifest__.py
+++ l10n_ve_iot_mf/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "license": "LGPL-3",
     "category": "Accounting",
-    "version": "17.0.0.0.5",
+    "version": "17.0.0.0.6",
     "author": "binaural-dev",
     "website": "https://binauraldev.com",
     "depends": [
