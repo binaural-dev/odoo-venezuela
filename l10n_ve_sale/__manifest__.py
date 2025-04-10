e59d0cbc6a8ed7697439816999b28b2a6347bc00
bin-daldana <david@binauraldev.com>
2025-04-09 22:23:45 -0400

    [ADD] l10n_ve_sale: Módulo de ventas para la localización en Venezuela
    - **Modelos**:
      - **`sale.order`**: Se amplió para incluir soporte de moneda extranjera con el campo `foreign_currency_id`, y el cálculo del NIT del cliente con el campo `vat`.
      - **`sale.order.line`**: Se añadió soporte para precios y subtotales en moneda extranjera, con campos como `foreign_price`, `foreign_subtotal`, y `invoiced`.
      - **`product.pricelist.item`**: Se calculan los precios con y sin impuestos en las listas de precios de productos mediante los campos `price_with_tax` y `price_without_tax`.
    
    - **Vistas**:
      - **`sale.order`**: Personalización del formulario de orden de venta, con la inclusión de un filtro de socios (`filter_partner`) y un dominio dinámico para el campo `partner_id`.
      - **`product_pricelist_item`**: Se mejoraron las vistas de las listas de precios de productos, añadiendo los nuevos campos de precios con impuestos.
      - **`res.config.settings`**: Se adaptó la vista de configuración para las nuevas funcionalidades de ventas.
    
    - **Seguridad**:
      - Se crearon grupos y reglas de acceso para gestionar qué usuarios pueden ver y modificar ciertos campos, como el filtro de socios en la orden de venta.
    
    - **Otras funcionalidades**:
      - Soporte completo para manejar monedas extranjeras en las órdenes de venta y líneas de venta.
      - Inclusión de la funcionalidad para calcular precios y subtotales en diferentes monedas y con o sin impuestos.

---
+++ l10n_ve_sale/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "Venezuela - Ventas",
+    "summary": """
+        Módulo de Ventas Venezuela
+    """,
+    "license": "LGPL-3",
+    "author": "binaural-dev",
+    "website": "https://binauraldev.com/",
+    "category": "Sales/Sales",
+    "version": "16.0.0.0.0",
+    # any module necessary for this one to work correctly
+    "depends": [
+        "base",
+        "l10n_ve_tax",
+        "sale_management",
+        "l10n_ve_rate",
+        "l10n_ve_contact",
+        "l10n_ve_invoice",
+        "l10n_ve_filter_partner",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/res_groups.xml",
+        "data/ir_cron.xml",
+        "views/res_config_settings.xml",
+        "views/sale_order.xml",
+        "views/product_pricelist_item_views.xml",
+        "views/menuitems.xml",
+    ],
+    "images": ["static/description/icon.png"],
+    "application": True,
+}
