ea001ad0f5001e4369831d97e0266c6dfdd14ca3
bin-daldana <david@binauraldev.com>
2025-04-09 22:05:39 -0400

    [ADD] l10n_ve_filter_partner: Añadir lógica de filtrado para tipos de socio
    - Implementar un mixin `FilterPartnerMixin` que permite filtrar socios por tipo: cliente, proveedor o contacto.
    - Añadir campo `filter_partner` para seleccionar el tipo de socio y campo computado `partner_id_domain` que genera el dominio de filtrado basado en la selección.
    - Definir dominio predeterminado para clientes, proveedores y contactos, y permitir extenderlo con dominios adicionales si es necesario.
    - Añadir código JavaScript para personalizar la evaluación de dominios en el campo `partner_id` y permitir el uso de valores de campo como dominio.

---
+++ l10n_ve_filter_partner/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Venezuela - Filtro Socio",
+    "summary": "Módulo técnico que contiene un mixin para filtrar los contactos de clientes y proveedores.",
+    "license": "LGPL-3",
+    "author": "binaural-dev",
+    "website": "https://www.binauraldev.com",
+    "category": "Technical",
+    "version": "16.0.0.0.0",
+    "depends": ["web"],
+    "data": [
+        # 'security/ir.model.access.csv',
+    ],
+    "web.assets_backend": [
+        "/l10n_ve_filter_partner/static/lib/js/*.js",
+    ],
+    "installable": True,
+}
