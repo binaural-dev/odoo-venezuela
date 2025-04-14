5635fd52d37921635ddb634de12bbe496cf339f3
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-04-14 10:45:54 -0400

    FEAT #47992: l10n_ve_invoice_digital
    Refactorizacion del codigo y traducciones
    
    Tarea (Link): https://binaural.odoo.com/web?db=binaural-dev-binaural-release-10413381&token=4P6sT2NxLGgos2OwxwuG#id=47992&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_invoice_digital/models/res_config_settings.py
+++ l10n_ve_invoice_digital/models/res_config_settings.py
@@ -1,13 +1,11 @@
-from odoo import fields, models
-import logging
-
-_logger = logging.getLogger(__name__)
-
-
+from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
     username_tfhka = fields.Char(related="company_id.username_tfhka", string="Username", readonly=False)
     password_tfhka = fields.Char(related="company_id.password_tfhka", string="Password", readonly=False)
     url_tfhka = fields.Char(related="company_id.url_tfhka", string="URL", readonly=False)
- 
+    token_auth_tfhka = fields.Char(related="company_id.token_auth_tfhka", string="Token Auth", readonly=False)
+
+    def action_generate_token_tfhka(self):
+        self.company_id.generate_token_tfhka()
