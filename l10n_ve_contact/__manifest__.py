959f272e3c32d20e7bd6fb33f038bdfbc4afba7a
bin-daldana <david@binauraldev.com>
2025-04-15 11:49:49 -0400

    [IMP] l10n_ve_contact:
    Mejora homologación de estándares SENIAT
    
    - Habilitar trazabilidad para campos críticos:
      * Información básica: name, mobile
      * Ubicación: street, street2, country_id, state_id, city_id, municipality, parish_id, zip
    
    - Se hacen obligatorios los siguientes campos en la vista res_partner:
      street, street2, country_id, state_id, zip
    
    - Eliminar componentes obsoletos:
      * Archivo README heredado
      * Pruebas unitarias desactualizadas
    
    - Optimizar validaciones:
      * Reactivar y refactorizar validación de cédulas/rifs y correos electrónicos
      * Implementar validaciones por compañía (multi-company safe)
    
    - Ajustes de UI/UX:
      * Reorganizar vista de configuración para mejor flujo
      * Mejorar disposición de campos en formularios

--- l10n_ve_contact/__manifest__.py
+++ l10n_ve_contact/__manifest__.py
@@ -1,21 +1,19 @@
 {
-    "name": "Binaural contactos",
+    "name": "Venezuela - Contactos",
     "summary": """
-       Modulo para información de contacto """,
+       Módulo para información de contactos de Venezuela
+    """,
     "license": "LGPL-3",
-    "author": "Binauraldev",
+    "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Contacts/Contacts",
-    "version": "17.0.1.0.2",
-    # any module necessary for this one to work correctly
-    'depends': ['base','contacts','l10n_ve_rate'],
-
-    # always loaded
-    'data': [
-        'views/res_partner.xml',
-        "views/res_config_settings.xml"
+    "version": "17.0.0.0.1",
+    "depends": ["base", "contacts", "l10n_ve_rate", "l10n_ve_location"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner.xml",
+        "views/res_config_settings.xml",
     ],
     "images": ["static/description/icon.png"],
     "application": True,
-    "binaural": True,
 }
