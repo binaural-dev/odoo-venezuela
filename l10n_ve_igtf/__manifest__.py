8ad17dabe907c518c1bbe1317f375bfb6695c88b
ivan-binaural <ivan@binaural.dev>
2025-05-20 16:36:12 -0400

    FIX #7791: account_move problema: Al conciliar una factura, esta generaba un error debido a un campo llamado 'payment_credit.is_igtf' el cual en la homologacion se deja de usar.
    Solucion: se elimino de la validacion ese campo, ahora se pueden conciliar normalmente cada factura
    
    tarea: https://binaural.odoo.com/web#id=7791&cids=2&model=helpdesk.ticket&view_type=form

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.0.0.3",
+    "version": "17.0.0.0.4",
     "depends": [
         "base",
         "account",
