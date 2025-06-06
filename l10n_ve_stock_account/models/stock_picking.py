8671e69fc98c80a757d9b580d7aa856d3c3ccc0a
Jaco <jaco@binaural.com.ve>
2025-06-06 10:08:30 -0400

    FIX_l10n_ve_stock_account#7987
    Problema:
    -Cuando la moneda principal es diferente a Bolívares, a la hora de
    imprimir la guía de despacho, se muestra el total con el símbolo de la
    moneda principal, es requerido que siempre se muestre el de Bolívares.
    
    Solución:
    -Se crea el método get_foreign_currency_is_vef en stock.picking, la cual
    evalúa si la moneda foránea es Bolívares.
    -En el template de la guía de despacho, se agrega método
    get_foreign_currency_is_vef, se evalúa su contenido, si es verdadero, se
    setea una variable llamada foreign_symbol, la cual almacena el valor que
    trae el request,el cual es el símbolo de bolívares, y se procede a
    mostrar la variable. Si el retorno es falso, muestra el symbolo de la
    moneda principal.
    
    Tarea []
    Ticket [x]
    
    Enlace:https://binaural.odoo.com/web#id=7987&cids=2&menu_id=293&action=389&model=helpdesk.ticket&view_type=form

--- l10n_ve_stock_account/models/stock_picking.py
+++ l10n_ve_stock_account/models/stock_picking.py
@@ -973,3 +973,5 @@ class StockPicking(models.Model):
             result = result - timedelta(days=1)
 
         return f"Tienes {len(pickings_combined)} guías de despacho sin facturar al {result.strftime('%d-%m-%Y')}. De facturarse en el siguiente periodo el Seniat será Notificado."
+    def get_foreign_currency_is_vef(self):
+        return self.env.company.currency_foreign_id == self.env.ref("base.VEF")
