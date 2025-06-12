4e64f480567cd78de37fc53a761cd0947383ba79
Jaco <jaco@binaural.com.ve>
2025-06-11 20:09:54 -0400

    [FIX]l10n_ve_accountant#8030
    Problema:
    -Al cambiar el diario de una factura que venía desde una orden de venta,
    lanzaba un error de validación que mostraba "Ya existe un asiento con
    este nombre"
    
    Solución:
    -En el método Write del account.move, existía una validación que
    evaluaba si ya existía un asiento con el mismo nombre, se agregó que si
    el asiento tenía como nombre "/", lo cual demuestra que es un borrador,
    no saltara esta validación.
    
    Ticket[x]
    Tarea[]
    
    Enlace:
    https://binaural.odoo.com/web#id=8030&cids=2&menu_id=293&action=389&model=helpdesk.ticket&view_type=form

--- l10n_ve_accountant/models/account_move.py
+++ l10n_ve_accountant/models/account_move.py
@@ -307,12 +307,12 @@ class AccountMove(models.Model):
         computes the foreign debit and foreign credit of the line_ids fields (journal entries) when
         the move is edited.
         """
-
-        if 'name' in vals:
+        
+        if 'name' in vals and vals['name'] != "/":
             existing_record = self.search([('name', '=', vals['name']), ('id', '!=', self.id)], limit=1)
             if existing_record:
                 raise ValidationError(_("The operation cannot be completed: Another entry with the same name already exists."))
-
+            
         if vals.get("foreign_rate", False):
             for move in self:
                 vals.update({"last_foreign_rate": move.foreign_rate})
