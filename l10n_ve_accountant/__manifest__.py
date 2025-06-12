20df8e1fdbfa3afa93cc5c71a066ffc151ff7391
Jaco <jaco@binaural.com.ve>
2025-06-11 20:09:54 -0400

    [FIX]l10n_ve_accountant#8030
    Problema:
    -Al cambiar el diario de una factura que venía desde una orden de venta,
    lanzaba un error de validación que mostraba "Ya existe un asiento con
    este nombre"
    
    Solución:
    -En el método Write del account.move, hay una validación que evalúa si
    ya existe un asiento con el mismo nombre. Se agregó que si el asiento
    tiene como nombre "/", lo cual demuestra que es un borrador, no salte
    esta validación.
    
    Ticket[x]
    Tarea[]
    
    Enlace:
    https://binaural.odoo.com/web#id=8030&cids=2&menu_id=293&action=389&model=helpdesk.ticket&view_type=form

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "17.0.0.0.7",
+    "version": "17.0.0.0.10",
     "depends": [
         "base",
         "web",
