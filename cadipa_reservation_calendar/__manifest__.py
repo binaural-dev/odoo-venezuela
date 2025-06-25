edbca1cc2ae5132158769d469306338ac0f5cef9
joc-binaural <josealejandro@binauraldev.com>
2025-06-25 08:35:15 -0400

    feat-cadipa_calendar_reservation-task#52741
    migrating cadipa calendar from odoo 16 to 17

--- cadipa_reservation_calendar/__manifest__.py
+++ cadipa_reservation_calendar/__manifest__.py
@@ -15,12 +15,12 @@
         "views/reservation_calendar.xml",
         "views/appointment_type.xml",
     ],
-    # "assets": {
-    #     "web.assets_frontend": [
-    #         "cadipa_reservation_calendar/static/src/css/*.css",
-    #         "cadipa_reservation_calendar/static/src/js/*.js",
-    #     ]
-    # },
+    "assets": {
+        "web.assets_frontend": [
+            "cadipa_reservation_calendar/static/src/css/reservation_calendar.css",
+            "cadipa_reservation_calendar/static/src/js/reservation_calendar.js",
+        ]
+    },
     "images": ["static/description/icon.png"],
     "application": True,
 }
