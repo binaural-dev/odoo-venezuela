caddd09cfc2d145ed52148ce2ad618d534812993
joc-binaural <josealejandro@binauraldev.com>
2025-06-25 14:08:12 -0400

    feat-cadipa_calendar_reservation-task#52741
    agregando odoo module al js (necesario)

--- cadipa_reservation_calendar/static/src/js/reservation_calendar.js
+++ cadipa_reservation_calendar/static/src/js/reservation_calendar.js
@@ -1,3 +1,4 @@
+/** @odoo-module **/
 import publicWidget from '@web/legacy/js/public/public_widget';
 import { jsonrpc } from '@web/core/network/rpc_service';
 import { _t } from '@web/core/l10n/translation';
