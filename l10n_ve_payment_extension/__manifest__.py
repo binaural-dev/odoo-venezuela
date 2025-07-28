47165862a23d43318958b7282c1a5016158f0b44
rsgg04 <ruben@binauraldev.com>
2025-07-28 12:10:07 -0400

    FIX #53528: l10n_ve_payment_extension
    .-Se agregó la opción de débito a los diarios de compra y se incorporó flujo al reporte de retenciones para las notas de débito.
    
    Tarea (Link):
    https://binaural.odoo.com/web\#id\=53528\&cids\=2\&menu_id\=975\&action\=341\&model\=project.task\&view_type\=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "17.0.0.0.7",
+    "version": "17.0.0.0.8",
     "depends": [
         "base",
         "account",
