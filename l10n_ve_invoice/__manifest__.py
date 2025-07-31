ca1a92e9e4f744d4f18d97bca4713bcca081283c
OmarYepez29 <omar@binauraldev.com>
2025-07-31 16:22:56 -0400

    [IMP]Correción de errores en Pos homologado.
    Se corrije el que no deja crear pedidos con descuento desde el pos
    agregandolo al context.
    Se corrije escenario en el que cierra la orden de pos pero no imprime la
    factura

--- l10n_ve_invoice/__manifest__.py
+++ l10n_ve_invoice/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Módulo de Facturación Venezuela
     """,
-    "version": "17.0.0.0.12",
+    "version": "17.0.0.0.13",
     "license": "LGPL-3",
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
@@ -34,5 +34,5 @@
     ],
     "images": ["static/description/icon.png"],
     "application": True,
-    "pre_init_hook": "pre_init_hook"
+    "pre_init_hook": "pre_init_hook",
 }
