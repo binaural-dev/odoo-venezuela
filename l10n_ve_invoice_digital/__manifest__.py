ee03178aaf6b3324068e33124f0eca26717328c5
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-08-22 17:43:41 -0400

    FEAT #56191: l10n_ve_invoice_digital
    Se agrego un nuevo modelo en el cual se guardan las formas de pago de The factory y de este se obtiene la descripcion y codigo que se manda a digitalizar
    
    Tarea (Link): https://binaural.odoo.com/web#id=56191&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_invoice_digital/__manifest__.py
+++ l10n_ve_invoice_digital/__manifest__.py
@@ -16,10 +16,11 @@
         "l10n_ve_payment_extension",
         "stock",
     ],
-    
+
     "images": ["static/description/icon.png"],
     "application": True,
     "data": [
+        "data/payment_method_data_tfhka.xml",
         "security/ir.model.access.csv",
         "views/res_config_settings.xml",
         "views/account_move_view.xml",
@@ -28,5 +29,6 @@
         "views/stock_picking.xml",
         "wizard/account_retention_alert_views.xml",
         "views/account_journal.xml",
+        "views/payment_method_tfhka.xml",
     ],
 }
--- l10n_ve_invoice_digital/models/account_journal.py
+++ l10n_ve_invoice_digital/models/account_journal.py
@@ -5,4 +5,6 @@ from odoo.exceptions import ValidationError
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    payment_method_code = fields.Char(size=2, default="03", help="This code identifies the payment method. It is used to digitize and link the corresponding payment method.")
+    payment_method_code = fields.Many2one(
+        "payment.method.tfhka",
+        help="This code identifies the payment method. It is used to digitize and link the corresponding payment method.")
