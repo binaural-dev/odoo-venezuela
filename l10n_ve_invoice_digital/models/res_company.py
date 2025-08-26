ccfff363a5ef78282276c91ea02424ff3e594980
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-08-26 19:51:40 -0400

    IMP #56483: l10n_ve_invoice_digital
    El sistema requiere soportar dos modalidades de facturación digital con The Factory HKA (una con registro de pagos y otra sin registro de pagos), la cual que actualmente  solo permite un flujo basado en confirmación > registro de pago > digitalización, y se necesita flexibilidad para adaptarse a diferentes requisitos operativos por cliente.
    
    Tarea (Link): https://binaural.odoo.com/web?db=binaural-dev-binaural-release-10413381&token=4P6sT2NxLGgos2OwxwuG#id=56483&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_invoice_digital/models/res_company.py
+++ l10n_ve_invoice_digital/models/res_company.py
@@ -15,6 +15,7 @@ class ResCompany(models.Model):
     token_auth_tfhka = fields.Char()
     invoice_digital_tfhka = fields.Boolean()
     sequence_validation_tfhka = fields.Boolean(default=True)
+    digitalization_with_payment_tfhka = fields.Boolean(default=False)
     
     def generate_token_tfhka(self):
         self.ensure_one()
