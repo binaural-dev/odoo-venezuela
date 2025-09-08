25dd46862964f2f61438832c14d83273872288b3
joc-binaural <josealejandro@binauraldev.com>
2025-09-08 12:09:09 -0400

    l10nve_17.0_fix_ti_9057-wrong_bi_igtf_calculation
    problema: al hacer un pago que aplique igtf desde pagos, pagar una parte y el restante a otra factura, se calcula mal el bi igtf con el monto residual
    
    solucion: se crea campo de monto residdual del pago para tener control sobre cuanto le queda al pago y asi al bit_igtf mandar el monto correcto
    
    recorre todos los pagos en los que se afilio una factura y los totaliza excluyendo los pagos que sean del movimiento actual al que se trata de conciliar
    
    link ticket: https://binaural.odoo.com/web#id=9089&cids=2&menu_id=302&action=386&model=helpdesk.ticket&view_type=form

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.0.0.8",
+    "version": "17.0.0.0.10",
     "depends": [
         "base",
         "account",
