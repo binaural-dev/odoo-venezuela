19a25a41799113493fefa25b53b3b08e827c285b
pastor-binaural <pastor@binaural.dev>
2025-09-09 12:41:27 -0400

    stg_17.0_fix#8805_l10n_ve_invoice
    Problema: Al aplicar un descuento global en una factura de venta, Odoo genera automáticamente una línea de producto “Descuento” con importe negativo. Sin embargo, al intentar crear la factura, el sistema arroja un error que indica que ninguna línea puede tener importe igual o menor a cero, impidiendo completar la facturación.
    
    Solución: se agrega validacion para verificar primero el campo de "sale_discount_product_id" esta presente en la configuracion de la compañia (Este campo se agrega al momento de instalar el modulo de sales base de odoo, se agrega la validacion de esta manera por si un cliente no tiene instalado el modulo de sales, pueda seguir con el flujo normal de facturas).
    
    ticket (Link): https://binaural.odoo.com/web#id=8805&cids=2&menu_id=293&action=389&model=helpdesk.ticket&view_type=form
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_invoice/models/account_move.py
+++ l10n_ve_invoice/models/account_move.py
@@ -36,6 +36,12 @@ class AccountMove(models.Model):
     def _check_price_in_zero(self):
         for line in self.filtered(lambda m: m.is_invoice()).mapped("invoice_line_ids"):
             if line.price_unit <= 0 and line.display_type not in ("line_section","line_note"):
+                if (
+                    'sale_discount_product_id' in self.env.company._fields
+                    and self.env.company.sale_discount_product_id
+                    and line.product_id == self.env.company.sale_discount_product_id
+                ):
+                    continue
                 raise ValidationError(_("An invoice cannot have a line with a price of zero"))
 
     @api.onchange("move_type")
