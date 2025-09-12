5086c9a0e5798e7f965f27ec319a91d24d8a769b
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-09-12 16:26:40 -0400

    FEAT #56646: l10n_ve_invoice_digital, l10n_ve_dispatch_guide_digital
    Se separo lo referente a guias de despacho del modulo l10n_ve_invoice_digital y se agrego un check para activar las guias de despacho, y se creo un nuevo modulo llamado l10n_ve_dispatch_guide_digital
    
    Tarea (Link): https://binaural.odoo.com/web#id=56646&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_invoice_digital/models/res_company.py
+++ l10n_ve_invoice_digital/models/res_company.py
@@ -14,6 +14,7 @@ class ResCompany(models.Model):
     url_tfhka = fields.Char()
     token_auth_tfhka = fields.Char()
     invoice_digital_tfhka = fields.Boolean()
+    dispatch_guide_digital_tfhka = fields.Boolean()
     sequence_validation_tfhka = fields.Boolean(default=True)
     
     def generate_token_tfhka(self):
