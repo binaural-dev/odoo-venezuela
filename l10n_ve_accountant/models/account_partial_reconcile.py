aa387a817520ce279970cf0f9f6edadb2fac8fbb
OmarYepez29 <omar@binauraldev.com>
2025-09-17 16:03:30 -0400

    [FIX]Add foreign_credit and foreign_debit to account_partial_reconcile model. Se agregan los campos de foreign_credit y foreign_debit al modelo,esto para evitar diferencias debido al calculo del monto base por la tasa. Link:https://binaural.odoo.com/web#id=9133&cids=2&menu_id=302&action=386&model=helpdesk.ticket&view_type=form

--- l10n_ve_accountant/models/account_partial_reconcile.py
+++ l10n_ve_accountant/models/account_partial_reconcile.py
@@ -14,3 +14,25 @@ class AccountPartialReconcile(models.Model):
         store=True,
         index=True,
     )
+    foreign_credit = fields.Monetary(
+        string="Foreign Credit",
+        related="credit_move_id.foreign_balance",
+        currency_field="credit_foreign_currency_id",
+        store=True,
+    )
+    foreign_debit = fields.Monetary(
+        string="Foreign Debit",
+        related="debit_move_id.foreign_balance",
+        currency_field="debit_foreign_currency_id",
+        store=True,
+    )
+    credit_foreign_currency_id = fields.Many2one(
+        related="credit_move_id.foreign_currency_id",
+        store=True,
+        index=True,
+    )
+    debit_foreign_currency_id = fields.Many2one(
+        related="debit_move_id.foreign_currency_id",
+        store=True,
+        index=True,
+    )
