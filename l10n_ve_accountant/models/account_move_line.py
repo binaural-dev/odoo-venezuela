b1bf35608f973a721a00727e9c20788062a94302
rsgg04 <ruben@binauraldev.com>
2025-09-18 10:14:41 -0400

    .- Se agrega total a pagar en el monto foraneo. .- Se toma en consideración principalmente la moneda configurada en el diario de la retención, de no tener moneda configurada, tomara la moneda principal de la compa;ia.

--- l10n_ve_accountant/models/account_move_line.py
+++ l10n_ve_accountant/models/account_move_line.py
@@ -98,16 +98,15 @@ class AccountMoveLine(models.Model):
                 and not line.move_id.is_invoice(True)
                 and line.move_id.payment_id
             ):
-                if (
-                    line.move_id.payment_id.foreign_inverse_rate != 0
-                    and line.amount_currency != 0
-                ):
-                    line.balance = line.company_id.currency_id.round(
-                        line.amount_currency
-                        / line.move_id.payment_id.foreign_inverse_rate
-                    )
-                else:
+                if line.amount_currency == 0:
+                    return
+
+                if line.move_id.payment_id.foreign_inverse_rate <= 0:
                     raise UserError(_("The rate should be greater than zero"))
+
+                line.balance = line.company_id.currency_id.round(
+                    line.amount_currency / line.move_id.payment_id.foreign_inverse_rate
+                )
 
     @api.depends("product_id", "move_id.name")
     def _compute_name(self):
@@ -372,6 +371,9 @@ class AccountMoveLine(models.Model):
                 amount_currency
             ):
                 return abs(amount_currency / balance)
+            
+            return 1.0
+        
 
         aml = aml_values["aml"]
         other_aml = (other_aml_values or {}).get("aml")
