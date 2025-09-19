57f8e2650fe575c8b79e0fd439d47015df0997fd
rsgg04 <ruben@binauraldev.com>
2025-09-19 10:46:30 -0400

    Se toma desde el wizard el monto a pagar desde el tax_totals para asi no tener que volverlo a recalcular

--- l10n_ve_accountant/models/account_move.py
+++ l10n_ve_accountant/models/account_move.py
@@ -97,19 +97,29 @@ class AccountMove(models.Model):
     
     foreign_inverse_rate_vef = fields.Float(compute="_compute_inverse_rate_vef",store=True)
 
-    @api.depends('invoice_date', 'date')
+    @api.depends('invoice_date', 'date', 'company_id.currency_foreign_id')
     def _compute_inverse_rate_vef(self):
-        for move in self:           
+        Rate = self.env['res.currency.rate']
+        for move in self:
+            currency = move.company_id.currency_foreign_id
+            rate = False
 
-            currency_vef = self.env["res.currency"].search([("id", "=", move.company_id.currency_foreign_id.id)], limit=1)
-            rate_record = self.env["res.currency.rate"].search(
-                [("currency_id", "=", currency_vef.id), ("name", "=", move.invoice_date or move.date)],
-                limit=1
-            )
-            move.foreign_inverse_rate_vef = rate_record.inverse_company_rate if rate_record else 0.0
+            if currency:
+                date = move.invoice_date or move.date
 
-    amount = fields.Float(tracking=True)
-    
+                if date:
+                    rate = Rate.search([
+                        ('currency_id', '=', currency.id),
+                        ('name', '=', date),
+                    ], limit=1)
+
+                if not rate:
+                    rate = Rate.search([
+                        ('currency_id', '=', currency.id),
+                    ], order='name desc', limit=1)
+
+            move.foreign_inverse_rate_vef = rate.inverse_company_rate if rate else 0.0
+
     @api.model
     def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
         context = self.with_context(active_test=False)
@@ -922,16 +932,6 @@ class AccountMove(models.Model):
         """
         Add the foreign rate and foreign inverse rate to the context of the action_register_payment.
         """
-        total_foreign_paid = 0
-
-        foreign_currency_id = self.env.company.currency_foreign_id
-
-        total_decimal_places = foreign_currency_id.decimal_places if foreign_currency_id else 2
-
-        for move in self:
-
-            total_foreign_paid = move.tax_totals['foreign_total_amount_paid'] - move.tax_totals['foreign_amount_total']
-            
         if len(set(self.mapped("foreign_rate"))) > 1:
             raise UserError(
                 _("You can only register payments for one foreign rate at a time.")
@@ -940,8 +940,6 @@ class AccountMove(models.Model):
         res = super().action_register_payment()
         res["context"]["default_foreign_rate"] = self[0].foreign_rate
         res["context"]["default_foreign_inverse_rate"] = self[0].foreign_inverse_rate
-        res["context"]["default_foreign_inverse_rate_vef"] = self[0].foreign_inverse_rate_vef
-        res["context"]["default_foreign_total_billed"] = float_round(total_foreign_paid,precision_digits=total_decimal_places)
         
         return res
 
