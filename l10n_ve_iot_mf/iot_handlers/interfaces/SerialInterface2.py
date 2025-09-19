a379a1c984846ba3303d12f0230496b3e1a11a0e
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-09-19 09:05:53 -0400

    FEAT #57355: l10n_ve_iot_mf
    1. Agregar campo de configuración:
    
    - Incluir un checkbox en el formulario de la caja IoT con la etiqueta "¿Tiene pinpad?".
    
    - Este campo permitirá al usuario indicar si la caja estará conectada a un dispositivo pinpad.
    
    2. Desplegar lista de puertos:
    
    - Al activar el checkbox, se debe mostrar una lista desplegable con los puertos IoT disponibles.
    
    - El usuario podrá seleccionar el puerto específico al que estará conectado el pinpad.
    
    Tarea (Link): https://binaural.odoo.com/web?db=binaural-dev-binaural-release-10413381&token=4P6sT2NxLGgos2OwxwuG#id=57355&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_iot_mf/iot_handlers/interfaces/SerialInterface2.py
+++ l10n_ve_iot_mf/iot_handlers/interfaces/SerialInterface2.py
@@ -7,6 +7,8 @@ import urllib3
 import json
 from odoo.addons.hw_drivers.iot_handlers.interfaces.SerialInterface import SerialInterface
 from odoo.addons.hw_drivers.tools import helpers
+from odoo import _
+import logging
 
 _logger = logging.getLogger(__name__)
 
@@ -16,22 +18,25 @@ class SerialInterface(SerialInterface):
 
     def get_devices(self):
         serial_devices = {}
-        try:
-            if platform.system() == "Windows":
-                server = helpers.get_odoo_server_url()
-                urllib3.disable_warnings()
-                http = urllib3.PoolManager(cert_reqs="CERT_NONE")
-                waiting = http.request(
-                    "GET",
-                    server + "/iot_blacklist/ports",
-                )
+        pinpad_ports = []
 
-                b_body = waiting._body
-                body = json.loads(b_body.decode("utf-8"))
+        if platform.system() == "Windows":
+            server = helpers.get_odoo_server_url()
+            mac = helpers.get_mac_address()
+            urllib3.disable_warnings()
+            http = urllib3.PoolManager(cert_reqs="CERT_NONE")
+            try:
+                resp = http.request("GET", server + "/iot_pinpad/ports")
+                if resp.status == 200:
+                    data = json.loads(resp.data.decode("utf-8"))
+                    pinpad_ports = data.get(mac, [])
+            except Exception as e:
+                _logger.error("Error getting pinpad ports: %s", e)
 
-                for port in serial.tools.list_ports.comports():
-                    if(port.device in body[helpers.get_mac_address()]):
-                        continue
+            for port in serial.tools.list_ports.comports():
+                if pinpad_ports and port.device in pinpad_ports:
+                    _logger.warning("Port disabled (Pinpad): %s", port.device)
+                    continue
 
                     serial_devices[port.device] = {
                         'identifier': port.device
