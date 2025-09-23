067f6050e9682c78c884e177b24d5b4db142fc1e
joc-binaural <josealejandro@binauraldev.com>
2025-09-23 17:51:05 -0400

    17.0_fix_ti_9089-igtf_calculation_from_payment_widget
    problema: los pagos de retenciones viajan como anticipos y no permite hacer retencion en cero
    solucion: se comenta validacion para retenciones en cero y se agrega computado para el check de anticipo en los pagos para que los casos que sus diarios sean de retenciones automaticamente se descarte
    
    link ticket https://binaural.odoo.com/web#id=9089&cids=2&menu_id=293&action=389&model=helpdesk.ticket&view_type=form

--- l10n_ve_payment_extension/models/account_retention.py
+++ l10n_ve_payment_extension/models/account_retention.py
@@ -812,6 +812,7 @@ class AccountRetention(models.Model):
             
             lines = payment.move_id.line_ids.filtered(lambda l: l.account_id.account_type == "liability_payable" and l.credit > 0)
             if not lines:
+
                 raise ValidationError(_("No registered lines found in the move to reconcile."))
             line_to_reconcile = lines[0]
             
@@ -821,19 +822,22 @@ class AccountRetention(models.Model):
         
         if payment.payment_type == "outbound":
             
-            lines = payment.move_id.line_ids.filtered(lambda l: l.account_id.account_type == "asset_receivable" and l.debit > 0)
+            lines = payment.move_id.line_ids.filtered(lambda l: l.account_id.account_type == "asset_receivable" and l.debit >= 0)
             
             if not lines:
                 raise ValidationError(_("No registered lines found in the move to reconcile."))
+            
             line_to_reconcile = lines[0]
             
             payment.retention_line_ids.move_id.js_assign_outstanding_line(line_to_reconcile.id)
         
         elif payment.payment_type == "inbound":
-            lines = payment.move_id.line_ids.filtered(lambda l: l.account_id.account_type == "asset_receivable" and l.credit > 0)
+            lines = payment.move_id.line_ids.filtered(lambda l: l.account_id.account_type == "asset_receivable" and l.credit >= 0)
             
-            if not lines:
-                raise ValidationError(_("No registered lines found in the move to reconcile."))
+            #se comenta para probar en caso de retenciones con montos en cero
+
+            # if not lines:
+            #     raise ValidationError(_("No registered lines found in the move to reconcile."))
             line_to_reconcile = lines[0]
 
             payment.retention_line_ids.move_id.js_assign_outstanding_line(line_to_reconcile.id)
