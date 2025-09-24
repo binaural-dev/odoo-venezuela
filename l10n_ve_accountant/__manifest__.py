035acb58226d773ab8d9c8120397589958fd660c
SaulOrte <saul@binaural.dev>
2025-09-24 14:15:33 -0400

    No se puede alterar el credito y debito alterno debido a restriccion en asientos con campo foreign_recalculate en valor verdadero

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
 
-    "version": "17.0.0.0.18",
+    "version": "17.0.0.0.19",
     "depends": [
         "base",
         "web",
