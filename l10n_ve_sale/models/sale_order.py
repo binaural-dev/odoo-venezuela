14ae2cea4775caf120de3fda03d81eecf8188be2
ivan-binaural <ivan@binaural.dev>
2025-10-15 14:26:30 -0400

    fix horrendous syntax error...

--- l10n_ve_sale/models/sale_order.py
+++ l10n_ve_sale/models/sale_order.py
@@ -78,7 +78,7 @@ class SaleOrder(models.Model):
 
     pricelist_id = fields.Many2one(
         domain=lambda self: (
-            "[('company_id', 'in', (company_id, False)),"
+            "[('company_id', 'in', (company_id, False))]"
         )
     )
 
