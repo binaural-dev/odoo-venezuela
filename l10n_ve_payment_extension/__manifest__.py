54a001ec55efd647f611afc28d96c0aca6338000
ivan-binaural <ivan@binaural.dev>
2025-10-23 11:49:20 -0400

    [FIX]Corrijiendo error al instalar el modulo. l10n_ve_payment_extension #58513
    Corrijiendo error al intentar instalar debido a las vistas.
    
    Link:https://binaural.odoo.com/web/#id=58513&cids=2&menu_id=975&action=341&model=project.task&view_type=form

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "1.0",
+    "version": "1.1",
     "depends": [
         "base",
         "account",
