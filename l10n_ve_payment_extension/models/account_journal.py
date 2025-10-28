dc7c935538478563898402c5ab98eae2f8ff9be2
rsgg04 <ruben@binauraldev.com>
2025-10-28 15:15:13 -0400

    FEAT 58139#: binaural_mobile (Migration)
    Se suben ajustes correspondientes para la migración de binaural_mobile a la versión 19.0, se indican las siguientes observaciones:
    .- Los módulos modificados para poder ser instalados fueron los siguientes:
    	*l10n_ve_location
    	*l10n_ve_payment_extension
    	*l10n_ve_stock
    
    Tarea (Link):
    https://binaural.odoo.com/web\#id\=58139\&cids\=2\&menu_id\=975\&action\=341\&model\=project.task\&view_type\=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_payment_extension/models/account_journal.py
+++ l10n_ve_payment_extension/models/account_journal.py
@@ -5,8 +5,9 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     default_account_id = fields.Many2one(
+        # ('deprecated', '=', False), 
         domain=(
-            "[('deprecated', '=', False), ('company_ids', 'in', company_id),"
+            "[('company_ids', 'in', company_id),"
             "'|',('account_type', '=', default_account_type),"
             "('account_type', 'in', ('income', 'income_other') if type == 'sale' else ('expense', 'expense_depreciation', 'expense_direct_cost') if type == 'purchase' else ('asset_current', 'liability_current'))]"
         )
--- l10n_ve_stock/models/stock_picking.py
+++ l10n_ve_stock/models/stock_picking.py
@@ -17,11 +17,11 @@ class StockPicking(models.Model):
     def _get_action_picking_delivery_type(self, picking_type):
         # action = self.env["ir.actions.actions"]._for_xml_id("stock.action_picking_tree_all")
         pickings = self.env["stock.picking"]
-        if self.group_id:
+        if self.reference_ids:
             pickings = self.search(
                 [
                     "&",
-                    ("group_id", "=", self.group_id.id),
+                    ("reference_ids", "=", self.reference_ids.id),
                     ("type_delivery_step", "=", picking_type),
                 ]
             )
@@ -52,7 +52,7 @@ class StockPicking(models.Model):
             default_partner_id=self.partner_id.id,
             default_picking_type_id=picking_id.picking_type_id.id,
             default_origin=self.name,
-            default_group_id=picking_id.group_id.id,
+            default_reference_ids=picking_id.reference_ids.id,
             default_type_delivery_step=picking_type,
         )
         return action
@@ -73,11 +73,11 @@ class StockPicking(models.Model):
     ##TODO Considerar si se pueden refactorizar estas funciones y dejar una sola a la que se le pase
     ###### el tipo de picking.
     def _get_picks(self, assigned=False):
-        if not self.group_id:
+        if not self.reference_ids:
             return self.env["stock.picking"]
         domain = [
             "&",
-            ("group_id", "=", self.group_id.id),
+            ("reference_ids", "=", self.reference_ids.id),
             ("type_delivery_step", "=", "pick"),
             ("id", "!=", self.id),
         ]
@@ -90,11 +90,11 @@ class StockPicking(models.Model):
         return self.search(domain)
 
     def _get_packs(self, assigned=False):
-        if not self.group_id:
+        if not self.reference_ids:
             return self.env["stock.picking"]
         domain = [
             "&",
-            ("group_id", "=", self.group_id.id),
+            ("reference_ids", "=", self.reference_ids.id),
             ("type_delivery_step", "=", "pack"),
             ("id", "!=", self.id),
         ]
@@ -107,11 +107,11 @@ class StockPicking(models.Model):
         return self.search(domain)
 
     def _get_outs(self, assigned=False):
-        if not self.group_id:
+        if not self.reference_ids:
             return self.env["stock.picking"]
         domain = [
             "&",
-            ("group_id", "=", self.group_id.id),
+            ("reference_ids", "=", self.reference_ids.id),
             ("type_delivery_step", "=", "out"),
             ("id", "!=", self.id),
         ]
@@ -164,13 +164,11 @@ class StockPicking(models.Model):
         for val in vals_list:
             self.validate_block_transfers_expedition(vals=val)
         res = super().create(vals_list)
-        self.move_line_ids_without_package.sorted(key=lambda x: x.priority_location)
         self.move_line_ids.sorted(key=lambda x: x.priority_location)
         return res
 
     def write(self, vals):
         res = super().write(vals)
-        self.move_line_ids_without_package.sorted(key=lambda x: x.priority_location)
         self.move_line_ids.sorted(key=lambda x: x.priority_location)
         keys_to_check = [
             "move_line_ids_without_package",
