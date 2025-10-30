dc01e317ec78d8817ae222fd3a6a7f43e6f8280f
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-10-30 10:29:31 -0400

    FEAT #9703: l10n_ve_invoice
    Problema: El boton cancelar debe aparecer disponible en las notas de debito y credito si forman parte del mismo período fiscal.
    
    Solución: Cálculo de entry_in_period y control de visibilidad del botón de cancelar
    
    - Se agregó un campo booleano computado `entry_in_period` en `account.move` para identificar si una nota de débito o crédito está dentro del período fiscal vigente.
    - La lógica considera el tipo de contribuyente (especial, ordinario, formal) y la fecha de la factura en relación con el límite del período.
    - Se actualizó la vista XML para incluir el campo y condicionar la visibilidad del botón de cancelar: solo se muestra si el documento está dentro del período fiscal y cumple con los criterios de estado y tipo.
    
    Tarea (Link): https://binaural.odoo.com/web?db=binaural-dev-binaural-release-10413381&token=4P6sT2NxLGgos2OwxwuG#id=9703&cids=2&menu_id=302&action=386&model=helpdesk.ticket&view_type=form
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_invoice/models/account_move.py
+++ l10n_ve_invoice/models/account_move.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import json
 import logging
 
@@ -38,6 +38,39 @@ class AccountMove(models.Model):
         compute="_compute_is_debit_journal",
         store=True
     )
+
+    entry_in_period = fields.Boolean(
+        compute="_compute_entry_in_period",
+    )
+
+    @api.depends("invoice_date", "entry_in_period", "state")
+    def _compute_entry_in_period(self):
+        """Computing that allows determining whether a debit or credit note is within the current fiscal period."""
+        today = date.today()
+        taxpayer_type = self.env.company.taxpayer_type
+        period_limit = self._get_period_limit(today, taxpayer_type)
+
+        for move in self:
+            move.entry_in_period = False
+
+            if move.state == "cancel":
+                continue
+
+            if move.move_type == "out_refund" or (move.move_type == "out_invoice" and move.debit_origin_id):
+                if (move.invoice_date.year, move.invoice_date.month) == (period_limit.year, period_limit.month) and move.invoice_date <= period_limit:
+                    if taxpayer_type == "special" and move.invoice_date.day < 15 < period_limit.day:
+                        move.entry_in_period = False
+                    else:
+                        move.entry_in_period = True
+
+    def _get_period_limit(self, today, taxpayer_type):
+            """Returns the tax period deadline according to the taxpayer type."""
+            if taxpayer_type == "special":
+                if today.day < 15:
+                    return today.replace(day=15)
+            last_day = date(today.year, today.month, 28) + timedelta(days=4)
+            return last_day - timedelta(days=1)
+
     @api.constrains("invoice_line_ids")
     def _check_price_in_zero(self):
         from_pos = self.env.context.get('from_pos', False)
