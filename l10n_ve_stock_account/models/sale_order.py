41151a820a24572137fa333ad3a22ba46b3afe85
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-10-30 17:49:12 -0400

    modificando logica de _compute_document

--- l10n_ve_stock_account/models/sale_order.py
+++ l10n_ve_stock_account/models/sale_order.py
@@ -57,11 +57,11 @@ class SaleOrder(models.Model):
             if order.warehouse_id and order.warehouse_id.is_consignation_warehouse:
                 order.document = "invoice"
 
-    @api.depends("compute_document")
+    @api.depends("compute_document", "document")
     def _compute_document(self):
         for order in self:
-            if not order.compute_document:
-                order.compute_document = order._default_document()
+            order.compute_document = order._default_document()
+                
             if order.env.user.has_group('l10n_ve_stock_account.group_not_dispatch_guide'):
                 order.document = order.compute_document or order._default_document()
 
