cc58775cc9ac031d2374b17b448334763e8ae471
ivan-binaural <ivan@binaural.dev>
2025-10-30 13:54:28 -0400

    [MIG] 58220 l10n_ve_purchase
    Corrijiendo error al intentar instalar y al pasar facturas de proveedores
    Link:https://binaural.odoo.com/web#id=58454&cids=2&model=project.task&view_type=form

--- l10n_ve_sale/models/sale_order.py
+++ l10n_ve_sale/models/sale_order.py
@@ -475,23 +475,23 @@ class SaleOrder(models.Model):
                             line.product_uom_qty,
                         )
                         raise ValidationError(msg)
-
-            if (
-                order.company_id.account_use_credit_limit
-                and order.partner_id.use_partner_credit_limit_order
-            ):
-                total_pay = order.partner_id.credit + order.amount_total
-                if total_pay > order.partner_id.credit_limit:
-                    decimal_places = order.currency_id.decimal_places
-                    raise ValidationError(
-                        _(
-                            "No se ha confirmado el presupuesto. Límite de crédito excedido. La cuenta por cobrar del cliente es de %s más %s en presupuesto da un total de %s superando el límite de ventas de %s. Por favor cancele el presupuesto o comuníquese con el administrador para aumentar el límite de crédito del cliente.",
-                            round(order.partner_id.credit, decimal_places),
-                            round(order.amount_total, decimal_places),
-                            round(total_pay, decimal_places),
-                            round(order.partner_id.credit_limit, decimal_places),
+            
+                if (
+                    order.company_id.account_use_credit_limit
+                    and order.partner_id.use_partner_credit_limit_order
+                ):
+                    total_pay = order.partner_id.credit + order.amount_total
+                    if total_pay > order.partner_id.credit_limit:
+                        decimal_places = order.currency_id.decimal_places
+                        raise ValidationError(
+                            _(
+                                "No se ha confirmado el presupuesto. Límite de crédito excedido. La cuenta por cobrar del cliente es de %s más %s en presupuesto da un total de %s superando el límite de ventas de %s. Por favor cancele el presupuesto o comuníquese con el administrador para aumentar el límite de crédito del cliente.",
+                                round(order.partner_id.credit, decimal_places),
+                                round(order.amount_total, decimal_places),
+                                round(total_pay, decimal_places),
+                                round(order.partner_id.credit_limit, decimal_places),
+                            )
                         )
-                    )
 
                 order._block_valid_confirm()
 
