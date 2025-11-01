9487ec2693c23aad02f100fdc2d0718ab94711dd
Jaco <jaco@binaural.com.ve>
2025-11-01 16:31:37 -0400

    FIX #59281: l10nve_sale
    Problema:
    -Al intentar registrar un pago de cliente, muestra un error.
    
    Solución:
    -Se refactoriza la función action_confirm para que pueda acceder correctamente a los valores de order, y recorra la respectiva lógica.
    Tarea (Link):
    https://binaural.odoo.com/web#id=59281&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_sale/models/sale_order.py
+++ l10n_ve_sale/models/sale_order.py
@@ -452,17 +452,16 @@ class SaleOrder(models.Model):
                 )
 
     def action_confirm(self):
-        for order in self:
-            if not order.order_line or all(line.display_type for line in order.order_line):
-                raise UserError(_("Before confirming an order, you need to add a product."))
         skip_not_allow_sell_products_validation = self.env.context.get(
             "skip_not_allow_sell_products_validation", False
         )
-        if (
-            self.env.company.not_allow_sell_products
-            and not skip_not_allow_sell_products_validation
-        ):
-            for order in self:
+        for order in self:
+            # Validación de líneas de producto
+            if not order.order_line or all(line.display_type for line in order.order_line):
+                raise UserError(_("Before confirming an order, you need to add a product."))
+
+            # Validación de productos no permitidos para la venta y límite de crédito
+            if self.env.company.not_allow_sell_products and not skip_not_allow_sell_products_validation:
                 for line in order.order_line:
                     if (
                         line.product_id.detailed_type == "product"
@@ -476,6 +475,7 @@ class SaleOrder(models.Model):
                         )
                         raise ValidationError(msg)
             
+
                 if (
                     order.company_id.account_use_credit_limit
                     and order.partner_id.use_partner_credit_limit_order
@@ -493,7 +493,7 @@ class SaleOrder(models.Model):
                             )
                         )
 
-                order._block_valid_confirm()
+                    order._block_valid_confirm()
 
 
         res = super().action_confirm()
