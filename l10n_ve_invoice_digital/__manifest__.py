8bce69a480cf026baeb483e84e50a347a0e6351c
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-11-04 19:10:49 -0400

    FIX: l10n_ve_invoice_digital, l10n_ve_dispatch_guide_digital
    Validación de numeración y búsqueda segura de series en: account_retention, account_move, stock_picking
    
    - Se refactorizó `query_numbering` para diferenciar entre rangos agotados y series no configuradas en TFHKA.
    - Además, se mejoró la lógica de búsqueda de series para evitar errores cuando no se encontraba coincidencia en el primer recorrido,
    asegurando que `start_number` y `end_number` solo se evalúen cuando la serie es válida.
    - Agregando traducciones.
    
    Tarea (Link):
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_invoice_digital/__manifest__.py
+++ l10n_ve_invoice_digital/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.0.0.11",
+    "version": "17.0.0.0.13",
     "depends": [
         "account",
         "l10n_ve_igtf",
