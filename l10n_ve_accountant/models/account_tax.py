0b54c83049d7c2455c0dfbd9153c18278b0b5a1c
ivan-binaural <ivan@binaural.dev>
2025-11-10 10:34:16 -0400

    FEAT l10n_ve_accountant #58983
    Corrijiendo vistas para que se muestren solo los calculos en bolivares a peticion del cliente en un componente personalizado
    
    agregando dichos campos en el componente de javascript tax_totals para que se reflejen en las vistas correspondientes
    
    y se toma en cuenta como estan organizados tanto en backend como en front-end.
    
    Ajuste sugerido por omar, en donde se tiene en cuenta los montos de la moneda local, para la conversion, queda probar con diferentes monedas, debido a que mi ambiente esta algo raro al no mostrar el campo de moneda correspondiente
    
    Link:https://binaural.odoo.com/web/#id=58983&cids=2&menu_id=975&action=341&model=project.task&view_type=form

--- l10n_ve_accountant/models/account_tax.py
+++ l10n_ve_accountant/models/account_tax.py
@@ -81,6 +81,24 @@ class AccountTax(models.Model):
             value=res.get('total_amount_currency', 0.0),
             currency_obj=currency_id
         )
+
+        #only VES amounts
+        res['formatted_base_amount_currency_ves'] = formatLang(
+            env=self.env,
+            value=res.get('base_amount', 0.0),
+            currency_obj=ves_currency
+        )
+        res['formatted_tax_amount_currency_ves'] = formatLang(
+            env=self.env,
+            value=res.get('tax_amount', 0.0),
+            currency_obj=ves_currency
+        )
+        res['formatted_total_amount_currency_ves'] = formatLang(
+            env=self.env,
+            value=res.get('total_amount', 0.0),
+            currency_obj=ves_currency
+        )
+    
         # Foraneos
         res['formatted_base_amount_foreign_currency'] = formatLang(
             env=self.env,
