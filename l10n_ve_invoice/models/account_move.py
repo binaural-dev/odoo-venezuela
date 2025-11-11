357e1453ca79bca30cdb5be5d69ddac7063e2296
pastor-binaural <pastor@binaural.dev>
2025-11-11 16:48:33 -0400

    17.0_fix#8805_l10nve_ve_invoice_loyalty
    Problema: Se requiere un modulo intermedio entre el modulo de compras y las promociones de loyalty para que no las validaciones al momento de crear una factura con alguna linea en 0 que pertenezca a los productos de descuentos no afecten al flujo.
    
    Solución: Se crea el modulo intermediario entre ventas y promociones para poder validar si alguna de las lineas de los productos de descuento tiene un precio menor a 0 sean ignorados por la validacion de odoo venezuela.
    
    Tarea (Link): https://binaural.odoo.com/web#id=8805&cids=2&menu_id=293&action=389&model=helpdesk.ticket&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_invoice/models/account_move.py
+++ l10n_ve_invoice/models/account_move.py
@@ -37,21 +37,13 @@ class AccountMove(models.Model):
         from_pos = self.env.context.get('from_pos', False)
         for line in self.filtered(lambda m: m.is_invoice()).mapped("invoice_line_ids"):
             if line.price_unit <= 0 and line.display_type not in ("line_section","line_note"):
+                from_loyalty = self.env.context.get('from_loyalty', False)
                 if (
-                    'sale_discount_product_id' in self.env.company._fields
-                    and self.env.company.sale_discount_product_id
+                    self.env.company.sale_discount_product_id
                     and line.product_id == self.env.company.sale_discount_product_id
                 ):
                     continue
-                
-                if 'loyalty.reward' in self.env:
-                    is_loyalty_reward = self.env['loyalty.reward'].search_count([
-                        ('discount_line_product_id', '=', line.product_id.id)
-                    ]) > 0
-                    if is_loyalty_reward:
-                        continue
-
-                if not from_pos:
+                if not from_pos and not from_loyalty:
                     raise ValidationError(_("An invoice cannot have a line with a price of zero"))
 
     @api.onchange("move_type")
