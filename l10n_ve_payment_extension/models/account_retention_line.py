a8f781749fec2e4c959d929ad2718b1289cea7c8
SaulOrte <saul@binaural.dev>
2025-11-12 16:04:38 -0400

    se corrio el campo 'code' en lineas de retencion el mismo siempre obtenia el mismo codigo para cada linea de retencion en base a los conceptos de pago, se cambio el campo depend a un campo computado almacenado.

--- l10n_ve_payment_extension/models/account_retention_line.py
+++ l10n_ve_payment_extension/models/account_retention_line.py
@@ -64,11 +64,17 @@ class AccountRetentionLine(models.Model):
     payment_concept_id = fields.Many2one(
         "payment.concept", "Payment concept", ondelete="cascade", index=True
     )
-    code=fields.Char(
-        related="payment_concept_id.line_payment_concept_ids.code"
+    
+    code = fields.Char(
+        string='Code',
+        compute='_compute_code',
+        store=True, 
+        readonly=False
     )
-    code_visible=fields.Boolean(
+
+    code_visible = fields.Boolean(
         related='company_id.code_visible')
+    
     economic_activity_id = fields.Many2one(
         "economic.activity",
         ondelete="cascade",
@@ -123,6 +129,88 @@ class AccountRetentionLine(models.Model):
     foreign_iva_amount = fields.Float(string="Foreign IVA")
     foreign_retention_amount = fields.Float()
     foreign_currency_rate = fields.Float(string="Rate")
+
+    @api.depends("payment_concept_id")
+    def _compute_code(self):
+        for rec in self:
+            if rec.retention_id.partner_id and rec.payment_concept_id:
+                codes = rec.payment_concept_id.line_payment_concept_ids.filtered(
+                    lambda l: l.type_person_id == rec.retention_id.partner_id.type_person_id
+                ).mapped('code')
+
+                rec.code = codes[0] if codes else ''
+            else:
+                rec.code = ''
+
+
+    @api.onchange("move_id")
+    def _onchange_move_id(self):
+        """
+        Calcula y actualiza los campos de la línea de retención cuando cambia la factura.
+        """
+        for record in self:
+            if not record.move_id:
+                return {}
+
+            if record.retention_id and record.retention_id.type_retention != 'iva' or not record.retention_id:
+                return
+            
+            invoice_id = record.move_id
+
+            # Lógica para obtener el monto de retención del partner
+            withholding_amount = invoice_id.partner_id.withholding_type_id.value
+
+            # Aquí va la lógica de tu método original
+            tax_ids = invoice_id.invoice_line_ids.filtered(
+                lambda l: l.tax_ids and l.tax_ids[0].amount > 0
+            ).mapped("tax_ids")
+            
+            if not any(tax_ids):
+                
+                return {
+                    'warning': {
+                        'title': _("Atención"),
+                        'message': _("The invoice has no tax.")
+                    }
+                }
+
+            subtotals_name = invoice_id.tax_totals["subtotals"][0]["name"]
+            tax_groups = zip(
+                invoice_id.tax_totals["groups_by_subtotal"][subtotals_name],
+                invoice_id.tax_totals["groups_by_foreign_subtotal"][subtotals_name],
+            )
+
+            for tax_group, foreign_tax_group in tax_groups:
+                taxes = tax_ids.filtered(lambda l: l.tax_group_id.id == tax_group["tax_group_id"])
+                if not taxes:
+                    continue
+                
+                tax = taxes[0]
+                retention_amount = tax_group["tax_group_amount"] * (withholding_amount / 100)
+                record.name = _("Iva Retention")
+                record.invoice_type = invoice_id.move_type
+                record.move_id = invoice_id.id
+                record.aliquot = tax.amount
+                record.iva_amount = tax_group["tax_group_amount"]
+                record.invoice_total = invoice_id.tax_totals["amount_total"]
+                record.related_percentage_tax_base = withholding_amount
+                record.invoice_amount = tax_group["tax_group_base_amount"]
+                record.foreign_currency_rate = invoice_id.foreign_rate
+                record.foreign_invoice_amount = foreign_tax_group["tax_group_base_amount"]
+                record.foreign_iva_amount = foreign_tax_group["tax_group_amount"]
+                record.foreign_invoice_total = invoice_id.tax_totals["foreign_amount_total"]
+
+                if invoice_id.move_type == "out_invoice":
+                    record.retention_amount = 0.0
+                    record.foreign_retention_amount = 0.0
+                else:
+                    record.retention_amount = retention_amount
+                    record.foreign_retention_amount = record.foreign_iva_amount * (withholding_amount / 100)
+                    
+                break
+
+
+   
 
     @api.depends("retention_id.type_retention", "move_id")
     def _compute_name(self):
