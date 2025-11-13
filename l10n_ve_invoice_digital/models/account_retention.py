0c2b72bc6724c1974eae52e339e065fdad076cd9
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2025-11-13 15:06:05 -0400

    FIX #9883: l10n_ve_invoice_digital
    Problema: No se esta obteniendo la serie en las lineas de las retenciones al digitalizar, ademas la fecha de estos se esta obteniendo de la retencion en vez de las l>
    
    Solución:
    - Se agrego que al obtener el documento (name) este se desglorasa en para obtener la serie(Las letras) y la serie (Los numeros)
    - SE cambio la forma de obtener la fecha del documento, antes era por la linea de retencion, ahora lo obtiene del documento de la linea
    
    Tarea (Link): https://binaural.odoo.com/web#cids=2&menu_id=293&action=393&active_id=58&model=helpdesk.ticket&view_type=form&id=9883
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_invoice_digital/models/account_retention.py
+++ l10n_ve_invoice_digital/models/account_retention.py
@@ -117,6 +117,7 @@ class AccountRetention(models.Model):
                 "detallesRetencion": retention_details,
             }
         }
+        _logger.info("Payload for digital retention document: %s", json.dumps(payload, indent=4))
         response = self.call_tfhka_api("emision", payload)
 
         if response:
@@ -279,7 +280,7 @@ class AccountRetention(models.Model):
                 retention_data["totalRetenido"] = total_retention
                 retention_data["totalIVA"] = total_iva
             else:
-                retention_data["TotalISRL"] = total_iva
+                retention_data["TotalISRL"] = total_retention
 
             return retention_data    
 
@@ -294,7 +295,9 @@ class AccountRetention(models.Model):
         for record in self:
             for line in record.retention_line_ids:
                 tipo_documento = type_document.get(line.move_id.move_type, "03") if not line.move_id.debit_origin_id else "03"
-                document_number_ret = str(line.move_id.sequence_number)
+                serie = line.move_id.name
+                document_series_ret = ''.join([c for c in serie if c.isalpha()])
+                document_number_ret = str(''.join([c for c in serie if c.isdigit()]))
 
                 if record.base_currency_is_vef:
                     invoice_total = str(round(line.invoice_total, 2))
@@ -310,8 +313,9 @@ class AccountRetention(models.Model):
 
                 retention_data = {
                     "numeroLinea": str(counter), 
-                    "fechaDocumento": line.date_accounting.strftime("%d/%m/%Y"), 
+                    "fechaDocumento": line.move_id.invoice_date.strftime("%d/%m/%Y"), 
                     "tipoDocumento": tipo_documento,
+                    "serieDocumento": document_series_ret,
                     "numeroDocumento": document_number_ret,
                     "numeroControl": line.move_id.correlative,
                     "montoTotal": invoice_total,  
