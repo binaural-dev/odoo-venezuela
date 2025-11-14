87b9285033afebe1fca0d295e9c65a70e923943b
Jaco <jaco@binaural.com.ve>
2025-11-14 11:46:38 -0400

    FIX  #59950: l10n_ve_accountant
    Problema:
    -Se requiere eliminar la capacidad de cambiar una factura de proveedor a
    recibo.
    Solución:
    -Se coloca como true el atributo invisible en el campo move_type de la
    vista.
    
    Tarea (Link):
    https://binaural.odoo.com/web#id=59950&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.0",
+    "version": "1.6",
     "depends": [
         "base",
         "web",
