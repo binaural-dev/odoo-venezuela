d5b0d19d01d760d79d4e3831705932f060ad2e83
ivan-binaural <ivan@binaural.dev>
2025-11-19 15:35:23 -0400

    FEAT #59962: l10n_ve_accountant
    Cambio
    
    -Diseno de forma libre especifico con moneda y tasa dinamicas segun la factura junto con ajustes en los modelos de account_tax para generar montos segun monedas
    
    -ajustes de montos grabados con solo moneda en bolivares en la factura
    
    -ajustes de descuentos y subtotales en la factura ahora en los reportes de forma libre
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=59962&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    
    Ticket de soporte []

--- l10n_ve_accountant/models/account_tax.py
+++ l10n_ve_accountant/models/account_tax.py
@@ -23,8 +23,10 @@ class AccountTax(models.Model):
         res = super()._get_tax_totals_summary(
             base_lines, currency, company, cash_rounding
         )
-        #only ves
+
+        #only ves currency
         ves_currency = self.env.ref('base.VEF')
+        
         # Obtener el registro de factura desde el contexto si está disponible
         active_model = self.env.context.get('active_model')
         active_id = self.env.context.get('active_id')
@@ -34,15 +36,43 @@ class AccountTax(models.Model):
         record = self.env[active_model].browse(active_id)
         currency_id = self.env.company.currency_id or False
         foreign_currency_id = self.env.company.foreign_currency_id or False
+        company_rate = 1.0
         if active_model == "account.move" and record.move_type in ("out_invoice", "in_invoice", "out_refund", "in_refund"):
+            company_rate = record.company_currency_rate
             currency_id = record.currency_id
             foreign_currency_id =record.foreign_currency_id
-        else:
+        else: 
             currency_id = record.company_id.currency_id
             foreign_currency_id = self.env.company.foreign_currency_id
 
         # FIXME: Evaluar escenarios en los que hay descuentos.
         res_without_discount = res.copy()
+        #? QUESTION do i need to put the amount without discount?
+        
+        #total amount discount 
+        has_discount = any(
+            line.discount > 0
+            for line in record.invoice_line_ids
+        )
+        formatted_total_discount = 0.0
+        if has_discount:
+            exchange_rate = company_rate
+            total_discount_amount = sum([
+                (line.get("price_unit", 0.0) * line.get("quantity", 0.0) * line.get("discount", 0.0) / 100)
+                for line in base_lines
+            ])
+            total_discount_amount_ves = total_discount_amount * exchange_rate
+            formatted_total_discount = formatLang(
+                env=self.env,
+                value=total_discount_amount,
+                currency_obj=currency_id
+            )
+            #discount only en VEF
+            formatted_total_discount_ves = formatLang(
+                env=self.env,
+                value=total_discount_amount_ves,
+                currency_obj=ves_currency
+            )
         foreign_lines = []
         #has_discount = not currency.is_zero(sum([line["discount"] for line in base_lines]))
         # if has_discount:
@@ -76,6 +106,9 @@ class AccountTax(models.Model):
         res['base_amount_foreign_currency'] = foreign_res['base_amount_currency']
         res['tax_amount_foreign_currency'] = foreign_res['tax_amount_currency']
         res['total_amount_foreign_currency'] = foreign_res['total_amount_currency']
+        #discount amount 
+        res['formatted_total_discount'] = formatted_total_discount
+        res['formatted_total_discount_ves'] = formatted_total_discount_ves
         # Moneda Base
         res['formatted_base_amount_currency'] = formatLang(
             env=self.env,
@@ -92,6 +125,8 @@ class AccountTax(models.Model):
             value=res.get('total_amount_currency', 0.0),
             currency_obj=currency_id
         )
+
+        
 
         #only VES amounts
         res['formatted_base_amount_currency_ves'] = formatLang(
@@ -148,6 +183,22 @@ class AccountTax(models.Model):
                 value=res_subtotal.get('total_amount_foreign_currency', 0.0),
                 currency_obj=foreign_currency_id
             )
+            #ONLY VES
+            res_subtotal['formatted_base_amount_currency_ves'] = formatLang(
+                env=self.env,
+                value=res_subtotal.get('base_amount', 0.0),
+                currency_obj=ves_currency
+            )
+            res_subtotal['formatted_tax_amount_currency_ves'] = formatLang(
+                env=self.env,
+                value=res_subtotal.get('tax_amount', 0.0),
+                currency_obj=ves_currency
+            )
+            res_subtotal['formatted_total_amount_currency_ves'] = formatLang(
+                env=self.env,
+                value=res_subtotal.get('total_amount', 0.0),
+                currency_obj=ves_currency
+            )
             #Base sistema
             res_subtotal['formatted_base_amount_currency'] = formatLang(
                 env=self.env,
@@ -164,6 +215,9 @@ class AccountTax(models.Model):
                 value=res_subtotal.get('total_amount_currency', 0.0),
                 currency_obj=currency_id
             )
+
+            #Amount discount
+            
 
             for res_tax_group, foreign_tax_group in zip(res_subtotal.get("tax_groups", []), foreign_subtotal.get("tax_groups", [])):
                 res_tax_group["tax_amount_foreign_currency"] = foreign_tax_group.get("tax_amount_currency", 0.0)
