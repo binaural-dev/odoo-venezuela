69101fa662a2f3819800877108f92498c540df45
Daniel Pereira <daniel@binaural.dev>
2025-11-20 13:27:23 -0400

    stg_fix_ti_9820_l10n_ve_pos_mf Error al reiniciar la computadora de la caja 1 se desconecta la maquina fiscal y se debe siempre reiniciar la IOT Problema:Error al reiniciar la computadora de la caja 1 se desconecta la maquina fiscal y se debe siempre reiniciar la IOT Link:https://binaural.odoo.com/web#id=9820&cids=2&menu_id=293&action=389&model=helpdesk.ticket&view_type=form

--- l10n_ve_pos_mf/static/src/js/Navbar.js
+++ l10n_ve_pos_mf/static/src/js/Navbar.js
@@ -2,6 +2,7 @@
 
 import { Navbar } from "@point_of_sale/app/navbar/navbar";
 import { patch } from "@web/core/utils/patch";
+import { ErrorPopup } from "@point_of_sale/app/errors/popups/error_popup";
 
 patch(Navbar.prototype, {
   async _on_click_mf_test() {
@@ -15,12 +16,28 @@ patch(Navbar.prototype, {
         throw new Error()
 
       }
+
     } catch (e) {
-      this.showPopup("ErrorPopup", {
-        title: "No se ha podido conectar a la Maquina fiscal",
-      });
+      console.log(e)
+      let message ;
+      if (typeof response ==='undefined') {
+        message = "No se ha podido conectar a la Maquina fiscal"
+      }
+      else{
+        message = response.result.error
+      }
+      this.env.services.popup.add(
+        ErrorPopup,
+        {
+          title: ("No se ha podido conectar a la Maquina fiscal"),
+          body: message,
+        }
+      )
     }
   },
+  
+
+
   get access_button_mf() {
     if (!this.pos.config) {
       return false
