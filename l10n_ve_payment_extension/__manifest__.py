72184cc06323c5229e7f4c1d60efada078db9d79
Jaco <jaco@binaural.com.ve>
2025-11-21 13:28:31 -0400

    FEAT #60473: l10n_ve_payment_extension
    Problema:
    -Al acceder a la vista para asignar diario en sucursales, muestra error de no conseguir el campo company_id en el modelo account.account.
    
    Solución:
    -Se migra el campo a company_ids el cual es el que lo reemplaza desde odoo versión 18.
    Tarea (Link):
    https://binaural.odoo.com/web#id=60473&cids=2&menu_id=1063&action=345&active_id=2141&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "1.2",
+    "version": "1.3",
     "depends": [
         "base",
         "account",
