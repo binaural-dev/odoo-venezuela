e02118f0990e9a21186117c5864f96e51967fb9f
SaulOrte <saul@binaural.dev>
2025-11-23 18:32:59 -0400

    Se elimina campo cross_move_ids ya el mismo almacenaba informacion de otro campo y nunca se usaba, el mismo se prestaba para equiovaciones ya que el campo no se almacenaba en todo momento por la relacion era dificil realizar procesos con los valores del mismo, se ajusta la fecha en el calculo de los saldos alternos para que tome en concideracion la fecha de los pagos en los pagos en los asientos de cruce,se remueven los digits en los campos de tasas y tasas alternas ya que los mismos redondeban los valores y en casos especificos se producian errores por redondeo, se elimina el redondeo al final de la funcion _convert() en res.currency para evitar que se redondeen los resultados de las conversiones y poder trabajar con calculos exactos, se condiciono el metodo round() para que redonde solamente cuando exista una diferencia significativa en el monto real respcto del redondeado en caso contrario no lo redondea porq no es necesario, esto para evitar los redondeos a nivel de conversiones, se agrego el achivo fields.py el mismo agrega unos MonkeyPatch a funciones del orm de la calse Monetary, esto para permitir q se guarden los valores en base de datos sin redondeo y que los mismos tambien puedan ser Leidos por el orm sin ser redondeados y obtener una correcta conversion de los datos.

--- l10n_ve_accountant/models/res_currency.py
+++ l10n_ve_accountant/models/res_currency.py
@@ -1,9 +1,11 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 import logging
 
 _logger = logging.getLogger(__name__)
-
+from odoo.fields import Monetary
+from odoo.tools import float_repr
 
 class ResCurrency(models.Model):
     _inherit = "res.currency"
@@ -21,3 +23,30 @@ class ResCurrency(models.Model):
                     "l10n_ve_accountant.group_fiscal_config_support"
                 )
             )
+
+    def _convert(self, from_amount, to_currency, company=None, date=None, round=True):  
+        
+        self, to_currency = self or to_currency, to_currency or self
+        assert self, "convert amount from unknown currency"
+        assert to_currency, "convert amount to unknown currency"
+        if from_amount:
+            to_amount = from_amount * self._get_conversion_rate(self, to_currency, company, date)
+        else:
+            return 0.0
+
+        return to_amount
+
+
+    def round(self, amount):
+        
+        self.ensure_one()
+
+        try:
+            amount_float = float(amount)
+        except (ValueError, TypeError):
+            return super(ResCurrency, self).round(amount)
+
+        if abs(amount_float - round(amount_float, 6)) > 1e-9:
+            return amount_float
+
+        return super(ResCurrency, self).round(amount)
