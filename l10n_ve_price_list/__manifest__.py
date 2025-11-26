9d9e4b207cfa69fa8b9b4241c22621de57791bcf
ivan-binaural <ivan@binaural.dev>
2025-11-26 19:54:33 -0400

    [FIX]# 60673 Se agrega nuevas reglas de seguridad por grupos para ventas y facturacion en los campos precio unitario y precio foraneao

--- l10n_ve_price_list/__manifest__.py
+++ l10n_ve_price_list/__manifest__.py
@@ -1,6 +1,6 @@
 {
-    "name": "Venezuela - reglas para listas de precio",
-    "version": "1.0",
+    "name": "Venezuela - reglas para listas de precio, precios en moneda extranjera y precios unitarios en factura",
+    "version": "1.1",
     "license": "LGPL-3",
     "summary": "Módulo para gestionar reglas de listas de precio en Venezuela",
     "description": """
@@ -13,6 +13,7 @@
     "data": [
         "security/pricelist_security.xml",
         "views/account_invoice_view.xml",
+        "views/sale_order.xml",
     ],
     "application": True,
     "images": ["static/description/icon.png"],
--- l10n_ve_price_list/models/__init__.py
+++ l10n_ve_price_list/models/__init__.py
@@ -1,1 +1,3 @@
 from . import account_move
+from . import account_move_line
+from . import sale_order
