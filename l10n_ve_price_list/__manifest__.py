7984d201bcec797d761865751020f47f33ccbe47
ivan-binaural <ivan@binaural.dev>
2025-11-28 14:08:04 -0400

    [FIX]# 60667
    Se agrega nuevas reglas y grupos para list_price en product.template
    
    se corrijen tambien problemas de herencia de la vista

--- l10n_ve_price_list/__manifest__.py
+++ l10n_ve_price_list/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    "name": "Venezuela - reglas para listas de precio, precios en moneda extranjera y precios unitarios",
+    "name": "Venezuela - reglas para listas de precio",
     "version": "1.1",
     "license": "LGPL-3",
     "summary": "Módulo para gestionar reglas de listas de precio en Venezuela",
     "description": """
-        Este módulo personaliza las reglas de listas de precio.
+        Este módulo personaliza las reglas de listas de precio, precios en moneda extranjera y precios unitarios.
     """,
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
--- l10n_ve_price_list/models/__init__.py
+++ l10n_ve_price_list/models/__init__.py
@@ -1,4 +1,4 @@
 from . import account_move
 from . import account_move_line
-from . import sale_order
+from . import sale_order_line
 from . import product_template
