b1dd8c2fa93b15de02aea98db69b22581890e116
ivan-binaural <ivan@binaural.dev>
2025-11-28 15:19:42 -0400

    [FIX]# 60667
    Se agrega nuevas reglas y grupos para list_price en product.template
    
    -se agregan dependencias

--- l10n_ve_price_list/__manifest__.py
+++ l10n_ve_price_list/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Venezuela - reglas para listas de precio",
-    "version": "1.1",
+    "version": "1.2",
     "license": "LGPL-3",
     "summary": "Módulo para gestionar reglas de listas de precio en Venezuela",
     "description": """
@@ -9,7 +9,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Purchase",
-    "depends": ["account","sale", "account_invoice_pricelist"],
+    "depends": ["account","sale", "account_invoice_pricelist", "l10n_ve_sale"],
     "data": [
         "security/pricelist_security.xml",
         "views/account_invoice_view.xml",
