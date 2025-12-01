665b6aa65406bb78f8bcf27e9e5bc6f73909e2c8
ivan-binaural <ivan@binaural.dev>
2025-12-01 14:23:30 -0400

    FIX #60677: l10n_ve_invoice_FEAT#60677
    Problema: Se requiere que la fecha por defecto sea el dia de hoy, incluso si cambia de tipo de facturas o notas siempre por defecto fecha de hoy
    
    Solución:se hereda y cambia el comportamiento del campo en su atributo default, asi como se reducen funciones computadas en donde al no cumplirse una condicion
    la fecha se vaciaba como ahora sera siempre por defecto se elimina esa condicion
    
    Tarea (Link):https://binaural.odoo.com/web/#id=60677&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_accountant/models/account_move.py
+++ l10n_ve_accountant/models/account_move.py
@@ -146,20 +146,6 @@ class AccountMove(models.Model):
         self.invoice_date = False if self.move_type == "entry" else fields.Date.today()
         self.invoice_date_display = False if self.move_type == "entry" else fields.Date.today()
 
-    def default_rate(self):
-        """
-        This method is used to get the rate of the payment.
-
-        Returns
-        -------
-        type = float
-            The rate of the payment
-        """
-        rate_values = self.env["res.currency.rate"].compute_rate(
-            self.currency_id.id or self.env.ref("base.VEF").id,
-            fields.Date.today(),
-        )
-        return rate_values.get("foreign_rate", 0)
 
     def default_rate(self):
         """
