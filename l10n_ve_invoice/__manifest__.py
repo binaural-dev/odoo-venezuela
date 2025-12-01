7231773403fe6f72225a1ad2e3c6c407469d655d
ivan-binaural <ivan@binaural.dev>
2025-12-01 19:41:56 -0400

    updated manifest file

--- l10n_ve_invoice/__manifest__.py
+++ l10n_ve_invoice/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Módulo de Facturación Venezuela
     """,
-    "version": "1.7",
+    "version": "1.8",
     "license": "LGPL-3",
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
