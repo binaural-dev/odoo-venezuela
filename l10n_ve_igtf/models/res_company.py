11ae8f031b78dd1cab370fce01b44eef2101b6a0
SaulOrte <saul@binaural.dev>
2025-12-08 16:33:43 -0400

    Fixing translation and update Manifest

--- l10n_ve_igtf/models/res_company.py
+++ l10n_ve_igtf/models/res_company.py
@@ -16,5 +16,5 @@ class ResCompany(models.Model):
     show_igtf_suggested_account_move = fields.Boolean(default=False)
     show_igtf_suggested_sale_order = fields.Boolean(default=False)
 
-    apply_igtf_in_wizard_payment = fields.Boolean(readonly=False
+    apply_igtf_in_wizard_payment = fields.Boolean(readonly=False, default=True
     )
