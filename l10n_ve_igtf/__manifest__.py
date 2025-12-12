4db035f0d9c8f851667ce63e5395bf09b3d33cd0
SaulOrte <saul@binaural.dev>
2025-12-12 11:56:36 -0400

    Fix igtf wizard amount

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.0.0.25",
+    "version": "17.0.0.0.26",
     "depends": [
         "base",
         "account",
