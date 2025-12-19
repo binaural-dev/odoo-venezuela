9ef4be1897d3dcffd06bb9d1b42000b4e9805369
Jaco <jaco@binaural.com.ve>
2025-12-19 11:30:25 -0400

    FEAT #61173: l10n_ve_accountant,l10n_ve_invoice,l10n_ve_sale,l10n_ve_igtf
    Problema:
    1.1 Visualización correcta de tasa en facturación.
    
    1.2 1.2. Ventas / Productos / Lista de precios / agregar una linea de
    regla de lista de precio / agregar un producto. Se visualiza mal el
    campo de Precio + IVA
    
    2. Mejorar la posición de los acentos en la forma libre
    
    3. Cambiar traducción de Forma Libre a Forma Libre (Binaural)
    
    Solución:
    1.Se descomenta el código anteriormente comentado a petición en la vista
    de l10n_ve_accountant, y se cambia el XPATH como se mencionaba en el
    comentario que se había dejado anteriormente. También, se mejora este
    xpath para que sea preciso en caso de cambios en la vista en siguientes
    versiones de odoo.
    
    1.2 Se cambia el xpath en la vista formulario heredada de
    product.pricelist.item (product_pricelist_item_form_view) para que el
    campo price_with_tax se introduzca después del div existente para el
    precio y la unidad de medida del producto, y que así no se sobreponga
    sobre estos.
    
    2.Se corrigen los acentos en el div.
    
    3.Se cambia el nombre del action y se realiza su respectiva traducción.
    
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=61173&cids=2&menu_id=257&action=341&model=project.task&view_type=form
    Tarea[x]
    Ticket[]

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.18",
+    "version": "1.22",
     "depends": [
         "base",
         "web",
