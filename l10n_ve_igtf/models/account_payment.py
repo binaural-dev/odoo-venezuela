86f32aad1a30912593e37cf92ecc6bf8736ca0ad
SaulOrte <saul@binaural.dev>
2025-12-23 10:09:24 -0400

    Se corrige condicion ene l calculo del igtf

--- l10n_ve_igtf/models/account_payment.py
+++ l10n_ve_igtf/models/account_payment.py
@@ -58,12 +58,18 @@ class AccountPaymentIgtf(models.Model):
 
     def calculate_igtf_for_payment(self, invoice, payment_amount, igtf_percentage=False):
        
-        currency = self.env.company.currency_id
+        currency = self.currency_id 
+
         principal_debt = invoice.amount_residual
+
         principal_amount = min(payment_amount, principal_debt)
+
         igtf_unrounded = principal_amount * (self.env.company.igtf_percentage / 100)
+
         igtf_top = currency.round(invoice.igtf_top_aply) 
+
         alter_bi_igtf = currency.round(invoice.alter_bi_igtf)
+
         igtf= currency.round(igtf_unrounded)
     
         if igtf > 0 and igtf_top == invoice.amount_residual:
@@ -72,9 +78,13 @@ class AccountPaymentIgtf(models.Model):
         
             return 0.0
         
-        residual_igtf = (invoice.amount_total * (self.env.company.igtf_percentage / 100)) - alter_bi_igtf
+
+        residual_igtf = igtf_top - alter_bi_igtf
+
+        
         if igtf > residual_igtf and residual_igtf != 0.0:
             igtf = residual_igtf
+        
             
         return max(igtf, 0.0)
     
