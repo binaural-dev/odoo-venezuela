115cf6fe1e25efda43b7f611a896b66475830d61
ivan-binaural <ivan@binaural.dev>
2026-01-07 16:18:52 -0400

    FIX #62121: l10n_ve_invoice
    se realizan validaciones para action_post para limitar la creacion de facturas sin lineas de impuestos en los productos
    
    Tarea (Link):https://binaural.odoo.com/web/#id=59953&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    
    Ticket de soporte

--- l10n_ve_invoice/models/account_move.py
+++ l10n_ve_invoice/models/account_move.py
@@ -54,7 +54,17 @@ class AccountMove(models.Model):
             self.invoice_date = fields.Date.today()
 
     def action_post(self):
+        
         for record in self:
+            # Validación: Solo si existe documento de origen (compra o venta)
+            if record.move_type in ("out_invoice", "in_invoice", "out_refund", "in_refund"):
+                for line in record.invoice_line_ids:
+                    if line.display_type in ("line_section", "line_note"):
+                        continue
+                    if not line.tax_ids:
+                        # Agregue un impuesto a cada línea de producto. No se permite confirmar la factura si alguna línea de producto no tiene impuesto.
+                        raise ValidationError(_("Add a tax to each product line. You cannot confirm the invoice if any product line is missing a tax."))
+
             sequence = record.env["ir.sequence"].sudo().search([("code", "=", "invoice.correlative"), ("company_id", "=", self.env.company.id)])
             correlative = str(sequence.number_next_actual).zfill(sequence.padding)
 
