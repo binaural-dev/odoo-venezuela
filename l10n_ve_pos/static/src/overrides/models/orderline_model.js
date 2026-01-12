e63e6e83f9522706afebe060b2316adddf3d2aab
OmarYepez29 <omar@binauraldev.com>
2026-01-12 09:44:36 -0400

    [IMP]Se usa la precion de la moneda para los calculos de redondeo.
    Se hacen mejoras en los calculos de redondeo,ya no se usa 4 como valor
    quemado.
    Segun sea el caso de usara la precision decimal de la moneda,de la
    moneda foranea o del precio/precio alterno segun sea necesario.

--- l10n_ve_pos/static/src/overrides/models/orderline_model.js
+++ l10n_ve_pos/static/src/overrides/models/orderline_model.js
@@ -6,6 +6,8 @@ import {
   formatFloat,
   roundDecimals as round_di,
   floatIsZero,
+  roundDecimals as round_di,
+  roundPrecision as round_pr,
 } from "@web/core/utils/numbers";
 
 // New orders are now associated with the current table, if any.
