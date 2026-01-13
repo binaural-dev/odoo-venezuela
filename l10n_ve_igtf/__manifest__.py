6d68d8ac08cb17031e032b6e809bf9b6bb5ee28d
David Mendoza <davidmendoza@binaural.com.ve>
2026-01-13 09:01:39 -0400

    FEAT #62157: l10n_ve_[igtf,invoice]
    Problema: Se necesita mostrar las columnas de igtf que estaban en la v16 en la v17.
    
    Solución:
    
    l10n_ve_igtf
     Account move:
      -se agregan dos campos nuevos, llamados foreign_bi_igtf y foreign_alter_bi_igtf, para obtener la base imponible alterna y el monto alterno del igtf respectivamente.
      -se agrega el computado para obtener ambos valores mediante los pagos con igtf asociados al movimiento.
      -se agregan docstrings.
     Res Company y Res Config Settings:
      -Se agregan campos de configuracion para que las columnas de igtf del reporte sean configurables.
      -Se agrega herencia de la vista de l10n_ve_tax para agregar los nuevos campos de configuracion.
    
     Accounting Reports:
      -Se agrega herencia del modelo wizard.accounting.reports del modulo l10n_ve_invoice.
      -se heredan las funciones: sale_book_fields, purchase_book_fields, _fields_sale_book_line, _fields_purchase_book_line, _get_sale_book_field_groups y _get_purchase_book_field_groups para agregar la implementacion de las columnas de igtf y bi igtf.
    
    l10n_ve_invoice
     Accounting Reports:
      -se agrega funcionalidad para sumar la columna del bi igtf y calcular el 3% de esa suma en las funciones generate_purchases_book y generate_sales_book.
    
    Tarea (Link): https://binaural.odoo.com/web#id=62157&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.1.0.0",
+    "version": "17.0.0.0.37",
         "depends": [
         "base",
         "l10n_ve_accountant",
@@ -18,6 +18,7 @@
        
     
     "data": [
+        "data/ir_actions_server.xml",
         "views/account_journal.xml",
         "views/res_config_settings.xml",
         "report/invoice_free_form.xml",
--- l10n_ve_igtf/wizard/accounting_reports.py
+++ l10n_ve_igtf/wizard/accounting_reports.py
@@ -28,13 +28,13 @@ class WizardAccountingReports(models.TransientModel):
         fields = super().sale_book_fields()
         fields.extend(
             [
-                # {
-                #     "name": "Bi igtf",
-                #     "field": "bi_igtf",
-                #     "format": "number",
-                # },
                 {
-                    "name": "IGTF",
+                    "name": "Bi igtf",
+                    "field": "bi_igtf",
+                    "format": "number",
+                },
+                {
+                    "name": "Igtf",
                     "field": "igtf",
                     "format": "number",
                 },
@@ -59,13 +59,13 @@ class WizardAccountingReports(models.TransientModel):
         fields = super().purchase_book_fields()
         fields.extend(
             [
-                # {
-                #     "name": "Bi igtf",
-                #     "field": "bi_igtf",
-                #     "format": "number",
-                # },
                 {
-                    "name": "IGTF",
+                    "name": "Bi igtf",
+                    "field": "bi_igtf",
+                    "format": "number",
+                },
+                {
+                    "name": "Igtf",
                     "field": "igtf",
                     "format": "number",
                 },
@@ -94,13 +94,11 @@ class WizardAccountingReports(models.TransientModel):
         Returns:
             dict: Updated field values for the sale book line.
         """
-        is_check_currency_system = self.currency_system
         fields = super()._fields_sale_book_line(move, taxes)
         multiplier = -1 if move.move_type == "out_refund" else 1
         bi_igtf = move.foreign_bi_igtf if not self.currency_system else move.bi_igtf
-        is_igtf = bool(move.alter_bi_igtf > 0 or move.foreign_alter_bi_igtf > 0)
-        igtf = (move.tax_totals["igtf"]["igtf_amount"] if is_check_currency_system else move.tax_totals["igtf"]["foreign_igtf_amount"]) if is_igtf else 0
-        # fields |= {"bi_igtf": bi_igtf,}
+        igtf = move.foreign_alter_bi_igtf if not self.currency_system else move.tax_totals["igtf"]["igtf_amount"]
+        fields |= {"bi_igtf": bi_igtf * multiplier,}
         fields |= {"igtf": igtf * multiplier,}
         return fields
 
@@ -125,13 +123,11 @@ class WizardAccountingReports(models.TransientModel):
         Returns:
             dict: Updated field values for the purchase book line.
         """
-        is_check_currency_system = self.currency_system
         fields = super()._fields_purchase_book_line(move, taxes)
         multiplier = -1 if move.move_type == "in_refund" else 1
         bi_igtf = move.foreign_bi_igtf if not self.currency_system else move.bi_igtf
-        is_igtf = bool(move.alter_bi_igtf > 0 or move.foreign_alter_bi_igtf > 0)
-        igtf =  (move.tax_totals["igtf"]["igtf_amount"] if is_check_currency_system else move.tax_totals["igtf"]["foreign_igtf_amount"]) if is_igtf else 0
-        # fields |= {"bi_igtf": bi_igtf,}
+        igtf = move.foreign_alter_bi_igtf if not self.currency_system else move.tax_totals["igtf"]["igtf_amount"]
+        fields |= {"bi_igtf": bi_igtf * multiplier,}
         fields |= {"igtf": igtf * multiplier,}
         return fields
     
@@ -154,10 +150,10 @@ class WizardAccountingReports(models.TransientModel):
         igtf_fields = [
         ]
 
-        # if not self.env.company.not_show_bi_igtf_sale_order:
-        #     igtf_fields.append(            
-        #         {"name": "Bi igtf", "field": "bi_igtf", "format": "number"},
-        #     )
+        if not self.env.company.not_show_bi_igtf_sale_order:
+            igtf_fields.append(            
+                {"name": "Bi igtf", "field": "bi_igtf", "format": "number"},
+            )
 
         if not self.env.company.not_show_igtf_sale_order:
             igtf_fields.append(            
@@ -192,10 +188,10 @@ class WizardAccountingReports(models.TransientModel):
         igtf_fields = [
         ]
 
-        # if not self.env.company.not_show_bi_igtf_purchase_order:
-        #     igtf_fields.append(            
-        #         {"name": "Bi igtf", "field": "bi_igtf", "format": "number"},
-        #     )
+        if not self.env.company.not_show_bi_igtf_purchase_order:
+            igtf_fields.append(            
+                {"name": "Bi igtf", "field": "bi_igtf", "format": "number"},
+            )
 
         if not self.env.company.not_show_igtf_purchase_order:
             igtf_fields.append(            
