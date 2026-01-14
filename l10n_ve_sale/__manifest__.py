db61e226bfa7dd6919a6840ffc0e5110f182c7d1
Jaco <jaco@binaural.com.ve>
2026-01-14 16:49:56 -0400

    FEAT #62160: l10n_ve_sale
    Problema:
    -Se requiere visualizar las columinas de total y subtotal en Bs y la moneda foranea del sistema, en la vista lista y pivot del módulo de ventas.
    
    -Se agregan los fields amount_untaxed_total_signed, amount_total_signed los cuales son los montos en moneda de la compañía, simulando los campos computados signed de Odoo en account.move.
    -Se agrega la función computada _compute_amount_signed, encargada de asignar los valores a los campos amount_total_signed y amount_untaxed_total_signed, usando el convert de odoo, convirtiendo desde la moneda de la orden de venta (si esta es diferente a la moneda principal de la compañía).
    
    -Se crea el campo foreign_untaxed_total, el cual contiene el valor sin impuestos en moneda foranea.
    
    -Se crea _compute_foreign_untaxed_total, la cual computa el valor de foreign_untaxed_total usando el campo base_amount_foreign_currency de tax_totals.
    
    -Se modifica get_view para mostrar dinamicamente el nombre de los totales en moneda foranea, tanto en la vista pivot como en la vista list.
    
    -Se agregan los nuevos campos a la vista de ordenes de compra y a la vista pivot, a su vez, se ocultan los campos amount_total y amount_untaxed para que estos no confundan al cliente con los cálculos que hace odoo nativo.
    Solución:
    
    Tarea (Link):
    https://binaural.odoo.com/web#id=62160&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_sale/__manifest__.py
+++ l10n_ve_sale/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Sales/Sales",
-    "version": "1.6",
+    "version": "1.8",
     # any module necessary for this one to work correctly
     "depends": [
         "base",
