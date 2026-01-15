e437c9e712207359a6b03a983f4ccda1413d3dc9
pastor-binaural <pastor@binaural.dev>
2026-01-15 19:03:02 -0400

    upd submodules

--- cadipa_appointment/static/src/js/guest_form.js
+++ cadipa_appointment/static/src/js/guest_form.js
@@ -6,51 +6,52 @@ const OriginalGuestForm = publicWidget.registry.GuestForm;
 
 if (OriginalGuestForm) {
     publicWidget.registry.GuestForm = OriginalGuestForm.extend({            
-        /**
-         * Extended function to add new age validations.
-         */
         _validateAgeInForm: function ($form) {
             const originalResult = this._super.apply(this, arguments);
-
-            if (!originalResult) {
-                return false;
-            }
+            if (!originalResult) return false;
 
             const $birth = $form.find('input.birthdate-input');
             const $error = $form.find('.age-error');
-            const clasification = $form.find('.guest-type-select').val();
-            const guest_type = $form.find('.type_relation-select').val();
+            const $relationSelect = $form.find('.type_relation-select');
             
+            const $selectedOption = $relationSelect.find('option:selected');
+            const isChild = $selectedOption.data('is-child') === true || $selectedOption.data('is-child') === 'true';
+
             const val = $birth.val();
-            
             const parsed = this._parseYMD(val);
 
-            if (!parsed) {
-                return true;
-            }
-            const minAge = parseInt(document.querySelector('#binaural_min_child_age')?.value || 0);
-            const maxAge = parseInt(document.querySelector('#binaural_max_child_age')?.value || 0);
+            if (!parsed) return true;
+
             const age = this._calcAge(parsed);
             
-            if (guest_type === 'children') {
+            if (age < 0) {
+                const msg = _t('La fecha de nacimiento no puede ser en el futuro');
+                $error.text(msg).show();
+                $birth[0]?.setCustomValidity(msg);
+                return false;
+            }
+
+            if (isChild) {
+                const maxAge = parseInt(document.querySelector('#binaural_max_child_age')?.value || 0);
+                const minAge = parseInt(document.querySelector('#binaural_min_child_age')?.value || 0);
+
                 if (age > maxAge) {
-                    $error.text(_t(`La edad máxima para un niño es de ${maxAge} años`)).show();
-                    $birth[0]?.setCustomValidity(_t(`La edad máxima para un niño es de ${maxAge} años`));
+                    const msg = _t(`La edad máxima para un niño es de ${maxAge} años`);
+                    $error.text(msg).show();
+                    $birth[0]?.setCustomValidity(msg);
+                    return false;
+                }
+                
+                if (age < minAge) {
+                    const msg = _t(`El niño debe tener al menos ${minAge} años`);
+                    $error.text(msg).show();
+                    $birth[0]?.setCustomValidity(msg);
                     return false;
                 }
             }
 
-            if (age < 0) {
-                $error.text(_t('La fecha de nacimiento no puede ser en el futuro')).show();
-                $birth[0]?.setCustomValidity(_t('La fecha de nacimiento no puede ser en el futuro'));
-                return false;
-            }
-
-            if (age < minAge) {
-                $error.text(_t(`El niño debe tener al menos ${minAge} años`)).show();
-                $birth[0]?.setCustomValidity(_t(`El niño debe tener al menos ${minAge} años`));
-                return false;
-            }
+            $error.hide();
+            $birth[0]?.setCustomValidity('');
             return true;
         }
     });
