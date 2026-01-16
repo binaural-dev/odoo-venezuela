92532ec18df44f062672a942fda36b2bc23c3631
David Mendoza <davidmendoza@binaural.com.ve>
2026-01-16 15:06:53 -0400

    FIX #10503: l10n_ve_[tax,invoice]
    Problema: Se requiere una columna que muestre el total por factura de los totales con iva y exentos en los libros de compra y venta.
    
    Solución:
    
    l10n_ve_tax
     res_config_settings:
      -Puesto que el campo not_show_international_purchase_in_book y sus onchanges se usaban para saber si se deberian incluir las facturas con diario internacional en los libros de compra y ventas, se elimina del python y las vistas para implementacion de logica que cubra este proposito en el propio account reports, puesto que los onchanges causaban problemas a la hora de elegir las configuraciones individuales de compras internacionales.
      -Se agrega el campo de configuracion not_show_total_purchases_international, para elegir si la columna del total internacional se reflejara en el libro de compras.
    
    l10n_ve_invoice
     accounting_reports:
       -_get_domain: se reemplaza el uso de not_show_international_purchase_in_book por las variables all_sub_aliquots_hidden (que almacenan las configuraciones pertinentes) y is_hidden_international (revisa mediante un all() que ninguna de las columnas internacionales deban mostrarse), en las condicion del domain que decide si se toman en cuenta las facturas con diario de compra internacional.
     -en las funciones _fields_purchase_book_line y _fields_sale_book_line se agregan los campos "total_purchases" y "total_sales" respectivamente, ademas de "total_purchases_international" en caso de que aplique.
    -en las funciones _get_purchase_book_field_groups y _get_sale_book_field_groups se agregan las columnas de "Total compras", "Total ventas", ademas de "Total compras" en el grupo de los totales internacionales en caso de que apliquen mediante la configuracion.​

---
+++ l10n_ve_tax/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Venezuela - Impuestos",
+    "summary": """
+        Impuestos para la localización en Venezuela
+    """,
+    "license": "LGPL-3",
+    "author": "binaural-dev",
+    "website": "https://binauraldev.com/",
+    "category": "Accounting/Localizations/Account Chart",
+    "version": "17.0.0.0.13",
+    # any module necessary for this one to work correctly
+    "depends": ["base", "account", "l10n_ve_base", "l10n_ve_rate"],
+    "data": [
+        "views/res_config_settings.xml",
+        "views/account_move.xml",
+        "views/account_joural.xml",
+    ],
+    "images": ["static/description/icon.png"],
+    "application": True,
+    "assets": {
+        "web.assets_backend": ["l10n_ve_tax/static/src/components/**/*"],
+    },
+}
