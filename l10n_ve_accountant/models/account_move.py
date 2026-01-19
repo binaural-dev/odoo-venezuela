75f6640d969c79c0c606470f0bd9196b65c63054
RafaelDT-DEV <rafael@binaural.com.ve>
2026-01-19 10:07:15 -0400

    MOD: l10n_ve_account Se agrego una validacion para que valide solo las facturas de cliente para el limite de credito ya que todo account_move que se crea, sea NC, ND, Pagos, etc esta entrando en esta validacion y reventando

--- l10n_ve_accountant/models/account_move.py
+++ l10n_ve_accountant/models/account_move.py
@@ -1023,6 +1023,7 @@ class AccountMove(models.Model):
             if (
                 invoice.company_id.account_use_credit_limit
                 and invoice.partner_id.use_partner_credit_limit
+                and invoice.move_type in ("out_invoice")
             ):
                 total_pay = invoice.partner_id.credit + invoice.amount_residual
                 if total_pay > invoice.partner_id.credit_limit:
