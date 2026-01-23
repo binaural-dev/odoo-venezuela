f14a1d72fbee3e669f17a9ff6fe378701a3d0f01
OmarYepez29 <omar@binauraldev.com>
2026-01-23 07:26:46 -0400

    [IMP]Se traen de nuevo los metodos de calculo anterirores al refactor.

--- l10n_ve_pos/static/src/overrides/models/orderline_model.js
+++ l10n_ve_pos/static/src/overrides/models/orderline_model.js
@@ -4,8 +4,6 @@ import { Orderline } from "@point_of_sale/app/store/models";
 import { patch } from "@web/core/utils/patch";
 import {
   formatFloat,
-  roundPrecision as round_pr,
-  roundDecimals as round_di,
   floatIsZero,
   roundDecimals as round_di,
   roundPrecision as round_pr,
