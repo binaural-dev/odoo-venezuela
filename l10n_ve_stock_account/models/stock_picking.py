654226ee6fe0a8d94d5e20a9697e502e16472f5f
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2026-01-28 13:05:51 -0400

    FIX #10623: l10n_ve_stock_account
    Problema:
    - Al generar una guía de despacho desde ventas, al momento de generar la factura, no esta apareciendo el número de guía de despacho en la misma.
    - (Adicional) Al intentar instalar el modulo en un ambiente nuevo sale un error por no encontrar la referencia al motivo de translado de 'transfer_reason_consignment' ya que esta en un _compute y se ejecuta antes que el data que crea el motivo de translado.
    
    Solución:
    - Se modifico la forma de crear la factura desde la guia de despacho (create_invoice), luego de crear la factura se guarda el campo de la guia de despacho de origen (picking_ids) en la misma factura.
    - Se agrego un raise_if_not_found al buscar la referencia del motivo de translado y se valida si trae valor o no.
    
    Tarea (Link): https://binaural.odoo.com/web?db=binaural-dev-binaural-release-10413381&token=4P6sT2NxLGgos2OwxwuG#id=10623&cids=2&menu_id=302&action=386&model=helpdesk.ticket&vie>
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_stock_account/models/stock_picking.py
+++ l10n_ve_stock_account/models/stock_picking.py
@@ -214,6 +214,7 @@ class StockPicking(models.Model):
                             "narration": picking_id.name,
                             "partner_id": picking_id.partner_id.id,
                             "payment_reference": picking_id.name,
+                            "picking_ids": picking_id,
                             "transfer_ids": self,
                             "from_picking": True,
                             "fiscal_position_id": picking_id.sale_id.fiscal_position_id.id if picking_id.sale_id.fiscal_position_id else False,
@@ -1176,10 +1177,11 @@ class StockPicking(models.Model):
 
     @api.depends('is_consignment', 'is_dispatch_guide', 'transfer_reason_id')
     def _compute_partner_required(self):
-        consignment_reason = self.env.ref('l10n_ve_stock_account.transfer_reason_consignment')
+        consignment_reason = self.env.ref('l10n_ve_stock_account.transfer_reason_consignment', raise_if_not_found=False)
         for picking in self:
             picking.partner_required = (
-                picking.transfer_reason_id.id == consignment_reason.id
+                consignment_reason
+                and picking.transfer_reason_id.id == consignment_reason.id
                 and picking.is_dispatch_guide
                 and picking.is_consignment
             )
