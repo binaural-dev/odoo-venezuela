045891128e2974a5f5fd96d1a80d6c377dad5924
Jaco <jaco@binaural.com.ve>
2026-01-28 00:16:34 -0400

    FEAT #63065: l10n_ve_accountant, l10n_ve_invoice
    Problema:
    -Se requiere agregar un campo para escribir la Declaración Única de Aduana en las facturas cuando el diario de esta sea para compras internacionales.
    
    -Se requiere agregar 2 columnas nuevas para el libro de compras, uno para mostrar la Declaración Única de Aduana, y otro para mostrar el Valor total de las importaciones definitivas.
    
    Solución:
    -Se agrega el campo declaration_unique_of_customs y el campo is_purchase_international, este último para guardar si una factura es de compras internacionales.
    
    -Se agrega a la vista del formulario para las facturas declaration_unique_of_customs. Se agrega invisible para que este se oculte cuando is_purchase_international sea false, readonly solamente cuando está posteado o cancelado, y es requerido cuando is_purchase_international sea True.
    
    -Se agrega al campo correlative, en el required "not is_purchase_international" , para que así este no se pida al guardar en una factura de compras internacionales.
    
    -Se agrega lógica en el create y en el write para asignar o borrar los valores de correlative y declaration_unique_of_customs. Cuando una factura es de compras internacionales, el correlative es igual a lo que el usuario tipeó en declaration_unique_of_customs.
    
    -En accounting_reports, modificamos que el correlative de una factura, no se muestre cuando esta sea de compras internacionales. Agregamos declaration_unique_of_customs para mostrarlo como primera columna, y segundo creamos un nuevo campo (amount_import_international), este dentrro, guarda los montos de BASE IMPONIBLE + IVA de cada una de las alícuotas mostradas.
    
    -Se corrigen errores de tipeo en el libre de compras .
    
    -Se agrega a la vista filter el  search por el cambio de declaration_unique_of_customs.
    
    Tarea (Link):
    https://binaural.odoo.com/web?debug=1#id=63065&cids=2&menu_id=262&action=327&active_id=306&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_accountant/models/account_move.py
+++ l10n_ve_accountant/models/account_move.py
@@ -166,6 +166,10 @@ class AccountMove(models.Model):
     
     foreign_inverse_rate_vef = fields.Float(compute="_compute_inverse_rate_vef",store=True)
 
+
+    declaration_unique_of_customs = fields.Char('Declaration unique of customs',copy=False)
+    is_purchase_international = fields.Boolean(related='journal_id.is_purchase_international', string='Is International Purchase')
+
     @api.depends('invoice_date', 'date', 'company_id.currency_foreign_id')
     def _compute_inverse_rate_vef(self):
         Rate = self.env['res.currency.rate']
@@ -431,6 +435,9 @@ class AccountMove(models.Model):
                     )
                     % ({"rate": move.foreign_rate, "last_rate": last_foreign_rate})
                 )
+
+            if move.is_purchase_international and move.declaration_unique_of_customs and not move.correlative:
+                move.correlative = move.declaration_unique_of_customs
         return moves
 
     @api.onchange("partner_id")
@@ -470,6 +477,14 @@ class AccountMove(models.Model):
                     )
                     % ({"rate": move.foreign_rate, "last_rate": move.last_foreign_rate})
                 )
+            
+            if move.is_purchase_international and move.declaration_unique_of_customs:
+                if move.correlative != move.declaration_unique_of_customs:
+                    move.correlative = move.declaration_unique_of_customs
+            elif not move.is_purchase_international and move.correlative and move.correlative == move.declaration_unique_of_customs:
+                move.correlative = False
+                move.declaration_unique_of_customs = False
+
             new_journal_id = move.journal_id.id
             if old_journal_id and new_journal_id and old_journal_id != new_journal_id:
                 if move.is_invoice(include_receipts=True) and move.move_type in ('out_invoice', 'out_refund', 'out_receipt'):
