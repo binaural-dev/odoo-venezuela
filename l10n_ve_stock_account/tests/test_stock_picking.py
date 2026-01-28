32d05d89890eaadec64957f1553a61cfbef39a28
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2026-01-28 13:05:51 -0400

    FIX #10623: l10n_ve_stock_account
    Problema:
    - Al generar una guía de despacho desde ventas, al momento de generar la factura, no esta apareciendo el número de guía de despacho en la misma.
    - (Adicional) Al intentar instalar el modulo en un ambiente nuevo sale un error por no encontrar la referencia al motivo de translado de 'transfer_reason_consignment' ya que esta en un _compute y se ejecuta antes que el data que crea el motivo de translado.
    
    Solución:
    - Se modifico la forma de crear la factura desde la guia de despacho (create_invoice), luego de crear la factura se guarda el campo de la guia de despacho de origen (picking_ids) en la misma factura.
    - Se agrego un raise_if_not_found al buscar la referencia del motivo de translado y se valida si trae valor o no.
    
    Tarea (Link): https://binaural.odoo.com/web?db=binaural-dev-binaural-release-10413381&token=4P6sT2NxLGgos2OwxwuG#id=10623&cids=2&menu_id=302&action=386&model=helpdesk.ticket&vie>
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_stock_account/tests/test_stock_picking.py
+++ l10n_ve_stock_account/tests/test_stock_picking.py
@@ -218,6 +218,10 @@ class TestStockPickingInvoice(TransactionCase):
             len(invoice.invoice_line_ids) == len(order.order_line),
             "The invoice created from the sales orders dispatch guide must have the same number of lines as the sales order.",
         )
+        self.assertTrue(
+            invoice.picking_ids == dispatch_guide,
+            "The invoice must be linked to the dispatch guide from which it was created.",
+        )
         _logger.info("test_01_generate_invoice_from_dispatch_guide --- successfully.")
 
     def test_02_generate_dispatch_guide_from_purchase_order(self):
