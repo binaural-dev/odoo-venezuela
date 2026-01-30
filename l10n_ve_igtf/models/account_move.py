376a140276c02e087cc9d9e1dfb0990c532c490f
David Mendoza <davidmendoza@binaural.com.ve>
2026-01-30 16:39:29 -0400

    FEAT #87515b42: l10n_ve_igtf
    Problema: Agregarle pruebas unitarias a las funciones que agregan las columnas de igtf en los libros de compra y venta
    
    Solución:
    
    -Se agregan los commons para crear los ambientes para crear las facturas de proveedor y clientes, y sus respectivos pagos.
    -Se agregan las pruebas para libro de compras y ventas en sus repectivos archivos.
    
    Tarea (Link): https://binaural.odoo.com/web#id=63125&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_igtf/models/account_move.py
+++ l10n_ve_igtf/models/account_move.py
@@ -336,3 +336,140 @@ class AccountMove(models.Model):
         for record in self:
             record.bi_igtf = 0
         return super().button_draft()
+
+    @api.depends('amount_residual')
+    def compute_bi_igtf(self):
+        for rec in self.filtered(lambda rec: rec.move_type in ['out_invoice','in_invoice','out_refund','in_refund']):
+            
+            amount = rec.amount_total
+            if rec.company_currency_id == self.env.ref("base.VEF"):
+                amount = rec.foreign_total_billed
+            rec.igtf_top_aply = amount * (self.company_id.igtf_percentage / 100)
+
+            receivable_payable_lines = rec.line_ids.filtered(lambda line: line.account_id.reconcile)
+
+            account = [self.company_id.customer_account_igtf_id.id,self.company_id.supplier_account_igtf_id.id ]
+            
+            payment_moves = self.env['account.move']
+           
+            all_partial_reconciles = receivable_payable_lines.matched_debit_ids | receivable_payable_lines.matched_credit_ids
+            
+            payment_moves |= all_partial_reconciles.mapped('debit_move_id.move_id')
+            payment_moves |= all_partial_reconciles.mapped('credit_move_id.move_id')
+
+            final_payment_moves = payment_moves.filtered(lambda m: m.state == 'posted' and m.id != rec.id)
+            total_bi_igtf = 0.0
+            total_foreign_bi_igtf = 0.0
+            igtf_top = 0.0
+            alter_bi_igtf = 0.0
+            foreign_alter_bi_igtf = 0.0
+            bank_amount = 0.0
+
+            foreign_bank_amount = 0.0
+            foreign_igtf_amount = 0.0
+            target_account = False
+
+            partial_amount = 0.0
+            partial_foreign_amount = 0.0
+            partner_context = rec.partner_id.with_company(rec.company_id)
+            for payment_move in final_payment_moves:
+                igtf_line = payment_move.line_ids.filtered(lambda line: line.account_id.id in account)
+                bank_line = payment_move.line_ids.filtered(lambda line: line.account_id.account_type in ['asset_cash','asset_receivable'])
+                
+                if rec.move_type in ['out_invoice', 'out_refund']:
+                    target_account = partner_context.property_account_receivable_id
+                else:
+                    target_account = partner_context.property_account_payable_id
+
+                factura_line = rec.line_ids.filtered(lambda l: l.account_id.id == target_account.id)
+
+                pago_line = payment_move.line_ids.filtered(lambda l: l.account_id.id == target_account.id)
+                
+                partial = self.env['account.partial.reconcile'].search([
+                    '|',
+                    '&', ('debit_move_id', '=', factura_line.id), ('credit_move_id', '=', pago_line.id),
+                    '&', ('debit_move_id', '=', pago_line.id), ('credit_move_id', '=', factura_line.id)
+                ], limit=1)
+
+                if partial:
+                    if bank_line[0].company_currency_id == self.env.ref("base.VEF") :
+                        partial_amount = abs(partial.foreign_amount) 
+                        partial_foreign_amount = abs(partial.amount) 
+                    else:
+                        partial_amount =  abs(partial.amount) 
+                        partial_foreign_amount = abs(partial.foreign_amount) 
+
+                    
+
+                if bank_line:
+                   
+                    bank_amount = partial_amount
+                    foreign_bank_amount = partial_foreign_amount
+                        
+               
+                if igtf_line:
+                    if igtf_line[0].company_currency_id == self.env.ref("base.VEF"):
+                        igtf_amount = abs(igtf_line[0].foreign_balance) 
+                        foreign_igtf_amount = abs(igtf_line[0].balance) 
+                    else:
+                        igtf_amount = abs(igtf_line[0].balance)
+                        foreign_igtf_amount = abs(igtf_line[0].foreign_balance) 
+
+
+                if not igtf_line and bank_line:
+                    
+                    igtf_top += bank_amount
+
+
+
+                amount_base_payment = 0.0
+                foreign_amount_base_payment = 0.0
+
+                if igtf_line and bank_line:
+
+                    if payment_move.payment_id and payment_move.payment_id.reconciled_invoices_count > 1:
+
+                        amount_base_payment = partial_amount
+                        foreign_amount_base_payment = partial_foreign_amount
+                    
+
+                    elif (bank_amount* (rec.company_id.igtf_percentage / 100)) < igtf_amount:
+                        if (bank_amount * (rec.company_id.igtf_percentage / 100)) == igtf_amount:
+                            
+                            amount_base_payment = bank_amount
+                            foreign_amount_base_payment = foreign_bank_amount
+                        else:
+                            
+                            amount_base_payment = igtf_amount / (rec.company_id.igtf_percentage / 100)
+                            foreign_amount_base_payment = foreign_igtf_amount / (rec.company_id.igtf_percentage / 100)
+
+                        
+                        if 'pos_payment_ids' in bank_line[0].move_id._fields:
+                            if bank_line[0].move_id.pos_payment_ids:
+                                amount_base_payment = igtf_amount / (rec.company_id.igtf_percentage / 100)
+                                foreign_amount_base_payment = foreign_igtf_amount / (rec.company_id.igtf_percentage / 100)
+                    else:
+                        
+
+                        amount_base_payment = igtf_amount / (rec.company_id.igtf_percentage / 100)
+                        foreign_amount_base_payment = foreign_igtf_amount / (rec.company_id.igtf_percentage / 100)
+
+                if igtf_line:
+                    
+                    alter_bi_igtf += igtf_amount
+                    foreign_alter_bi_igtf += foreign_igtf_amount
+
+                total_bi_igtf += amount_base_payment
+                total_foreign_bi_igtf = foreign_amount_base_payment
+            
+            apply = rec.igtf_top_aply - (igtf_top * (rec.company_id.igtf_percentage / 100))
+            rec.write({
+                'igtf_top_aply': apply,
+                'alter_bi_igtf': alter_bi_igtf,
+                'foreign_alter_bi_igtf': foreign_alter_bi_igtf,
+                'foreign_bi_igtf': total_foreign_bi_igtf
+            })
+            rec.bi_igtf = total_bi_igtf
+                    
+
+
--- l10n_ve_igtf/tests/__init__.py
+++ l10n_ve_igtf/tests/__init__.py
@@ -5,3 +5,7 @@ from . import new_igtf_common_partner
 from . import test_igtf_partner
 from . import new_igtf_common_providers
 from . import test_igtf_providers
+from . import test_sale_book_igtf_usd_partner_formal
+from . import test_purchase_book_igtf_usd_provider_formal
+from . import test_common_purchase_book_igtf_usd_provider_formal
+from . import test_common_sale_book_igtf_usd_partner_formal
