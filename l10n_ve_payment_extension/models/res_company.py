3c2291bcb0d1d8a2ee31b5e55e445ed45fe6ef3f
SaulOrte <saul@binaural.dev>
2026-02-03 12:32:19 -0400

    Modificaciones a reporte de retenciones minicipales: Se agrega campo Fecha emion de Comprobante, el mismo se asigna al momento de la primera emision del comprobante de retencion municipal, se ajustan columnas del reporte para que se muestre todo el texto en la celda, se agrega la coletilla 'Total retenido', se condiciona la fecha de entrega en base a una configuracion que permite que la misma se muestre o se oculte en el reporte, se actualiza el encabezado en base a providencia Vigente, se ubica imagen de firma en posicion correspondiente

--- l10n_ve_payment_extension/models/res_company.py
+++ l10n_ve_payment_extension/models/res_company.py
@@ -49,3 +49,5 @@ class ResCompany(models.Model):
     create_retentions_of_suppliers_in_draft = fields.Boolean(
         string="Create Suppliers Retentions in Draft", default=False
     )
+
+    hide_issue_date_of_municipal_withholding_receipt = fields.Boolean('Hide issue date of municipal withholding receipt',default=False)
