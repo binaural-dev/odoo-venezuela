a841afc4d222c24237cc9a2f0db709ae561e8a78
ivan-binaural <ivan@binaural.dev>
2026-02-08 13:31:40 -0400

    FIX #62241 l10n_ve_accountant
    se agrega un nuego modulo en donde estaran las dependencias solicitadas al momento de instalar
    
    Tarea (Link):https://binaural.odoo.com/web/#id=62241&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    
    Ticket de soporte

---
+++ binaural_invoice_pricelist/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Binaural Invoice Pricelist',
+    'version': '1.0',
+    'summary': 'Custom invoice pricelist for Venezuela',
+    'description': 'Adds custom invoice pricelist functionality for Venezuelan localization.',
+    'author': 'binaural-dev',
+    "website": "https://binauraldev.com",
+    'category': 'Accounting',
+    'depends': ['account','l10n_ve_accountant', 'account_invoice_pricelist', 'account_invoice_pricelist_sale' ],
+    'data': [],
+    'auto_install': True,
+}
