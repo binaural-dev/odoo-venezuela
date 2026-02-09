b125adf741acb7d19dde7fdb5d02fd341bdf9c42
binauralbot CI/CD <binauralbot@binaural.com.ve>
2026-02-09 10:40:03 -0400

    17.0_fix-ti_10623_dispatch_guide_number_on_invoice
    Merged

--- l10n_ve_stock_account/models/stock_picking.py
+++ l10n_ve_stock_account/models/stock_picking.py
@@ -1248,21 +1248,6 @@ class StockPicking(models.Model):
             "l10n_ve_stock_account.transfer_reason_consignment",
             raise_if_not_found=False,
         )
-        for picking in self:
-            picking.partner_required = (
-                consignment_reason
-                and picking.transfer_reason_id.id == consignment_reason.id
-                and picking.is_dispatch_guide
-                and picking.is_consignment
-            )
-            picking._assign_partner_from_location()
-
-    @api.depends("is_consignment", "is_dispatch_guide", "transfer_reason_id")
-    def _compute_partner_required(self):
-        consignment_reason = self.env.ref(
-            "l10n_ve_stock_account.transfer_reason_consignment",
-            raise_if_not_found=False,
-        )
 
         if not consignment_reason:
             consignment_reason = self.env["transfer.reason"].search(
