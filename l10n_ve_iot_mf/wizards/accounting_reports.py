04652615e05b69851a521aa43e3b8fb6ae067607
OmarYepez29 <omar@binauraldev.com>
2026-02-10 17:04:04 -0400

    [IMP]l10n_ve_iot_mf-#63928 Se agrega un nuevo campo en el wizard para incluir las facturas de forma libre y de máquina fiscal en el libro de ventas.
    Link:https://binaural.odoo.com/web#id=63928&cids=2&menu_id=257&model=project.task&view_type=form

--- l10n_ve_iot_mf/wizards/accounting_reports.py
+++ l10n_ve_iot_mf/wizards/accounting_reports.py
@@ -11,6 +11,25 @@ class WizardAccountingReportsBinauralInvoice(models.TransientModel):
 
     with_fiscal_machine = fields.Boolean(default=False)
 
+    all_documents = fields.Boolean(
+        default=False,
+        string="Include all issued documents",
+        help="It includes all documents, both those issued by a fiscal machine and those issued by free form.",
+    )
+
+    def _get_domain_all_documents(self):
+        """
+        Returns two domains, one for free form documents and one for fiscal machine documents
+        
+        """
+        domain_free_form = self._get_domain()
+        domain_fiscal_machine = self._get_domain()
+        domain_fiscal_machine = [d for d in domain_fiscal_machine if d != ('correlative', 'not in', ['/', False])]
+        domain_fiscal_machine.append(("mf_invoice_number", "!=", False))
+        domain_fiscal_machine.append(("mf_reportz", "!=", False))
+        domain_fiscal_machine.append(("mf_serial", "!=", False))
+        return domain_free_form , domain_fiscal_machine
+
     def _get_domain(self):
         res = super()._get_domain()
         if not self.with_fiscal_machine:
@@ -27,16 +46,21 @@ class WizardAccountingReportsBinauralInvoice(models.TransientModel):
             res = res.filtered_domain([("mf_serial", "!=", False)])
             res = res.sorted(key=lambda r: r.invoice_date)
             return res
-
+        if self.all_documents:
+            domain_free_form , domain_fiscal_machine = self._get_domain_all_documents()
+            account_moves_free_form = self.env["account.move"].search(domain_free_form, order="invoice_date asc")
+            account_moves_fiscal_machine = self.env["account.move"].search(domain_fiscal_machine, order="invoice_date asc")
+            moves = account_moves_free_form + account_moves_fiscal_machine
+            return moves.sorted(key=lambda r: r.invoice_date or r.date)
         move_model = self.env["account.move"]
         domain = self._get_domain()
         moves = move_model.search(domain, order="invoice_date asc")
         return moves
-    
+
     def _get_sale_book_field_groups(self):
         sale_groups = super()._get_sale_book_field_groups()
 
-        if not self.with_fiscal_machine:
+        if not self.with_fiscal_machine and not self.all_documents:
             return sale_groups
 
         new_fields = [
@@ -47,17 +71,17 @@ class WizardAccountingReportsBinauralInvoice(models.TransientModel):
         for group in sale_groups:
             if group.get('header') == 'DETALLE DEL DOCUMENTO':
                 basic_fields = group['fields']
-                
+
                 insertion_index = -1
                 for i, field_dict in enumerate(basic_fields):
                     if field_dict.get('field') == 'move_type':
                         insertion_index = i + 1  
                         break
-                
+
                 if insertion_index != -1:
-                   
+
                     basic_fields.insert(insertion_index, new_fields[1]) 
-                    
+
                     basic_fields.insert(insertion_index, new_fields[0]) 
 
                 break 
@@ -66,15 +90,22 @@ class WizardAccountingReportsBinauralInvoice(models.TransientModel):
 
     def _fields_sale_book_line(self, move, taxes):
         res = super()._fields_sale_book_line(move, taxes)
-        if not self.with_fiscal_machine:
+        if not self.with_fiscal_machine and not self.all_documents:
             return res
-        del res["correlative"]
-        res["document_number"] = (
-            move.mf_invoice_number if move.mf_invoice_number else "-"
-        )
-        res["mf_reportz"] = move.mf_reportz if move.mf_reportz else "-"
-        res["mf_serial"] = move.mf_serial if move.mf_serial else "-"
-        res["number_invoice_affected"] = move.reversed_entry_id.mf_invoice_number or ""
+
+        if self.with_fiscal_machine:
+            del res["correlative"]
+
+        if move.mf_invoice_number:
+            res["document_number"] = move.mf_invoice_number
+
+        if move.reversed_entry_id.mf_invoice_number:
+            res["number_invoice_affected"] = move.reversed_entry_id.mf_invoice_number
+
+        res.update({
+            "mf_reportz": move.mf_reportz or "-",
+            "mf_serial": move.mf_serial or "-",
+        })
         return res
 
     def update_amounts(self, cumulative, amounts):
@@ -116,7 +147,7 @@ class WizardAccountingReportsBinauralInvoice(models.TransientModel):
         }
 
     def parse_sale_book_data(self):
-        if not self.with_fiscal_machine:
+        if not self.with_fiscal_machine and not self.all_documents:
             return super().parse_sale_book_data()
 
         sale_book_lines = []
