6af6d7006b42d6c142977c9c18d2242b251bc29d
Jaco <jaco@binaural.com.ve>
2026-02-20 10:06:30 -0400

    FEAT #63063: l10n_ve_accountant & l10n_ve_igtf
    Problema:
    -Se requiere hacerle un bypass a IGTF cuando el diario es de compras internacionales.
    Solución:
    -De account_payment_register.py se elimina la condicional de elf._from_sibling_companies(lines) ya que esta evitaba que corrieran las pruebas unitarias.
    
    -en _compute_amount_to_pay_igtf se condiciona para colocar el campo amount_to_pay_igtf en 0 si el diario de la factura es de compras internacionales.
    
    -En compute_bi_igtf, se condiciona para colocar todos los campos relacionados al bi_igtf en 0 cuando el diario sea de compras internacionales.
    
    -en compute_foreign_bi_igtf se condiciona para que los campos relacionados se asignen en 0 si el diario de la factura es de compras internacionales.
    
    -En account_payment.py se agrega validación para evitar error en la línea contable cuando se va a crear un pago con IGTF y no se tienen las cuentas configuradas para el mismo.
    
    -En _compute_check_igtf y _compute_is_igtf_journal se crean los flujos alternativos para saltarse los flujos de IGTF cuando el diario de factura es de compras internacionales.
    Tarea (Link):
    https://www.binauraldev.com/web#id=63063&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_igtf/tests/__init__.py
+++ l10n_ve_igtf/tests/__init__.py
@@ -2,3 +2,4 @@ from . import test_sale_book_igtf_usd_partner_formal
 from . import test_purchase_book_igtf_usd_provider_formal
 from . import test_common_purchase_book_igtf_usd_provider_formal
 from . import test_common_sale_book_igtf_usd_partner_formal
+from . import test_igtf_bypass
