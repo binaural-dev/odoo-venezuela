c8b0f34691b2ab57153e79c2d0c2ec60c645dcb5
SaulOrte <saul@binaural.dev>
2026-02-21 09:43:43 -0400

    [MIG] IGTF - Se Crea Migracion de IGTF y Anticipos. - SE Unifica la Logica de Pagos y Anticipos en 1 solo modulo, deprecando los relacionados a estos. - Se ajusta logica de calculo de igtf para el asiento de pago, este mismo se eliminan sus valores y vuelven a calcular para garantizar que el monto no se desbalance , aunq sigue persistiendo el problema del diferencial por tasa.

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.23",
+    "version": "1.24",
     "depends": [
         "base",
         "web",
