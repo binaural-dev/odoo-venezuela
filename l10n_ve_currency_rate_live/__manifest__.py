1e3407fce398647a0e5e3ce24c57b9d33838cd55
Jaco <jaco@binaural.com.ve>
2026-02-23 09:13:10 -0400

    FEAT #62106: l10n_ve_currency_rate_live
    Problema:
    -Al intentar realizar la conexión con el BCV, no se recibía respuesta alguna, ya que la página rechazaba la conexión.
    
    Solución:
    -Se le pasan headers al request para evitar que el BCV rechace la conexión.
    Tarea (Link):
    https://binaural.odoo.com/web#id=62106&cids=2&menu_id=257&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_currency_rate_live/__manifest__.py
+++ l10n_ve_currency_rate_live/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "1.2",
+    "version": "1.3",
     "depends": ["l10n_ve_rate", "currency_rate_live"],
     "data": [
         "views/res_config_settings.xml",
