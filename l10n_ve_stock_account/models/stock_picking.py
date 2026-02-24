9b10ed998946bb7de5bcf178db8db0c08436787e
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2026-02-24 16:10:58 -0400

    FEAT #57191: l10n_ve_stock_account
    Creando grupo y modificando vistas y campos para que lo usuario en ese grupo no puedan hacer cosas relacionadas con guias de despacho.
    
    Tarea (Link): https://binaural.odoo.com/web?db=binaural-dev-binaural-release-10413381&token=4P6sT2NxLGgos2OwxwuG#id=57191&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_stock_account/models/stock_picking.py
+++ l10n_ve_stock_account/models/stock_picking.py
@@ -978,11 +978,18 @@ class StockPicking(models.Model):
 
         for picking in self:
 
+            if self.env.user.has_group("l10n_ve_stock_account.group_not_dispatch_guide"):
+                picking.is_dispatch_guide = False
+                continue
+
             picking.is_dispatch_guide = (
                 False
                 if picking.is_dispatch_guide is None
                 else picking.is_dispatch_guide
             )
+
+            picking.is_dispatch_guide = False if picking.is_dispatch_guide is None else picking.is_dispatch_guide
+
             if picking.document == "invoice":
                 picking.is_dispatch_guide = False
                 continue
@@ -1236,6 +1243,9 @@ class StockPicking(models.Model):
             # Siempre último día del mes para estos tipos
             last_day = calendar.monthrange(hoy.year, hoy.month)[1]
             result = date(hoy.year, hoy.month, last_day)
+
+        if self.env.user.has_group("l10n_ve_stock_account.group_not_dispatch_guide"):
+            return
 
         return f"Tienes {len(pickings_combined)} guías de despacho sin facturar al {result.strftime('%d-%m-%Y')}. De facturarse en el siguiente periodo el Seniat será Notificado."
 
