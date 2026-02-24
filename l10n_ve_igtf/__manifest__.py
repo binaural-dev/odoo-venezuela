b4b0c5b5cbc3481f4a5800e5bd3211b9d7069dc2
SaulOrte <saul@binaural.dev>
2026-02-24 09:10:10 -0400

    SE corrige campo origin_payment_id en computados de widget de pagos, el mismo conserva el mismo nombre en las lineas de los apuntes, solamente cambio en el asiento

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -19,7 +19,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "19.0.0.0.6",
+    "version": "19.0.0.0.5",
 
     "depends": [
         "base",
