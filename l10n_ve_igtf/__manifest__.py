e8664a2d196720b811eb012193d100173d831622
yeisonasuaje <yeison@binaural.com.ve>
2026-02-26 12:24:32 -0400

    [FIX#11377] l10n_ve_igtf: -Error al momento de  realizar una retención de ISLR -Evita IndexError en compute_bi_igtf validando que bank_line no esté vacío antes de acceder a bank_line[0]. -Esto previene errores cuando no existen líneas bancarias en el movimiento de pago." -https://binaural.odoo.com/web#id=11377&cids=2&menu_id=303&action=387&model=helpdesk.ticket&view_type=form

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.0.0.44",
+    "version": "17.0.0.0.47",
         "depends": [
         "base",
         "l10n_ve_accountant",
