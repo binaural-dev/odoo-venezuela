7cf60023b37c741e0f3ab116e9207d2fa493a4be
Jaco <jaco@binaural.com.ve>
2026-02-27 12:12:47 -0400

    FIX #65047: l10n_ve_invoice
    Problema:
    -Totales nacionales e internacionales muestran montos copiados en
    factura de compras internacionales
    Solución:
    Función:  _determinate_amount_taxeds(self, move)
    Se agregó: Un bloque de código dentro de la condición if
    move.journal_id.is_purchase_international:. Motivo: En esta función se
    analizan los impuestos de la factura para clasificarlos en variables
    (general, reducida, exenta, etc.). Originalmente, cuando era compra
    internacional, Odoo copiaba esos valores capturados a nuevas variables
    con sufijo "_international" (ej.
    tax_base_general_aliquot_international), pero dejaba los montos vivos
    también en las variables originales (nacionales). Comportamiento nuevo:
    Agregamos una actualización al diccionario tax_result para que,
    inmediatamente después de copiar los montos a variables internacionales,
    fuerce a 0.0 todas las variables de impuestos nacionales base, exentas,
    no deducibles, IVA y totales. Esto "limpia" el lado nacional, evitando
    la duplicidad en el reporte.
    
    Función:  _fields_purchase_book_line(self, move, taxes)
    Se agregó/modificó: La forma en que se calcula la variable principal
    amount_taxed y tax_base_exempt_aliquot sólo para el cálculo de los
    campos totales de la fila (específicamente total_purchases,
    total_purchases_iva y total_purchases_not_iva). Motivo: En esta función
    se mapean las variables que van a escribirse en el Excel. Como en el
    paso anterior forzamos a cero el  amount_taxed
     base para una factura internacional, la columna general de "Total
    Compras" en el libro estaba imprimiendo 0.00. Comportamiento nuevo:
    Ahora, antes de armar el diccionario  fields_purchase_book_line, sumamos
    explícitamente el monto nacional más el internacional.
    
    Tarea (Link):
    https://binaural.odoo.com/web#id=65047&cids=2&menu_id=1063&action=345&active_id=3056&model=project.task&view_type=form
     Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_invoice/__manifest__.py
+++ l10n_ve_invoice/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Módulo de Facturación Venezuela
     """,
-    "version": "1.15",
+    "version": "1.16",
     "license": "LGPL-3",
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
