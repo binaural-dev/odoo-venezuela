2f5d2046df994207683b50183ee99aaa1ec50803
Jaco <jaco@binaural.com.ve>
2026-03-05 12:47:48 -0400

    FEAT #65633: l10n_ve_accountant
    Problema:
    -Se puede editar el campo de IVA en el tax_totals en facturas de proveedores, se requiere evitar este comportamiento.
    
    Solución:
    
    Sobreescribir el getter readonly del  TaxTotalsComponent de Odoo usando Object.defineProperty para que siempre retorne true, bloqueando la edición inline del monto de impuesto en el widget tax_totals desde la UI, sin afectar los cálculos del backend.
    
    Tarea (Link):
    https://binaural.odoo.com/web#id=65633&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.32",
+    "version": "1.33",
     "depends": [
         "base",
         "web",
