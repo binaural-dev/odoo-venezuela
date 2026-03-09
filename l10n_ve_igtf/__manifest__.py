b78e6684d198dcf93412b3eab6587c23a02a8e87
luisHerice <luis.herice@binaural.com.ve>
2026-03-09 18:48:52 -0400

    FIX #11631 l10n_ve_igtf
    problema: Error "Expected singleton" al facturar en el punto de venta cuando se aplican múltiples métodos de pago (pagos fraccionados múltiples).
    
    solucion: Modificada la validación de conciliación en account.partial.reconcile para usar colecciones (.ids), permitiendo procesar y conciliar correctamente múltiples líneas de pago a la vez.
    
    link: https://binaural.odoo.com/web#id=11631&cids=2&menu_id=293&action=390&model=helpdesk.ticket&view_type=form
    
    ticket de soporte [x]

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.0.0.50",
+    "version": "17.0.0.0.51",
     "depends": [
         "base",
         "l10n_ve_accountant",
