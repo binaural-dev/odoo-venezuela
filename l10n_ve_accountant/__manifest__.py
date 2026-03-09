84c5c795c0dd203b4bfedf1aa685d731586dd2f6
Jaco <jaco@binaural.com.ve>
2026-03-09 16:34:12 -0400

    IMP #65755: l10n_ve_accountant
    Problema:
    
    -Al cambiar la fecha de tasa (invoice_date), se cambia la fecha contable en la factura de proveedores, el comportamiento correcto debería ser el mismo pero en ves de fecha de tasa (invoice_date), que sea fecha de factura (invoice_date_display)
    
    Solución:
    -Se hereda _compute_rate para agregar como dependencia invoice_date_display.
    
    -Se hereda _get_accounting_date_source para que asigne como fecha contable, la fecha que está en invoice_date_display cuando el documento sea de compras.
    
    -Se agrega el default para que coloque la fecha de creación de la factura en el campo invoice_display_date.
    
    Tarea (Link):
    https://binaural.odoo.com/web#id=65755&cids=2&menu_id=257&action=341&model=project.task&view_type=form
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.36",
+    "version": "1.37",
     "depends": [
         "base",
         "web",
