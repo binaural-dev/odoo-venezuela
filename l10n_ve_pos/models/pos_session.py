5534b333ace344277c010ba6868dea5f21c47e9e
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2026-03-11 15:28:32 -0400

    FIX #11297: l10n_ve_pos
    Problema:
    - Las operaciones realizadas desde el POS con ciertos pagos causan que al cerrar la sesion se creen asientos contables sin tasa.
    
    Solución:
    - Se modifica la funcion de "_create_combine_account_payment" para agregar flujo nativo de odoo y forzar la cuenta de destino del pago.
    
    Tarea (Link): https://binaural.odoo.com/web#id=11297&cids=2&menu_id=302&action=386&model=helpdesk.ticket&view_type=form
    
    Tarea de proyecto []
    Ticket de soporte [x]

--- l10n_ve_pos/models/pos_session.py
+++ l10n_ve_pos/models/pos_session.py
@@ -505,16 +505,38 @@ class PosSession(models.Model):
         return res
 
     def _create_combine_account_payment(self, payment_method, amounts, diff_amount):
-        res = super(
-            PosSession, self.with_context(from_pos=True)
-        )._create_combine_account_payment(payment_method, amounts, diff_amount)
-        account_payment = res.move_id.payment_id
+        outstanding_account = payment_method.outstanding_account_id or self.company_id.account_journal_payment_debit_account_id
+        destination_account = self._get_receivable_account(payment_method)
+
+        if float_compare(amounts['amount'], 0, precision_rounding=self.currency_id.rounding) < 0:
+            # revert the accounts because account.payment doesn't accept negative amount.
+            outstanding_account, destination_account = destination_account, outstanding_account
+
+        account_payment = self.env['account.payment'].with_context(pos_payment=True).create({
+            'amount': abs(amounts['amount']),
+            'journal_id': payment_method.journal_id.id,
+            'force_outstanding_account_id': outstanding_account.id,
+            'destination_account_id':  destination_account.id,
+            'ref': _('Combine %s POS payments from %s', payment_method.name, self.name),
+            'pos_payment_method_id': payment_method.id,
+            'pos_session_id': self.id,
+            'company_id': self.company_id.id,
+        })
+
+        diff_amount_compare_to_zero = self.currency_id.compare_amounts(diff_amount, 0)
+        if diff_amount_compare_to_zero != 0:
+            self._apply_diff_on_account_payment_move(account_payment, payment_method, diff_amount)
+
+        account_payment.action_post()
         account_payment.with_context(skip_account_move_synchronization=True).write(
             {
                 "foreign_rate": self.config_id.foreign_rate,
                 "foreign_inverse_rate": self.config_id.foreign_inverse_rate,
+                "destination_account_id": destination_account.id,
             }
         )
+
+        res = account_payment.move_id.line_ids.filtered(lambda line: line.account_id == account_payment.destination_account_id)
 
         for line in account_payment.move_id.line_ids:
             if line.credit > 0 and amounts.get("foreign_amount", False):
