db86d287b678d922d41a150671676bde483f5af5
Jaco <jaco@binaural.com.ve>
2026-03-11 16:17:11 -0400

    [FEAT] #65929: l10n_ve_payment_extension
    Problema:
    -Se requiere crear flujos para creación de comprobantes de retención a
    nombre de terceros.
    
    Solución:
    -Se crea el campo is_third_party_retention, para marcar cuando la
    factura es para generarle una retención a terceros.
    
    -Se crean los campos computados [third_party_iva_retention_count,
    third_party_islr_retention_count] para el conteo de retenciones a
    terceros que se han realizado.
    
    -Se crea el campo is_third_party_retention en account_retention para
    indicar cuando una retención viene desde el flujo de facturación para
    terceros, esta solamente debe ser True cuando proviene del flujo
    anteriormente mencionado, por ende, es un campo que debe estar readonly
    para evitar comportamientos inesperados.
    
    -Se crea el action action_view_third_party_iva_retentions, que abre la
    vista form o list de retenciones de IVA dependiendo si la factura tiene
    una o menos retenciones, si tiene más de una retención a terceros, abre
    la vista list, si no tiene retenciones, abre la vista form. Envía como
    default el tipo de la retención, las facturas disponibles (la factura a
    la cuál se le va a realizar la retención), envía por default una línea
    de retención que ya apunta a la factura, marca como True el campo
    is_third_party_retention en la retención. Lo mismo realizaa el action
    action_view_third_party_islr_retentions pero con ISLR.
    
    -En account_retention_line , se modifica para que _onchange_move_id,
    tome el partner desde la retención si este está disponible, sino,
    agarrará el de la factura, en withholding_amount se pasa el partner para
    que este sepa de donde agarrar el monto, también se agrega el .name para
    que no haya problemas al cambiar de partner.
    
    -En _compute_related_fields, se modifica para que agarre el partner de
    la retención y no el de la factura, así todos los cálculos relacionados
    a la retención se le realizan a este, y así evitar cálculos erróneos en
    una retención para terceros.
    
    -En _compute_retention_amount también se agrega que use el partner de la
    retención para calcular los montos de las líneas.
    
    -En account_retention, en la función onchange_partner_id, se agrega para
    que se recomputen los montos de una retención de IVA cuando se cambia el
    partner, volviendo a ejecutar _onchange_move_id si la retención es de
    terceros.
    
    -Se agregan los smart buttons a la vista de factura, y se limita su
    visibilidad dependiendo del campo is_third_party_retention.
    
    -Se agrega a la vista de retenciones el campo is_third_party_retention.
    
    Tarea (Link):
    
    https://binaural.odoo.com/web#id=65929&cids=2&menu_id=257&action=341&model=project.task&view_type=form
    
    Tarea de proyecto [x]
    Ticket de soporte []

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "1.7",
+    "version": "1.10",
     "depends": [
         "base",
         "account",
