723c16954f23bd75dda18f088b8061983087c7d1
SaulOrte <saul@binaural.dev>
2026-03-12 12:39:54 -0400

    [FIX] L10n_ve_igtf:
    -Ajustes de IGTF
    
    Al Momento de obtener los valores generados por odoo para las lineas del pago ya no genera el credito automaticamente, solo el Balance de las lineas por lo cual se cambio la logica q trabajaba con dicha Key del diccionario de valores.
    
    References:
    - Ticket:
    - Tarea(/s):
    https://binaural.odoo.com/web#id=65638&cids=2&menu_id=975&action=341&model=project.task&view_type=form
     https://binaural.odoo.com/web#id=64828&cids=2&menu_id=975&action=341&model=project.task&view_type=form
    - PR:
    - Otros:

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -19,7 +19,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "19.0.0.1.2",
+    "version": "19.0.0.1.3",
 
     "depends": [
         "base",
