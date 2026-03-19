a6f4753d265e70b4894dfb9d92e157bc29f3b466
Jaco <jaco@binaural.com.ve>
2026-03-19 11:28:04 -0400

    [I18N] l10n_ve_accountant: actualizar traducciones para fecha de factura
    Se actualiza el archivo de traducción es_VE.po para incluir la etiqueta "Fecha de factura" correspondiente al campo 'invoice_date_display' en la nueva vista de búsqueda de facturas.
    
    References: https://binaural.odoo.com/web#id=65944&cids=2&menu_id=975&action=341&model=project.task&view_type=form

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.40",
+    "version": "1.41",
     "depends": [
         "base",
         "web",
