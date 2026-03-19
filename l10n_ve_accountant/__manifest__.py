1ee764f224e062bc4b3240e53b68070bbfd8679b
Jaco <jaco@binaural.com.ve>
2026-03-19 11:01:29 -0400

    [FEAT] l10n_ve_accountant: añadir invoice_date_display a vistas lista y búsqueda de facturas
    Se incorpora el campo 'invoice_date_display' en las vistas de lista de facturas y se habilita su uso en los filtros de búsqueda y agrupaciones dentro de la vista de facturas/facturas de proveedor. Esto permite a los usuarios realizar un seguimiento y filtrado jerárquico (Año, Mes, etc.) basado en la fecha de factura real mostrada.
    
    References: https://binaural.odoo.com/web#id=65944&cids=2&menu_id=975&action=341&model=project.task&view_type=form

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.39",
+    "version": "1.40",
     "depends": [
         "base",
         "web",
