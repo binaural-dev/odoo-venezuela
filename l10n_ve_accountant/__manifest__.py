d9b53dd4e57993f43a2c74ffc4fcb894db72640e
Jaco <jaco@binaural.com.ve>
2026-03-23 09:30:49 -0400

    manifest

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.42",
+    "version": "1.43",
     "depends": [
         "base",
         "web",
