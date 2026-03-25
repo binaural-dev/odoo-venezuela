7fff4c8a0fa3f92f15e3a25f7b0c3ac3ecf85a14
Jaco <jaco@binaural.com.ve>
2026-03-24 21:03:22 -0400

    [FEAT] l10n_ve_accountant: Sincronización de fecha de tasa (invoice_date) para ventas
    Se implementa la sincronización automática de la fecha de tasa (invoice_date)
    mediante el campo invoice_date_display específicamente para documentos de
    venta. Además, se ajustan los atributos de la vista para que la fecha de
    tasa sea de solo lectura en ventas, forzando su guardado desde la interfaz.
    
    References: https://binaural.odoo.com/web#id=65755&model=project.task

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.41",
+    "version": "1.42",
     "depends": [
         "base",
         "web",
