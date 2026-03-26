eb764370ecfcadddab6994bacf431c6bb2eeb840
Jaco <jaco@binaural.com.ve>
2026-03-26 11:10:34 -0400

    [FIX] l10n_ve_invoice: ajustar formato de fecha en reportes
    - Se actualiza el reporte de factura de forma libre para mostrar la fecha de emisión (o.date) con formato DD/MM/YYYY.
    - Se incrementa la versión del módulo a 1.20.
    
    References: https://binaural.odoo.com/web#id=65755&model=project.task

--- l10n_ve_invoice/__manifest__.py
+++ l10n_ve_invoice/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Módulo de Facturación Venezuela
     """,
-    "version": "1.18",
+    "version": "1.20",
     "license": "LGPL-3",
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
