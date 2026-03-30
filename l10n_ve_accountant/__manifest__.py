8dce44f0ef0c9a6ac2a3f203397954b07cacdb40
SaulOrte <saul@binaural.dev>
2026-03-30 12:39:27 -0400

    [FIX]  L10n_ve_accountant: Al publicar un anticipo la tasa se pone en 0,00Bs
    -Solucion el campo requeria force_save en la vista ya que el mismo era computado y de sololectura en la vista.
    
    References:
    - Ticket:
    - Tarea: https://binaural.odoo.com/odoo/3555/action-345/67434?debug=1
    - PR:
    - Otros:

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "1.43",
+    "version": "1.45",
     "depends": [
         "base",
         "web",
