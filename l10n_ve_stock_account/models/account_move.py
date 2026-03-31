5491b6522dca36fef339d1fdab642d592f935b27
SaulOrte <saul@binaural.dev>
2026-03-31 17:34:01 -0400

    [FIX] l10n_ve_stock_account: Error al generar factura desde una entrega desde inventario
    - Bug: al intentar crear una factura desde inventario(entregas) aparecer un error con el numero de guia
    
    - Solucion: se valido el computado de numero de guia en account.move, el mismo solo esta presente en algunos picking dependiendo del motivo de entrega, en el codigo que se soluciono el mismo siempre se asignaba y cuando no existia generaba error al ser un campo computado y garantiza que si no existe no de error
    
    References:
    - Ticket: https://binaural.odoo.com/odoo/helpdesk/action-389/12067
    - Tarea:
    - PR:
    - Otros:

--- l10n_ve_stock_account/models/account_move.py
+++ l10n_ve_stock_account/models/account_move.py
@@ -30,8 +30,5 @@ class AccountMove(models.Model):
     @api.depends("picking_ids")
     def _compute_guide_number(self):
         for record in self:
-            list_guide_number = [picking.guide_number for picking in record.picking_ids]
-            if list_guide_number:
-                record.guide_number = "/".join(list_guide_number)
-            else:
-                record.guide_number = ""
+            guides = {p.guide_number for p in record.picking_ids if p.guide_number}
+            record.guide_number = ", ".join(guides) if guides else ""
