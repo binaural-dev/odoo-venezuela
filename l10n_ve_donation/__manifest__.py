5cfe39a35b50b2952946fac6b1bbcd54d02e3986
kouzte <oyepezalcala@gmail.com>
2026-04-07 09:16:29 -0400

    [FIX]l10n_ve_donation,l10n_ve_invoice:Se añade depedencia al manifest,se elimina validación.
    Se añade dependencia del modulo l10n_ve_sale para el modulo
    l10n_ve_donation.
    Se elimina validación en l10n_ve_invoice.

--- l10n_ve_donation/__manifest__.py
+++ l10n_ve_donation/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Venezuela - Donaciones",
     "author": "",
     "website": "",
-    "depends": ["l10n_ve_accountant", "l10n_ve_stock"],
+    "depends": ["l10n_ve_accountant", "l10n_ve_stock", "l10n_ve_sale"],
     "data": [
         "data/donation_certificate_paperformat.xml",
         "report/donation_certificate_report.xml",
