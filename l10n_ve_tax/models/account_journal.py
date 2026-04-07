683f743b54dd69bb738f3aa1b7e47b290cb454f8
Miguel Gozaine <96434156+miguel-binaural@users.noreply.github.com>
2026-04-07 09:25:08 -0400

    Fix company_id reference in international sale check

--- l10n_ve_tax/models/account_journal.py
+++ l10n_ve_tax/models/account_journal.py
@@ -36,7 +36,7 @@ class AccountJournal(models.Model):
         for record in self:
             if record.is_sale_international:
                 domain = [
-                    ('company_id','=',self.env.company.id),
+                    ('company_id', '=', record.company_id.id or self.env.company.id),
                     ('is_sale_international', '=', True),
                     ('id', '!=', record.id),
                 ]
