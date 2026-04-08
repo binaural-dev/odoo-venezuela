e2d181ec45bd6300fb1177babe14f1845a94e64f
JacoBinaural <jesus@binaural.com.ve>
2026-04-08 11:21:44 -0400

    Remove invoice_date field from account_move model
    Removed the invoice_date field from account_move model because its already exist.

--- l10n_ve_invoice/models/account_move.py
+++ l10n_ve_invoice/models/account_move.py
@@ -33,12 +33,6 @@ class AccountMove(models.Model):
         tracking=True,
     )
 
-    invoice_date = fields.Date(
-        string="Invoice Date",
-        default=fields.Date.today,
-        help="Date of the invoice. Defaults to today when creating a new invoice."
-    )
-
     last_payment_date = fields.Date(compute="_compute_payment_dates", store=True)
     first_payment_date = fields.Date(compute="_compute_payment_dates", store=True)
     is_contingency = fields.Boolean(related="journal_id.is_contingency")
