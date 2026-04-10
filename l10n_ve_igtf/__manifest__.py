77ef4a09aaec4b5b156243714e72b09f8426eb36
SaulOrte <saul@binaural.dev>
2026-04-10 16:38:36 -0400

    Se corrige la validacion de los pagos en la accion que llama al wizar de pagos

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -19,7 +19,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "19.0.0.1.9",
+    "version": "19.0.0.1.10",
 
     "depends": [
         "base",
