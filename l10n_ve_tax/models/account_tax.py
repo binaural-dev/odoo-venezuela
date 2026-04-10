de629e3a30ac1046a76a7656fcc7fdb54a11b497
luis silva <luis.silva@binaural.dev>
2026-04-10 12:40:45 -0400

    [FIX]l10n_ve_accountant,l10n_ve_igtf,l10n_ve_tax: diferencias en montoalterno
    Descripción
    Caso igtf en monto alterno
    se hace calcula en base a la bi igtf
    caso bi igtf cuando se realiza un pago completo se toma el monto total de la factura
    
    References:
    - Ticket: https://binaural.odoo.com/odoo/my-tickets/12158
    - Tarea:
    - PR:
    - Otros:

--- l10n_ve_tax/models/account_tax.py
+++ l10n_ve_tax/models/account_tax.py
@@ -176,18 +176,26 @@ class AccountTax(models.Model):
             return amounts
 
         content = invoice_payments.get('content') or []
+        
+        # Identificar la cuenta a cobrar/pagar de la factura
+        target_account_ids = move.line_ids.filtered(lambda l: l.account_id.account_type in ['asset_receivable', 'liability_payable']).mapped('account_id.id')
+
         for payment in content:
-            
             move_id = payment.get('move_id')
             payment_id = self.env['account.move'].browse(move_id)
             foreign_amt = 0.0
+            igtf_foreign_amt = 0.0
+            
+            # Sólo sumar el saldo de las líneas de cuentas por cobrar/pagar
             for line in payment_id.line_ids:
-              
+                if line.account_id.id in target_account_ids:
+                    foreign_amt += abs(line.foreign_balance)
+
+                # Aislar la línea cargada con el gasto del IGTF
+                if line.name == 'IGTF' or (line.account_id.id in [self.env.company.customer_account_igtf_id.id, self.env.company.supplier_account_igtf_id.id]):
+                    igtf_foreign_amt += abs(line.foreign_balance)
                 
-                foreign_amt = abs(line.foreign_balance)
-                
-            foreign_amt = foreign_amt
-            amounts.append(foreign_amt)
+            amounts.append(foreign_amt - igtf_foreign_amt)
         return amounts
 
     def get_foreign_base_tax_lines(self, base_lines, tax_lines, currency):
