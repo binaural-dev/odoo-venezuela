214080d387c80be52c254ec02c417bcaed6fb9a5
carlos-lin-binaural <carlos@binauraldev.com>
2026-04-13 09:11:20 -0400

    HOTFIX L10n_ve_igtf 67754
    - Problema> Se consiguió una función de odoo base heredada que no hace nada, está tal cual está en base
    
    - Solución> Se removió el código para poder ser heredable
    
    Tarea: https://binaural.odoo.com/odoo/action-341/67754

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -6,8 +6,8 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.1.0.0",
-        "depends": [
+    "version": "17.0.1.0.1",
+    "depends": [
         "base",
         "l10n_ve_accountant",
         "l10n_ve_rate",
@@ -15,8 +15,6 @@
         "l10n_ve_invoice",
         "l10n_ve_tax_payer",
     ],
-       
-    
     "data": [
         "views/account_journal.xml",
         "views/res_config_settings.xml",
