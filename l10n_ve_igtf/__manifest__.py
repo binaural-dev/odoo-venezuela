cec4272e0102d015ff564ccb6afb6cf49f965d78
SaulOrte <saul@binaural.dev>
2026-04-17 11:29:29 -0400

    Se ajustan los valores en wizard de pagos en lotes para que el mismo asuma la moneda del pago y convierta el valor adeudado origen en esa moneda cuando la moneda de la factura y la mmoneda del pago son diferentes, nativamente se implemento una validacion que impide dando preferencia a la moneda del sistema y los valores en moneda del sistema dentro de las facturas

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -19,7 +19,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "19.0.2.0.1",
+    "version": "19.0.2.0.2",
 
     "depends": [
         "base",
