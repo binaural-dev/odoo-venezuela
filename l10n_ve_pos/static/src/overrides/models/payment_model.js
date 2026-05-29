1d543a0d8517ce2224673931277e3d0eb0b94b75
RogerVBinaural <roger@binaural.dev>
2026-05-29 15:10:23 -0400

    [FIX] l10n_ve_pos: Error en calculo de monto alterno con pagos en moneda alterna
    Ticket 13284: Error en calculo de bs en método de pago pos al momento de realizar pago con 2 métodos
    
    Se hizo revision del calculo y nos percatamos de un error de signos.
    Se estaba usando el tasa inversa alterna para dividir el monto en lugar de usar una multiplicacion.
    El errror fue visto cuando revisando grupo kam, que si funciona correctamente, no se presentaba el error.
    
    References:
    - Ticket: https://binaural.odoo.com/odoo/my-tickets/13284
    - Tarea:
    - PR:
    - Otros:

--- l10n_ve_pos/static/src/overrides/models/payment_model.js
+++ l10n_ve_pos/static/src/overrides/models/payment_model.js
@@ -55,12 +55,12 @@ patch(Payment.prototype, {
 			if (this.pos.currency.name == "USD") {
 				if (this.payment_method.is_foreign_currency) {
 					this.set_amount(
-						this.foreign_amount * 1 / this.pos.foreign_currency.inverse_rate,
+						this.foreign_amount * this.pos.foreign_currency.inverse_rate,
 					);
 					return;
 				}
 				this.set_amount(
-					this.foreign_amount * 1 / this.order.init_conversion_rate,
+					this.foreign_amount * this.order.init_conversion_rate,
 					true,
 				);
 			}
