fdef9de680901d4869f3b4bb378f390f54f36def
yeisonasuaje <yeison@binaural.com.ve>
2026-06-04 16:21:25 -0400

    -fix(l10n_ve_contact #Ticket13369): evitar error de validación al fusionar -Se ajusta la validación de duplicado de RIF/VAT en check_duplicate_vat. Durante la fusión de contactos (active_model = res.partner), se excluyen los IDs en active_ids del dominio de búsqueda. Esto evita que los propios contactos en proceso de merge sean detectados como duplicados entre sí. Se permite completar la fusión y continuar el flujo sin ValidationError. -https://binaural.odoo.com/odoo/all-tickets/13369

--- l10n_ve_contact/__manifest__.py
+++ l10n_ve_contact/__manifest__.py
@@ -10,7 +10,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Contacts/Contacts",
-    "version": "17.0.0.0.6",
+    "version": "17.0.0.0.7",
     "depends": ["base", "contacts", "l10n_ve_rate", "l10n_ve_location"],
     "data": [
         "security/ir.model.access.csv",
