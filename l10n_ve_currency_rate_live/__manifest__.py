045544c0aa5be0b9c8c477a8c6ea424db3a78c50
Julio Nieto <julionieto@binaural.dev>
2026-06-08 14:39:30 -0400

    [FIX] l10n_ve_currency_rate_live: Actualización automatica de tasa BCV
    [REVISIÓN] Actualización automatica de tasa BCV ha presentado
    fallas en varios proyectos (incluyendo homologados)
    
    References:
     -Ticket: https://binaural.odoo.com/odoo/my-tickets/8166

--- l10n_ve_currency_rate_live/__manifest__.py
+++ l10n_ve_currency_rate_live/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Technical",
-    "version": "17.0.1.0.6",
+    "version": "17.0.1.0.7",
     "depends": ["l10n_ve_rate", "currency_rate_live"],
     "data": [
         "data/cron_data.xml",
