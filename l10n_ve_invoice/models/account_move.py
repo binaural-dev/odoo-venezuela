44790ef9116004ffac74547b03a57b631fe0f902
AlbertHamm-binaural <albert.hamm@binaural.com.ve>
2026-06-16 11:34:46 -0400

    Cambios solicitados para pasar el precommit

--- l10n_ve_invoice/models/account_move.py
+++ l10n_ve_invoice/models/account_move.py
@@ -4,7 +4,6 @@ import logging
 import calendar
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
-from odoo.tools import format_date
 from odoo.tools import format_date, float_is_zero, float_compare
 
 _logger = logging.getLogger(__name__)
@@ -105,7 +104,7 @@ class AccountMove(models.Model):
             invoices = record.env['account.move'].with_company(self.env.company.id).sudo().search([("correlative","=",correlative),('move_type', 'in',["out_invoice","out_refund"]),('company_id', '=', self.env.company.id)])
 
             if invoices and record.move_type in ["out_invoice","out_refund"]:
-                raise ValidationError(_("An invoice already exists with the Control Number: %s" % correlative))
+                raise ValidationError(_("An invoice already exists with the Control Number: %s") % correlative)
             if record.invoice_date and record.date and record.date < record.invoice_date:
                 raise UserError(_("The accounting date cannot be earlier than the invoice date."))
             if record.invoice_date:
@@ -208,7 +207,7 @@ class AccountMove(models.Model):
             max_product_invoice = self.company_id.max_product_invoice
             if len(self.invoice_line_ids) > max_product_invoice:
                 raise ValidationError(
-                    _("You can not add more than %s products to the invoice." % max_product_invoice)
+                    _("You can not add more than %s products to the invoice.") % max_product_invoice
                 )
 
     @api.depends("payment_term_details")
@@ -354,13 +353,11 @@ class AccountMove(models.Model):
 
             if changes:
                 move.message_post(
-                    body=_(
-                        """
+                    body="""
                         <div>
                             <ul>%s</ul>
                         </div>
-                        """
-                    ) % "".join(changes),
+                        """ % "".join(changes),
                     message_type='notification',
                     body_is_html=True
                 )
