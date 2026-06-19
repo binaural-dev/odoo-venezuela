002c6c1e838f92aae680ac4edd7735a8c34f0df9
yeisonasuaje <yeison@binaural.com.ve>
2026-06-19 10:19:57 -0400

    manifest

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accountant/Accountant",
-    "version": "17.0.0.0.23",
+    "version": "17.0.0.0.32",
     "depends": [
         "base",
         "account",
