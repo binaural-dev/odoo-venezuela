3183a774fb88c9f081eaa9c158b07582d2819845
SaulOrte <saul@binaural.dev>
2026-06-23 15:33:34 -0400

    Add traductions

--- l10n_ve_account_fiscalyear_closing/__manifest__.py
+++ l10n_ve_account_fiscalyear_closing/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "19.0.0.2",
+    "version": "19.0.0.3",
     "depends": [
         "account_fiscal_year_closing",
         "l10n_ve_contact",
