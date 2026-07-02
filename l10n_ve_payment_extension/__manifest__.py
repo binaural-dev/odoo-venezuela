81cca18e41844eaa9e02c245b75637bb2b8e9bd4
SaulOrte <saul@binaural.dev>
2026-07-02 12:18:04 -0400

    Se elimina flush innecesario, se ajusta el data para incluir el campo con un valor por defecto en la fecha  y se actualiza la rama

--- l10n_ve_payment_extension/__manifest__.py
+++ l10n_ve_payment_extension/__manifest__.py
@@ -25,6 +25,7 @@
         "security/ir_rule.xml",
         "data/sequence_data.xml",
         "data/type_person_data.xml",
+        "data/tax_unit_data.xml",
         "data/account_withholding_type_data.xml",
         "data/report_paperformat_data.xml",
         "data/fees_retention_data.xml",
