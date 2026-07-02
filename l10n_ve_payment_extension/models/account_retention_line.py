6a16d4191c27d178866e54ce543fc2a96101d2e5
SaulOrte <saul@binaural.dev>
2026-07-02 12:39:43 -0400

    Delete field display_invoice_number

--- l10n_ve_payment_extension/models/account_retention_line.py
+++ l10n_ve_payment_extension/models/account_retention_line.py
@@ -42,8 +42,9 @@ class AccountRetentionLine(models.Model):
     retention_rate = fields.Float(store=True, digits="Tasa")
     move_id = fields.Many2one("account.move", "move", ondelete="cascade", store=True)
     is_retention_client = fields.Boolean(default=True)
- 
-    invoice_amount = fields.Float(
+
+    invoice_amount = fields.Monetary(
+        currency_field="company_currency_id",
         string="Taxable income",
         compute="_compute_amounts",
         store=True,
