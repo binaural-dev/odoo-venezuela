0faa3a1faa72c1f974e06a589a23ac9f98e1038c
SaulOrte <saul@binaural.dev>
2026-07-02 15:13:05 -0400

    Se agrrega Pre-migrate para asignar el valor al campo de la fecha de disponiblidad a la unidad tributaria que se crea por defecto y tiene no-update - se corrijen test de retenciones que tienen incidencias en las  unidades tributarias

--- l10n_ve_payment_extension/tests/test_retention_flow_restriction.py
+++ l10n_ve_payment_extension/tests/test_retention_flow_restriction.py
@@ -10,6 +10,8 @@ class TestRetentionFlowRestriction(TransactionCase):
     def setUpClass(cls):
         super(TestRetentionFlowRestriction, cls).setUpClass()
 
+        cls.env.company.foreign_currency_id = cls.env.ref('base.USD')
+
         cls.partner = cls.env["res.partner"].create({"name": "Test Partner Retention Flow Restriction"})
         cls.product = cls.env["product.product"].create({"name": "Test Service Flow Restriction", "type": "service"})
         cls.journal = cls.env["account.journal"].search(
