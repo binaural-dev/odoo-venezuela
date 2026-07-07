a98e6f64731b730fc871db292ab2fa5e420e294b
David Mendoza <davidmendoza@binaural.com.ve>
2026-07-07 18:20:43 -0400

    manifest

--- l10n_ve_stock/__manifest__.py
+++ l10n_ve_stock/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "binaural-dev",
     "website": "https://www.binauraldev.com",
     "category": "Stock",
-    "version": "19.0.1.0.2",
+    "version": "19.0.1.0.3",
     "depends": [
         "stock",
         "product",
