a2f10994c03a11d0309f85c03a4594f85fce5e4d
SaulOrte <saul@binaural.dev>
2026-07-12 23:28:22 -0400

    [FIX] l10n_ve_invoice: Eliminar la Impresión de borradores de documentos fiscales como: facturas, Notas de Débito y Notas de Crédito y Ordenes de ventas
    - Se Hereda del modelo ir.action.report las funciones que controlan los botones de impresion para bloquear en todo el modelo las impresiones para modelos en estado que no sean posted en las facturas, y en las ordenes de ventas solo se permite imprimir para documentos en estados diferentes de draft.
    
    References:
    - Ticket: https://binaural.odoo.com/odoo/helpdesk/action-389/14012
    - Tarea:
    - PR:
    - Otros:

---
+++ l10n_ve_invoice/models/ir_actions_report.py
@@ -0,0 +1,54 @@
+from odoo import _, models
+from odoo.exceptions import UserError
+
+
+class IrActionsReport(models.Model):
+    _inherit = 'ir.actions.report'
+
+    def _render_qweb_pdf_prepare_streams(self, report_ref, data, res_ids=None):
+        report = self._get_report(report_ref) if report_ref else None
+        if report and res_ids and report.model == 'account.move':
+            docs = self.env['account.move'].browse(res_ids)
+            valid = docs.filtered(lambda d: d.state == 'posted')
+            if not valid:
+                raise UserError(_(
+                    "None of the selected documents are posted.\n"
+                    "Only posted documents can be printed."
+                ))
+            res_ids = valid.ids
+        if report and res_ids and report.model == 'sale.order':
+            docs = self.env['sale.order'].browse(res_ids)
+            valid = docs.filtered(lambda d: d.state != 'draft')
+            if not valid:
+                raise UserError(_(
+                    "None of the selected sale orders are confirmed.\n"
+                    "Only confirmed orders can be printed."
+                ))
+            res_ids = valid.ids
+        return super()._render_qweb_pdf_prepare_streams(report_ref, data, res_ids=res_ids)
+
+    def _render_qweb_html(self, report_ref, docids, data=None):
+        report = self._get_report(report_ref)
+        if data and data.get('context'):
+            ctx = data['context']
+            ids = ctx.get('active_ids') or docids
+            model = ctx.get('active_model') or report.model
+            if model == 'account.move' and ids:
+                docs = self.env[model].browse(ids)
+                valid = docs.filtered(lambda d: d.state == 'posted')
+                if not valid:
+                    raise UserError(_(
+                        "None of the selected documents are posted.\n"
+                        "Only posted documents can be printed."
+                    ))
+                return super()._render_qweb_html(report_ref, valid.ids, data=data)
+            if model == 'sale.order' and ids:
+                docs = self.env[model].browse(ids)
+                valid = docs.filtered(lambda d: d.state != 'draft')
+                if not valid:
+                    raise UserError(_(
+                        "None of the selected sale orders are confirmed.\n"
+                        "Only confirmed orders can be printed."
+                    ))
+                return super()._render_qweb_html(report_ref, valid.ids, data=data)
+        return super()._render_qweb_html(report_ref, docids, data=data)
