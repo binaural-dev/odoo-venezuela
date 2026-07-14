b934575008283d21bb83768ca9d4d146e777e4ba
David Mendoza <davidmendoza@binaural.com.ve>
2026-07-14 10:34:13 -0400

    fixin tests

--- l10n_ve_invoice/tests/test_tax_action_post.py
+++ l10n_ve_invoice/tests/test_tax_action_post.py
@@ -7,6 +7,8 @@ class TestInvoiceTaxConstraint(TransactionCase):
     def setUp(self):
         super(TestInvoiceTaxConstraint, self).setUp()
         self.company = self.env.ref("base.main_company")
+        self.company.currency_id = self.env.ref("base.VEF")
+        self.company.foreign_currency_id = self.env.ref("base.USD")
         self.partner = self.env["res.partner"].create({"name": "Test customer"})
         self.journal = self.env["account.journal"].create({
             "name": "Sales Journal",
