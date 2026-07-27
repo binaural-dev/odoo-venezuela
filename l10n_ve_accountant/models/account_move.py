268cc7636362d8873868bd66a005cc1305cb4ddb
SaulOrte <saul@binaural.dev>
2026-07-27 16:05:44 -0400

    Se reedita test unitario, solo es valido para validar el campo digits en el foreign_inverse_rate del account_move ya q lo demas lo hereda, se agrega validacion en la conversion de la moneda para cuando sea compañia USD ya que realizaba mal la conversion en ese caso especifico cuando custom_rate estaba definido

--- l10n_ve_accountant/models/account_move.py
+++ l10n_ve_accountant/models/account_move.py
@@ -123,6 +123,7 @@ class AccountMove(models.Model):
         store=True,
         index=True,
         readonly=False,
+        digits=0,
     )
 
     manually_set_rate = fields.Boolean(default=False)
