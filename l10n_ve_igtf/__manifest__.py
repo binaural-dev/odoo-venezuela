7a8866dfd42f84a41eecc649a476168cda7473ac
SaulOrte <saul@binaural.dev>
2026-08-02 10:27:30 -0400

    update manifest

--- l10n_ve_igtf/__manifest__.py
+++ l10n_ve_igtf/__manifest__.py
@@ -5,7 +5,7 @@
     "author": "binaural-dev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Accounting",
-    "version": "17.0.1.0.2",
+    "version": "17.0.1.0.3",
         "depends": [
         "base",
         "l10n_ve_accountant",
