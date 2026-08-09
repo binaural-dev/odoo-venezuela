3d358600f8afb91439d42cb3c68f954893ced5dd
SaulOrte <saul@binaural.dev>
2026-08-09 18:07:57 -0400

    chore: revert unintended manifest version bumps
    Los commits anteriores de esta rama no requerian incrementar la
    version del manifest.

--- l10n_ve_accountant/__manifest__.py
+++ l10n_ve_accountant/__manifest__.py
@@ -41,7 +41,7 @@ Cambios en UI / Modelos impactados
     "author": "Binauraldev",
     "website": "https://binauraldev.com/",
     "category": "Accounting/Localizations/Account Chart",
-    "version": "17.0.0.0.61",
+    "version": "17.0.0.0.60",
     "depends": [
         "base",
         "web",
